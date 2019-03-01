dd751323d1213c02c18a9b8a9fc92abcc2dd7a94
Shaed Parkar <shaed.parkar@hmcts.net>
2019-03-01 17:24:34 +0000

    Add delete hearing endpoint

---
+++ BookingsAPI/Bookings.DAL/Commands/RemoveHearingCommand.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Bookings.DAL.Commands
+{
+    public class RemoveHearingCommand : ICommand
+    {
+        public RemoveHearingCommand(Guid hearingId)
+        {
+            HearingId = hearingId;
+        }
+
+        public Guid HearingId { get; set; }
+    }
+}
---
+++ BookingsAPI/Bookings.IntegrationTests/Database/Commands/RemoveHearingCommandTests.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Bookings.DAL;
+using Bookings.DAL.Commands;
+using Bookings.DAL.Exceptions;
+using Bookings.DAL.Queries;
+using Bookings.Domain.Participants;
+using Bookings.Domain.RefData;
+using Bookings.Domain.Validations;
+using NUnit.Framework;
+using Testing.Common.Builders.Domain;
+
+namespace Bookings.IntegrationTests.Database.Commands
+{
+    public class RemoveHearingCommandTests : DatabaseTestsBase
+    {
+        private RemoveHearingCommandHandler _commandHandler;
+        private Guid _newHearingId;
+
+        [SetUp]
+        public void Setup()
+        {
+            var context = new BookingsDbContext(BookingsDbContextOptions);
+            _commandHandler = new RemoveHearingCommandHandler(context);
+            _newHearingId = Guid.Empty;
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            if (_newHearingId != Guid.Empty)
+            {
+                TestContext.WriteLine($"Removing test hearing {_newHearingId}");
+                await Hooks.RemoveVideoHearing(_newHearingId);
+            }
+        }
+
+        [Test]
+        public void should_throw_exception_when_hearing_does_not_exist()
+        {
+            var hearingId = Guid.NewGuid();
+            Assert.ThrowsAsync<HearingNotFoundException>(() => _commandHandler.Handle(
+                new RemoveHearingCommand(hearingId)));
+        }
+
+        [Test]
+        public async Task should_throw_exception_when_participant_does_not_exist()
+        {
+            var seededHearing = await Hooks.SeedVideoHearing();
+            TestContext.WriteLine($"New seeded video hearing id: {seededHearing.Id}");
+            _newHearingId = seededHearing.Id;
+
+            var newPerson = new PersonBuilder(true).Build();
+            var participant = new Individual(newPerson, new HearingRole(1, "Dummy"), new CaseRole(1, "Dummy"));
+            Assert.ThrowsAsync<DomainRuleException>(() => _commandHandler.Handle(
+                new RemoveHearingCommand(seededHearing.Id, participant)));
+        }
+
+        [Test]
+        public async Task should_remove_hearing()
+        {
+            var seededHearing = await Hooks.SeedVideoHearing();
+            TestContext.WriteLine($"New seeded video hearing id: {seededHearing.Id}");
+            _newHearingId = seededHearing.Id;
+
+            var beforeCount = seededHearing.GetParticipants().Count;
+
+            var participant = seededHearing.GetParticipants().First();
+            await _commandHandler.Handle(new RemoveParticipantFromHearingCommand(seededHearing.Id, participant));
+
+
+            var returnedVideoHearing =
+                await _getHearingByIdQueryHandler.Handle(new GetHearingByIdQuery(seededHearing.Id));
+            var afterCount = returnedVideoHearing.GetParticipants().Count;
+
+            afterCount.Should().BeLessThan(beforeCount);
+        }
+    }
+}
