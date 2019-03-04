e65e7b1744cbc93a3d07b77b32da323d39f2001a
Shaed Parkar <shaed.parkar@hmcts.net>
2019-03-04 15:43:18 +0000

    add missing dependecies after merge issues

--- BookingsAPI/Bookings.AcceptanceTests/Steps/StepsBase.cs
+++ BookingsAPI/Bookings.AcceptanceTests/Steps/StepsBase.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Net;
 using Bookings.AcceptanceTests.Contexts;
 using Bookings.AcceptanceTests.Helpers;
 using Bookings.API;
 using Bookings.Common.Configuration;
 using Bookings.Common.Security;
+using FluentAssertions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 using TechTalk.SpecFlow;
