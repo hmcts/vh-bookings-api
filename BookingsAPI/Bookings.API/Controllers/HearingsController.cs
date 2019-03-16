6f29c25f35f19103d69c1e1d8a41906abf3ad07b
Travis CI <travis@travis-ci.org>
2019-03-16 08:50:43 +0000

    booking controller -4193

--- BookingsAPI/Bookings.API/Controllers/HearingsController.cs
+++ BookingsAPI/Bookings.API/Controllers/HearingsController.cs
@@ -226,48 +226,5 @@ namespace Bookings.API.Controllers
                 await _queryHandler.Handle<GetHearingVenuesQuery, List<HearingVenue>>(getHearingVenuesQuery);
             return hearingVenues.SingleOrDefault(x => x.Name == venueName);
         }
-
-        ///// <summary>
-        /////     Gets list of upcoming bookings hearing for a given case types
-        ///// </summary>
-        ///// <param name="types">The hearing case types.</param>
-        ///// <param name="cursor">Cursor specifying from which entries to read next page, is defaulted if not specified</param>
-        ///// <param name="limit">The max number hearings records to return.</param>
-        ///// <returns>The list of bookings video hearing</returns>
-        //[HttpGet("types", Name = "GetHearingsByTypes")]
-        //[SwaggerOperation(OperationId = "GetHearingsByTypes")]
-        //[ProducesResponseType(typeof(BookingsResponse), (int)HttpStatusCode.OK)]
-        //[ProducesResponseType((int)HttpStatusCode.BadRequest)]
-        //[ProducesResponseType((int)HttpStatusCode.NotFound)]
-        //public async Task<ActionResult<BookingsResponse>> GetHearingsByTypes([FromQuery(Name = "types")]List<int> types, [FromQuery]string cursor = "0", [FromQuery]int limit = 100)
-        //{
-        //    const string BookingsEndpointUrl = "hearings/types";
-        //    types = types ?? new List<int>();
-        //    if (!await ValidateCaseTypes(types))
-        //    {
-        //        ModelState.AddModelError("Hearing types", "Invalid value for hearing types");
-        //        return BadRequest(ModelState);
-        //    }
-
-        //    var query = new GetBookingsByCaseTypesQuery(types, cursor, limit);
-        //    var videoHearings = await _queryHandler.Handle<GetBookingsByCaseTypesQuery, IList<VideoHearing>>(query);
-        //    var mapper = new VideoHearingsToBookingsResponseMapper();
-        //    var response = new PaginationCursorBasedBuilder<BookingsResponse, VideoHearing>(mapper.MapHearingResponses)
-        //       .WithSourceItems(videoHearings.AsQueryable())
-        //       .Limit(limit)
-        //       .CaseTypes(types)
-        //       .Cursor(cursor)
-        //       .ResourceUrl(BookingsEndpointUrl)
-        //       .Build();
-
-        //    return Ok(response);
-        //}
-
-        //private async Task<bool> ValidateCaseTypes(List<int> caseTypes)
-        //{
-        //    var query = new GetAllCaseTypesQuery();
-        //    var allCasesTypes = await _queryHandler.Handle<GetAllCaseTypesQuery, List<CaseType>>(query);
-        //    return caseTypes.Count == 0 || caseTypes.All(s => allCasesTypes.Any(x => x.Id == s));
-        //}
     }
 }
