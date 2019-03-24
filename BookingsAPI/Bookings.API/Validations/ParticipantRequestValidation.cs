4f2cc99d67e41992e29a354c0344d0cb63717244
kkodalikk <karuna.kodali@hmcts.net>
2019-03-24 23:17:29 +0000

    Added Fluent validations for Address

--- BookingsAPI/Bookings.API/Validations/ParticipantRequestValidation.cs
+++ BookingsAPI/Bookings.API/Validations/ParticipantRequestValidation.cs
@@ -1,5 +1,6 @@
 using Bookings.Api.Contract.Requests;
 using FluentValidation;
+using System.Linq;
 
 namespace Bookings.API.Validations
 {
@@ -13,9 +14,17 @@ namespace Bookings.API.Validations
         public static readonly string NoLastNameErrorMessage = "Last name is required";
         public static readonly string NoUsernameErrorMessage = "Username is required";
         public static readonly string NoContactEmailErrorMessage = "Contact Email is required";
-        
+        public static readonly string NoHousenumberErrorMessage = "Housenumber is required";
+        public static readonly string NoStreetErrorMessage = "Street is required";
+        public static readonly string NoCityErrorMessage = "City is required";
+        public static readonly string NoCountyErrorMessage = "County is required";
+        public static readonly string NoPostcodeErrorMessage = "Postcode is required";
+
+
+
         public ParticipantRequestValidation()
         {
+            var individualRoles = new[] { "Claimant LIP", "Defendant LIP", "Applicant LIP", "Respondent LIP" };
             RuleFor(x => x.Title).NotEmpty().WithMessage(NoTitleErrorMessage);
             RuleFor(x => x.FirstName).NotEmpty().WithMessage(NoFirstNameErrorMessage);
             RuleFor(x => x.LastName).NotEmpty().WithMessage(NoLastNameErrorMessage);
@@ -25,6 +34,16 @@ namespace Bookings.API.Validations
             RuleFor(x => x.DisplayName).NotEmpty().WithMessage(NoDisplayNameErrorMessage);
             RuleFor(x => x.CaseRoleName).NotEmpty().WithMessage(NoCaseRoleNameErrorMessage);
             RuleFor(x => x.HearingRoleName).NotEmpty().WithMessage(NoHearingRoleNameErrorMessage);
+            RuleFor(x => x.HouseNumber).NotEmpty().When(x => individualRoles.Contains(x.HearingRoleName)).WithMessage(NoHousenumberErrorMessage);
+            RuleFor(x => x.Street).NotEmpty().When(x => individualRoles.Contains(x.HearingRoleName)).WithMessage(NoStreetErrorMessage);
+            RuleFor(x => x.City).NotEmpty().When(x => individualRoles.Contains(x.HearingRoleName)).WithMessage(NoCityErrorMessage);
+            RuleFor(x => x.County).NotEmpty().When(x => individualRoles.Contains(x.HearingRoleName)).WithMessage(NoCountyErrorMessage);
+            RuleFor(x => x.Postcode).NotEmpty().When(x => individualRoles.Contains(x.HearingRoleName)).WithMessage(NoPostcodeErrorMessage);
+
+
+
         }
+
+
     }
 }
