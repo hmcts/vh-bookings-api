f62a462797177608456f4c49b2a5fba6d45d370f
muralikaturi <murali.katuri@hmcts.net>
2019-03-26 13:04:45 +0000

    Added UserRole and Representee to the events

--- BookingsAPI/Bookings.Infrastructure.Services/IntegrationEvents/Events/HearingIsReadyForVideoIntegrationEvent.cs
+++ BookingsAPI/Bookings.Infrastructure.Services/IntegrationEvents/Events/HearingIsReadyForVideoIntegrationEvent.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using Bookings.Domain;
+using Bookings.Domain.Participants;
 using Bookings.Infrastructure.Services.Dtos;
 
 namespace Bookings.Infrastructure.Services.IntegrationEvents.Events
@@ -9,13 +10,20 @@ namespace Bookings.Infrastructure.Services.IntegrationEvents.Events
     {
         public HearingIsReadyForVideoIntegrationEvent(Hearing hearing)
         {
-            var @case = hearing.GetCases().First(); // is this needs to ba lead case?
+            var @case = hearing.GetCases().First(); // is this needs to be a lead case?
             Hearing = new HearingDto(hearing.Id, hearing.ScheduledDateTime, hearing.ScheduledDuration,
                 hearing.CaseType.Name, @case.Number);
 
-            Participants = hearing.GetParticipants().Select(x => new ParticipantDto(x.Id,
-                $"{x.Person.Title} {x.Person.FirstName} {x.Person.LastName}",
-                x.Person.Username, x.DisplayName, x.HearingRole.UserRole.Name, x.CaseRole.Group)).ToList();
+            var hearingParticipants = hearing.GetParticipants();
+
+            Participants = hearingParticipants.Select(x =>
+            {
+                var representee = x is Representative representative ? representative.Representee : string.Empty;
+                return new ParticipantDto(x.Id,
+                    $"{x.Person.Title} {x.Person.FirstName} {x.Person.LastName}",
+                    x.Person.Username, x.DisplayName, x.HearingRole.Name, x.HearingRole.UserRole.Name,
+                    x.CaseRole.Group, representee);
+            }).ToList();
         }
 
         public HearingDto Hearing { get; }
--- BookingsAPI/Bookings.Infrastructure.Services/IntegrationEvents/Events/ParticipantAddedIntegrationEvent.cs
+++ BookingsAPI/Bookings.Infrastructure.Services/IntegrationEvents/Events/ParticipantAddedIntegrationEvent.cs
@@ -9,9 +9,13 @@ namespace Bookings.Infrastructure.Services.IntegrationEvents.Events
         public ParticipantAddedIntegrationEvent(Guid hearingId, Participant participant)
         {
             HearingId = hearingId;
+            var representee = participant is Representative representative ? representative.Representee : string.Empty;
+
             Participant = new ParticipantDto(participant.Id,
                 $"{participant.Person.Title} {participant.Person.FirstName} {participant.Person.LastName}",
-                participant.Person.Username, participant.DisplayName, participant.HearingRole.UserRole.Name, participant.CaseRole.Group);
+                participant.Person.Username, participant.DisplayName, 
+                participant.HearingRole.Name, participant.HearingRole.UserRole.Name, 
+                participant.CaseRole.Group, representee);
         }
 
         public Guid HearingId { get; }
