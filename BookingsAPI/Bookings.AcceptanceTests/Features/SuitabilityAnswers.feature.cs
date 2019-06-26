ab078470926d1498033c865d5d985eddf7f93e65
abitec <abiodun.ve@gmail.com>
2019-06-26 15:41:16 +0100

    Added new feature file

---
+++ BookingsAPI/Bookings.AcceptanceTests/Features/SuitabilityAnswers.feature.cs
@@ -0,0 +1,3 @@
+﻿#error Could not find a reference to SpecFlow in project 'Bookings.AcceptanceTests'.
+#error Please add the 'TechTalk.SpecFlow' package to the project and use MSBuild generation instead of using SpecFlowSingleFileGenerator.
+#error For more information see httsps://specflow.org/documentation/Generate-Tests-from-MsBuild/
---
+++ BookingsAPI/Bookings.AcceptanceTests/Steps/SuitabilityAnswersSteps.cs
@@ -0,0 +1,24 @@
+﻿using Bookings.AcceptanceTests.Contexts;
+using TechTalk.SpecFlow;
+using Testing.Common.Builders.Api;
+
+namespace Bookings.AcceptanceTests.Steps
+{
+    [Binding]
+    public sealed class SuitabilityAnswersSteps
+    {
+        private readonly TestContext _acTestContext;
+        private readonly SuitabilityAnswerEndpoints _endpoints = new ApiUriFactory().SuitabilityAnswerEndpoints;
+
+        public SuitabilityAnswersSteps(TestContext acTestContext)
+        {
+            _acTestContext = acTestContext;
+        }
+
+        [Given(@"I have a get suitability answers request")]
+        public void GivenIHaveAGetSuitabilityAnswersRequest()
+        {
+            
+        }
+    }
+}
