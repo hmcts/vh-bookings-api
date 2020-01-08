8912d056a472fbfc21c976784aff7ac8a351e5e3
Atif Rehman <atif.rehman@outlook.com>
2020-01-08 09:59:12 +0000

    VIH-5429: Adding new venue

---
+++ BookingsAPI/Bookings.DAL/Migrations/20200108093744_Add_New_Venue.cs
@@ -0,0 +1,24 @@
+﻿using Bookings.Domain;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bookings.DAL.Migrations
+{
+    public partial class Add_New_Venue : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: nameof(HearingVenue),
+                columns: new[] {"Id", "Name"},
+                values: new object[,]
+                {
+                    {3, "Taylor House Tribunal Hearing Centre"}
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData("HearingVenue", "Id", 3);
+        }
+    }
+}
