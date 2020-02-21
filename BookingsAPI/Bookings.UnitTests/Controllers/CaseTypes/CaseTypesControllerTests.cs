a51bc34a2e6aa1bcc5e8c2b13a52c288412d8b4e
Saravanan Radha Krishnan <Saravanan.Krishnan@HMCTS.NET>
2020-02-21 17:30:38 +0000

    Added unit test coverage for CaseTypesController

---
+++ BookingsAPI/Bookings.UnitTests/Controllers/CaseTypes/CaseTypesControllerTests.cs
@@ -0,0 +1,22 @@
+﻿
+using Bookings.API.Controllers;
+using Bookings.DAL.Queries.Core;
+using Moq;
+using NUnit.Framework;
+
+namespace Bookings.UnitTests.Controllers
+{
+    public class CaseTypesControllerTests
+    {
+        protected Mock<IQueryHandler> QueryHandler;
+        protected CaseTypesController Controller;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            QueryHandler = new Mock<IQueryHandler>();
+
+            Controller = new CaseTypesController(QueryHandler.Object);
+        }
+    }
+}
