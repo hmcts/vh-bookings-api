d9978a5339b0de09407e13ca29ec9ff29186b732
Sunil D'Souza <41689433+jdssunil@users.noreply.github.com>
2020-06-18 10:15:26 +0100

    VIH-5669 New venue list (#144)
    * feature/VIH-5669 : New venue list
    
    * feature/VIH-5669 : Updated migration

---
+++ BookingsAPI/Bookings.DAL/Migrations/20200617143900_Add_New_Hearing_Venue.cs
@@ -0,0 +1,32 @@
+﻿using Bookings.Domain;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bookings.DAL.Migrations
+{
+    public partial class Add_New_Hearing_Venue : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+               nameof(HearingVenue),
+               new[] { "Id", "Name" },
+               new object[,]
+               {
+                    { 4, "London Property Tribunal" },
+                    { 5, "Eastern Property Tribunal" },
+                    { 6, "Midlands Property Tribunal" },
+                    { 7, "Northern Property Tribunal" },
+                    { 8, "Southern Property Tribunal" },
+               });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData("HearingVenue", "Id", 4);
+            migrationBuilder.DeleteData("HearingVenue", "Id", 5);
+            migrationBuilder.DeleteData("HearingVenue", "Id", 6);
+            migrationBuilder.DeleteData("HearingVenue", "Id", 7);
+            migrationBuilder.DeleteData("HearingVenue", "Id", 8);
+        }
+    }
+}
