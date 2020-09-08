23084e0cccff184cbf22b1b5f928ee0a93df732f
Shaed Parkar <shaed.parkar@slimsolutions.company>
2020-09-08 14:11:00 +0100

    Remove duplicate response code from swagger doc Do not excude exceptions folder from coverage in dal project

--- BookingsAPI/Bookings.API/Controllers/PersonsController.cs
+++ BookingsAPI/Bookings.API/Controllers/PersonsController.cs
@@ -71,7 +71,6 @@ namespace Bookings.API.Controllers
         [SwaggerOperation(OperationId = "GetHearingsByUsernameForDeletion")]
         [ProducesResponseType(typeof(List<HearingsByUsernameForDeletionResponse>), (int) HttpStatusCode.OK)]
         [ProducesResponseType((int) HttpStatusCode.NotFound)]
-        [ProducesResponseType((int) HttpStatusCode.Unauthorized)]
         public async Task<IActionResult> GetHearingsByUsernameForDeletion([FromQuery] string username)
         {
             var query = new GetHearingsByUsernameForDeletionQuery(username);
