71cfe4167c1aa60ee8de1c9403b224d527bb26a2
Valentina <Valentina.bennett@hmcts.net>
2020-09-11 13:55:26 +0100

    feature/VIH-6416 : Add zipSuccess field to Hearing

--- BookingsAPI/Bookings.DAL/Migrations/20200911124945_add_field_zipSuccess_to_hearing_table.Designer.cs
+++ BookingsAPI/Bookings.DAL/Migrations/20200911124945_add_field_zipSuccess_to_hearing_table.Designer.cs
@@ -5,14 +5,16 @@ using Bookings.Domain.Enumerations;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Bookings.DAL.Migrations
 {
     [DbContext(typeof(BookingsDbContext))]
-    partial class BookingsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200911124945_add_field_zipSuccess_to_hearing_table")]
+    partial class add_field_zipSuccess_to_hearing_table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
