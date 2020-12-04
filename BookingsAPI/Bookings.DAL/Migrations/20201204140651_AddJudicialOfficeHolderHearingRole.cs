f002dd5f02e7c4662d24f08522ead95df3081485
Viren Sareen <viren.sareen@solirius.com>
2020-12-04 15:06:05 +0000

    added migrations for JudicialOfficeHolder

---
+++ BookingsAPI/Bookings.DAL/Migrations/20201204140651_AddJudicialOfficeHolderHearingRole.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bookings.DAL.Migrations
+{
+    public partial class AddJudicialOfficeHolderHearingRole : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "HearingRole",
+                columns: new[] {"Name", "UserRoleId", "CaseRoleId"},
+                values: new object[,]
+                {
+                    {"Winger", 7, 88 },
+                    {"Panel Member", 7, 50 },
+                    {"Panel Member", 7, 52 },
+                    {"Panel Member", 7, 54 },
+                    {"Panel Member", 7, 56 },
+                    {"Panel Member", 7, 58 },
+                    {"Panel Member", 7, 60 },
+                    {"Panel Member", 7, 62 },
+                    {"Panel Member", 7, 64 },
+                    {"Panel Member", 7, 66 },
+                    {"Panel Member", 7, 68 },
+                    {"Panel Member", 7, 70 },
+                    {"Panel Member", 7, 72 },
+                    {"Panel Member", 7, 74 },
+                    {"Panel Member", 7, 76 },
+                    {"Panel Member", 7, 78 },
+                    {"Panel Member", 7, 80 },
+                    {"Panel Member", 7, 87 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            for (var hearingRoleId = 214; hearingRoleId < 233; hearingRoleId++)
+            {
+                migrationBuilder.DeleteData("HearingRole", "Id", hearingRoleId);
+            }
+        }
+    }
+}
---
+++ BookingsAPI/Bookings.DAL/Migrations/20201204144413_SetParticipantDiscriminatorToJudicialOfficeHolder.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bookings.DAL.Migrations
+{
+    public partial class SetParticipantDiscriminatorToJudicialOfficeHolder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"UPDATE Participant
+                SET Discriminator = 'JudicialOfficeHolder'
+                FROM Participant p 
+                INNER JOIN HearingRole hr ON p.HearingRoleId = hr.Id
+                WHERE hr.Name = 'Panel Member' OR hr.Name = 'Winger'");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"UPDATE Participant SET Discriminator = 'Individual' 
+                WHERE Id = 'bb4a09d2-447e-4669-a0db-bec363419f34'");
+        }
+    }
+}
