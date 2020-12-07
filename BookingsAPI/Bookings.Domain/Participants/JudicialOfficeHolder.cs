6c72f14e960978648d057dd5f69b0d9b8d4f8006
Viren Sareen <viren.sareen@solirius.com>
2020-12-07 16:33:37 +0000

    removed protected ctor

--- BookingsAPI/Bookings.Domain/Participants/JudicialOfficeHolder.cs
+++ BookingsAPI/Bookings.Domain/Participants/JudicialOfficeHolder.cs
@@ -4,10 +4,6 @@ namespace Bookings.Domain.Participants
 {
     public class JudicialOfficeHolder : Participant
     {
-        protected JudicialOfficeHolder()
-        {
-        }
-
         public JudicialOfficeHolder(Person person, HearingRole hearingRole, CaseRole caseRole) : base(person, hearingRole,
             caseRole)
         {
