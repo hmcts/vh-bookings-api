034d4adf74fcaa426d3758c35c8f42261f07ba58
Viren Sareen <viren.sareen@solirius.com>
2020-12-08 15:35:02 +0000

    pushing remaining files

--- BookingsAPI/Bookings.Domain/Participants/JudicialOfficeHolder.cs
+++ BookingsAPI/Bookings.Domain/Participants/JudicialOfficeHolder.cs
@@ -4,6 +4,8 @@ namespace Bookings.Domain.Participants
 {
     public class JudicialOfficeHolder : Participant
     {
+        protected JudicialOfficeHolder() { }
+        
         public JudicialOfficeHolder(Person person, HearingRole hearingRole, CaseRole caseRole) : base(person, hearingRole,
             caseRole)
         {
