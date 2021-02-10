3258418b98335351d75d4e0e6b0a117f1d9a5371
Shaed Parkar <shaed.parkar@slimsolutions.company>
2021-02-10 11:27:14 +0000

    WIP add empty migration to update roles

--- BookingsApi/BookingsApi.DAL/Migrations/20210210112405_UpdateInterpreterHearingRole.Designer.cs
+++ BookingsApi/BookingsApi.DAL/Migrations/20210210112405_UpdateInterpreterHearingRole.Designer.cs
@@ -5,14 +5,16 @@ using BookingsApi.Domain.Enumerations;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BookingsApi.DAL.Migrations
 {
     [DbContext(typeof(BookingsDbContext))]
-    partial class BookingsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210210112405_UpdateInterpreterHearingRole")]
+    partial class UpdateInterpreterHearingRole
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
