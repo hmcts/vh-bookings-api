4055770693309c5118ac3049de8a4571d3083531
Viren Sareen <viren.sareen@solirius.com>
2021-02-10 12:36:59 +0000

    updated linked participant dto

--- BookingsApi/BookingsApi.Infrastructure.Services/Dtos/LinkedParticipantDto.cs
+++ BookingsApi/BookingsApi.Infrastructure.Services/Dtos/LinkedParticipantDto.cs
@@ -1,21 +1,16 @@
 using System;
-using BookingsApi.Domain.Ddd;
 using BookingsApi.Domain.Enumerations;
-using BookingsApi.Domain.Participants;
 
 namespace BookingsApi.Infrastructure.Services.Dtos
 {
-    public class LinkedParticipantDto : Entity<Guid>
+    public class LinkedParticipantDto
     {
         public Guid ParticipantId { get; private set; }
-        public Participant Participant { get; private set; }
         public Guid LinkedId { get; private set; }
-        public Participant Linked { get; private set; }
         public LinkedParticipantType Type { get; private set; }
         
         public LinkedParticipantDto(Guid participantId, Guid linkedId, LinkedParticipantType type)
         {
-            Id = Guid.NewGuid();
             ParticipantId = participantId;
             LinkedId = linkedId;
             Type = type;
