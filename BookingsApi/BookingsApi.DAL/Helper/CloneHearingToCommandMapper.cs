c97fbcbc91788a2b1d10a0c5c2cb0a8f1544a9d6
Lionel Roche <lionel.roche@hmcts.net>
2021-02-20 12:01:48 +0000

    VIH-6895 - VHO_adds_an_interpreter-multi-day - reverted filtering logic and removed the domain rule for add links

--- BookingsApi/BookingsApi.DAL/Helper/CloneHearingToCommandMapper.cs
+++ BookingsApi/BookingsApi.DAL/Helper/CloneHearingToCommandMapper.cs
@@ -80,19 +80,10 @@ namespace BookingsApi.DAL.Helper
 
         private static List<LinkedParticipantDto> GetLinkedParticipantDtos(Hearing hearing)
         {
-            var hearingParticipantTemp = hearing.Participants.SelectMany(x => x.LinkedParticipants).ToList();
-            var hearingParticipantsFiltered = new List<Participant>();
-            foreach (var lp in hearingParticipantTemp)
-            {
-                // check if we have added a participant with the linked or participant id into filtered list
-                if (!hearingParticipantsFiltered.Any(x => x.Id == lp.ParticipantId || x.Id == lp.LinkedId))
-                {
-                    hearingParticipantsFiltered.Add(hearing.Participants.Single(x=>x.Id == lp.ParticipantId));
-                }
-            }
+            var hearingParticipants = hearing.Participants.Where(x => x.LinkedParticipants.Any()).ToList();
 
             var linkedParticipantDtos = new List<LinkedParticipantDto>();
-            foreach (var hearingParticipant in hearingParticipantsFiltered)
+            foreach (var hearingParticipant in hearingParticipants)
             {
                 var participantEmail = hearingParticipant.Person.ContactEmail;
                 var participantLink = hearingParticipant.GetLinkedParticipants()
