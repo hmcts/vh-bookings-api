ca19140e1a1995f06eb2d41e77aed9e71582bce0
RocheL <lionel.roche@hmcts.net>
2021-02-24 11:10:54 +0000

    VIH-7215 : Update Booking api to return linked participants - updated query to return linked participants (#245)
    Co-authored-by: shaed-parkar <41630528+shaed-parkar@users.noreply.github.com>

--- BookingsApi/BookingsApi.IntegrationTests/Database/Queries/GetHearingsByUsernameQueryHandlerTests.cs
+++ BookingsApi/BookingsApi.IntegrationTests/Database/Queries/GetHearingsByUsernameQueryHandlerTests.cs
@@ -2,6 +2,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using BookingsApi.DAL;
 using BookingsApi.DAL.Queries;
+using BookingsApi.Domain.Enumerations;
+using BookingsApi.Domain.Participants;
 using FluentAssertions;
 using NUnit.Framework;
 
@@ -34,6 +36,26 @@ namespace BookingsApi.IntegrationTests.Database.Queries
             result.Any(x => x.Id == hearing2.Id).Should().BeTrue();
             result.Any(x => x.Id == hearing1.Id).Should().BeFalse();
             result.Any(x => x.Id == hearing3.Id).Should().BeFalse();
+        }
+
+        [Test]
+        public async Task Should_return_hearings_with_linked_participants_for_username()
+        {
+            var seededHearing = await Hooks.SeedVideoHearingLinkedParticipants(null);
+            var username = seededHearing.GetPersons().First().Username;
+            var query = new GetHearingsByUsernameQuery(username);
+
+            var hearings = await _handler.Handle(query);
+            hearings.Any().Should().BeTrue();
+
+            var participants = hearings[0].GetParticipants();
+            participants.Any().Should().BeTrue();
+            var individuals = participants.Where(x => x.GetType() == typeof(Individual))
+                .ToList();
+            individuals[0].LinkedParticipants.Should().NotBeNull();
+            individuals[0].LinkedParticipants[0].Type.Should().Be(LinkedParticipantType.Interpreter);
+            individuals[1].LinkedParticipants.Should().NotBeNull();
+            individuals[1].LinkedParticipants[0].Type.Should().Be(LinkedParticipantType.Interpreter);
         }
     }
 }
