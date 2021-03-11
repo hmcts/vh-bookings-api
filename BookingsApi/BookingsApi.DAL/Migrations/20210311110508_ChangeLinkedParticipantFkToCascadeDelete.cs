f9a56fc95e5a147d5d618ef559fff7f7c6d8c741
williambowshersolirius <77436840+williambowshersolirius@users.noreply.github.com>
2021-03-11 11:24:44 +0000

    Modifying Foreign Key constraint on LinkedParticipant table to take NoAction on delete of Participants and just allow deleteing LinkedParticipant manually.

---
+++ BookingsApi/BookingsApi.DAL/Migrations/20210311110508_ChangeLinkedParticipantFkToCascadeDelete.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookingsApi.DAL.Migrations
+{
+    public partial class ChangeLinkedParticipantFkToCascadeDelete : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey("FK_LinkedParticipant_Participant_LinkedId", "LinkedParticipant");
+            migrationBuilder.DropForeignKey("FK_LinkedParticipant_Participant_ParticipantId", "LinkedParticipant");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LinkedParticipant_Participant_LinkedId",
+                table: "LinkedParticipant",
+                column: "LinkedId",
+                principalTable: "Participant",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction
+            );
+            
+            migrationBuilder.AddForeignKey(
+                name: "FK_LinkedParticipant_Participant_ParticipantId",
+                table: "LinkedParticipant",
+                column: "ParticipantId",
+                principalTable: "Participant",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey("FK_LinkedParticipant_Participant_LinkedId", "LinkedParticipant");
+            migrationBuilder.DropForeignKey("FK_LinkedParticipant_Participant_ParticipantId", "LinkedParticipant");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LinkedParticipant_Participant_LinkedId",
+                table: "LinkedParticipant",
+                column: "LinkedId",
+                principalTable: "Participant",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict
+            );
+            
+            migrationBuilder.AddForeignKey(
+                name: "FK_LinkedParticipant_Participant_ParticipantId",
+                table: "LinkedParticipant",
+                column: "ParticipantId",
+                principalTable: "Participant",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict
+            );
+        }
+    }
+}
