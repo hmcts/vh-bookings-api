e96ea8bcfaa1374e0c0e9c7cab0913c8e39b304a
Valentina <Valentina.bennett@hmcts.net>
2021-03-27 17:49:27 +0000

    feature/VIH-7528 : unit test added

---
+++ BookingsApi/BookingsApi.UnitTests/DAL/Queries/GetJudiciaryPersonBySearchTermQueryTest.cs
@@ -0,0 +1,19 @@
+﻿using BookingsApi.DAL.Queries;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace BookingsApi.UnitTests.DAL.Queries
+{
+    public class GetJudiciaryPersonBySearchTermQueryTest
+    {
+        [Test]
+        public void Should_search_forJudiciary_person_by_term_lower_case()
+        {
+            var query = new GetJudiciaryPersonBySearchTermQuery("Tester");
+
+            query.Term.Should().Be("tester");
+        }
+    }
+}
+
+
