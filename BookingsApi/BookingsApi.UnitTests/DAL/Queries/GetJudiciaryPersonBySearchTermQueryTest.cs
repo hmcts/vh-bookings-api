04197ab9cb7b42207cdd38ebc50cfa7d6fb3ac48
Valentina <Valentina.bennett@hmcts.net>
2021-03-27 19:37:26 +0000

    feature/VIH-7528 : unit test

--- BookingsApi/BookingsApi.UnitTests/DAL/Queries/GetJudiciaryPersonBySearchTermQueryTest.cs
+++ BookingsApi/BookingsApi.UnitTests/DAL/Queries/GetJudiciaryPersonBySearchTermQueryTest.cs
@@ -1,5 +1,8 @@
-﻿using BookingsApi.DAL.Queries;
+﻿using BookingsApi.DAL;
+using BookingsApi.DAL.Queries;
 using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Moq;
 using NUnit.Framework;
 
 namespace BookingsApi.UnitTests.DAL.Queries
@@ -12,6 +15,21 @@ namespace BookingsApi.UnitTests.DAL.Queries
             var query = new GetJudiciaryPersonBySearchTermQuery("Tester");
 
             query.Term.Should().Be("tester");
+        }
+
+        [Test]
+        public void Should_create_handler()
+        {
+            var connectionstring = "Connection string";
+
+            var optionsBuilder = new DbContextOptionsBuilder<BookingsDbContext>();
+            optionsBuilder.UseSqlServer(connectionstring);
+
+
+           var context = new BookingsDbContext(optionsBuilder.Options);
+
+            var handler = new GetJudiciaryPersonBySearchTermQueryHandler(context);
+            handler.Should().NotBeNull();
         }
     }
 }
