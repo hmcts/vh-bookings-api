b003ca9c53a09bd75abbd13b8c896d1ef71a0ceb
Valentina <Valentina.bennett@hmcts.net>
2021-03-27 12:53:18 +0000

    feature/VIH-7528 : added end point to search by email term judiciary person

---
+++ BookingsAPI/BookingsApi.UnitTests/Mappings/JudiciaryPersonToResponseMapperTest.cs
@@ -0,0 +1,26 @@
+﻿using BookingsApi.Mappings;
+using FluentAssertions;
+using NUnit.Framework;
+using Testing.Common.Builders.Domain;
+
+namespace BookingsApi.UnitTests.Mappings
+{
+    public class JudiciaryPersonToResponseMapperTest
+    {
+        private readonly JudiciaryPersonToResponseMapper _mapper = new JudiciaryPersonToResponseMapper();
+
+        [Test]
+        public void Should_map_to_response()
+        {
+            var judiciaryPerson = new JudiciaryPersonBuilder().Build();
+
+            var result = _mapper.MapJudiciaryPersonToResponse(judiciaryPerson);
+
+            result.FirstName.Should().Be(judiciaryPerson.KnownAs);
+            result.LastName.Should().Be(judiciaryPerson.Surname);
+            result.Title.Should().Be(judiciaryPerson.Title);
+            result.Username.Should().Be(judiciaryPerson.Email);
+            result.Id.Should().Be(judiciaryPerson.Id);
+        }
+    }
+}
--- BookingsApi/BookingsApi.Client/BookingsApiClient.cs
+++ BookingsApi/BookingsApi.Client/BookingsApiClient.cs
@@ -372,6 +372,19 @@ namespace BookingsApi.Client
         /// <exception cref="BookingsApiException">A server side error occurred.</exception>
         System.Threading.Tasks.Task<BulkJudiciaryPersonResponse> BulkJudiciaryPersonsAsync(System.Collections.Generic.IEnumerable<JudiciaryPersonRequest> request, System.Threading.CancellationToken cancellationToken);
     
+        /// <summary>Find persons with the email matching a search term.</summary>
+        /// <param name="term">Partial string to match email with, case-insensitive.</param>
+        /// <returns>Person list</returns>
+        /// <exception cref="BookingsApiException">A server side error occurred.</exception>
+        System.Threading.Tasks.Task<System.Collections.Generic.ICollection<PersonResponse>> PostJudiciaryPersonBySearchTermAsync(SearchTermRequest term);
+    
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <summary>Find persons with the email matching a search term.</summary>
+        /// <param name="term">Partial string to match email with, case-insensitive.</param>
+        /// <returns>Person list</returns>
+        /// <exception cref="BookingsApiException">A server side error occurred.</exception>
+        System.Threading.Tasks.Task<System.Collections.Generic.ICollection<PersonResponse>> PostJudiciaryPersonBySearchTermAsync(SearchTermRequest term, System.Threading.CancellationToken cancellationToken);
+    
         /// <summary>Get a participants by username</summary>
         /// <param name="username">The username of the participant</param>
         /// <returns>Participant</returns>
@@ -3125,6 +3138,100 @@ namespace BookingsApi.Client
                         if (status_ == 200)
                         {
                             var objectResponse_ = await ReadObjectResponseAsync<BulkJudiciaryPersonResponse>(response_, headers_, cancellationToken).ConfigureAwait(false);
+                            if (objectResponse_.Object == null)
+                            {
+                                throw new BookingsApiException("Response was null which was not expected.", status_, objectResponse_.Text, headers_, null);
+                            }
+                            return objectResponse_.Object;
+                        }
+                        else
+                        if (status_ == 400)
+                        {
+                            var objectResponse_ = await ReadObjectResponseAsync<ProblemDetails>(response_, headers_, cancellationToken).ConfigureAwait(false);
+                            if (objectResponse_.Object == null)
+                            {
+                                throw new BookingsApiException("Response was null which was not expected.", status_, objectResponse_.Text, headers_, null);
+                            }
+                            throw new BookingsApiException<ProblemDetails>("A server side error occurred.", status_, objectResponse_.Text, headers_, objectResponse_.Object, null);
+                        }
+                        else
+                        {
+                            var responseData_ = response_.Content == null ? null : await response_.Content.ReadAsStringAsync().ConfigureAwait(false);
+                            throw new BookingsApiException("The HTTP status code of the response was not expected (" + status_ + ").", status_, responseData_, headers_, null);
+                        }
+                    }
+                    finally
+                    {
+                        if (disposeResponse_)
+                            response_.Dispose();
+                    }
+                }
+            }
+            finally
+            {
+                if (disposeClient_)
+                    client_.Dispose();
+            }
+        }
+    
+        /// <summary>Find persons with the email matching a search term.</summary>
+        /// <param name="term">Partial string to match email with, case-insensitive.</param>
+        /// <returns>Person list</returns>
+        /// <exception cref="BookingsApiException">A server side error occurred.</exception>
+        public System.Threading.Tasks.Task<System.Collections.Generic.ICollection<PersonResponse>> PostJudiciaryPersonBySearchTermAsync(SearchTermRequest term)
+        {
+            return PostJudiciaryPersonBySearchTermAsync(term, System.Threading.CancellationToken.None);
+        }
+    
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <summary>Find persons with the email matching a search term.</summary>
+        /// <param name="term">Partial string to match email with, case-insensitive.</param>
+        /// <returns>Person list</returns>
+        /// <exception cref="BookingsApiException">A server side error occurred.</exception>
+        public async System.Threading.Tasks.Task<System.Collections.Generic.ICollection<PersonResponse>> PostJudiciaryPersonBySearchTermAsync(SearchTermRequest term, System.Threading.CancellationToken cancellationToken)
+        {
+            if (term == null)
+                throw new System.ArgumentNullException("term");
+    
+            var urlBuilder_ = new System.Text.StringBuilder();
+            urlBuilder_.Append(BaseUrl != null ? BaseUrl.TrimEnd('/') : "").Append("/judiciaryperson/search");
+    
+            var client_ = _httpClient;
+            var disposeClient_ = false;
+            try
+            {
+                using (var request_ = new System.Net.Http.HttpRequestMessage())
+                {
+                    var content_ = new System.Net.Http.StringContent(Newtonsoft.Json.JsonConvert.SerializeObject(term, _settings.Value));
+                    content_.Headers.ContentType = System.Net.Http.Headers.MediaTypeHeaderValue.Parse("application/json");
+                    request_.Content = content_;
+                    request_.Method = new System.Net.Http.HttpMethod("POST");
+                    request_.Headers.Accept.Add(System.Net.Http.Headers.MediaTypeWithQualityHeaderValue.Parse("application/json"));
+    
+                    PrepareRequest(client_, request_, urlBuilder_);
+    
+                    var url_ = urlBuilder_.ToString();
+                    request_.RequestUri = new System.Uri(url_, System.UriKind.RelativeOrAbsolute);
+    
+                    PrepareRequest(client_, request_, url_);
+    
+                    var response_ = await client_.SendAsync(request_, System.Net.Http.HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
+                    var disposeResponse_ = true;
+                    try
+                    {
+                        var headers_ = System.Linq.Enumerable.ToDictionary(response_.Headers, h_ => h_.Key, h_ => h_.Value);
+                        if (response_.Content != null && response_.Content.Headers != null)
+                        {
+                            foreach (var item_ in response_.Content.Headers)
+                                headers_[item_.Key] = item_.Value;
+                        }
+    
+                        ProcessResponse(client_, response_);
+    
+                        var status_ = (int)response_.StatusCode;
+                        if (status_ == 200)
+                        {
+                            var objectResponse_ = await ReadObjectResponseAsync<System.Collections.Generic.ICollection<PersonResponse>>(response_, headers_, cancellationToken).ConfigureAwait(false);
                             if (objectResponse_.Object == null)
                             {
                                 throw new BookingsApiException("Response was null which was not expected.", status_, objectResponse_.Text, headers_, null);
