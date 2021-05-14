0c236cb14812c65ed659cdf9c5467a00a1709bc6
Robert Bennett <Robert.Bennett@version1.com>
2021-05-14 17:47:55 +0100

    Added Scottish tribunal venues

---
+++ BookingsApi/BookingsApi.DAL/Migrations/20210514163641_Add_Scottish_Employment_Trib_Venues.cs
@@ -0,0 +1,32 @@
+﻿using BookingsApi.Domain;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookingsApi.DAL.Migrations
+{
+    public partial class Add_Scottish_Employment_Trib_Venues : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: nameof(HearingVenue),
+                columns: new[] {"Id", "Name"},
+                values: new object[,]
+                {
+                    {15, "Aberdeen Tribunal Hearing Centre"},
+                    {16, "Dundee Tribunal Hearing Centre"},
+                    {17, "Edinburgh Employment Tribunal"},
+                    {18, "Glasgow Tribunals Centre"},
+                    {19, "Inverness Employment Tribunal"},
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData("HearingVenue", "Id", 15);
+            migrationBuilder.DeleteData("HearingVenue", "Id", 16);
+            migrationBuilder.DeleteData("HearingVenue", "Id", 17);
+            migrationBuilder.DeleteData("HearingVenue", "Id", 18);
+            migrationBuilder.DeleteData("HearingVenue", "Id", 19);
+        }
+    }
+}
