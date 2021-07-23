8c49d13f4fc852636d5af30be3c517f1768ec9e3
PrasannaDommalapati <Prasanna.Dommalapati@gmail.com>
2021-07-23 12:12:30 +0100

    VIH-7978 - db update

--- BookingsApi/BookingsApi.DAL/Migrations/20210722142154_UpdateAccountTypeForCourtRoomAccounts.cs
+++ BookingsApi/BookingsApi.DAL/Migrations/20210722142154_UpdateAccountTypeForCourtRoomAccounts.cs
@@ -1,22 +1,29 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.Collections.Generic;
 
 namespace BookingsApi.DAL.Migrations
 {
     public partial class UpdateAccountTypeForCourtRoomAccounts : Migration
     {
+        private List<string> _judges;
+        public UpdateAccountTypeForCourtRoomAccounts()
+        {
+            _judges = UserApiWrapperClient.GetJudgesFromAdAsync().GetAwaiter().GetResult();
+        }
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            var judges = UserApiWrapperClient.GetJudgesFromAdAsync().GetAwaiter().GetResult();
-
-            foreach(var judge in judges)
+            _judges.ForEach(judge =>
             {
-               
-            }
+                migrationBuilder.Sql($"UPDATE [dbo].[Person] SET AccountType='Court Room' where UserName={judge}");
+            });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            _judges.ForEach(judge =>
+            {
+                migrationBuilder.Sql($"UPDATE [dbo].[Person] SET AccountType=NULL where UserName={judge}");
+            });
         }
     }
 }
--- BookingsApi/BookingsApi.DAL/Migrations/UserApiWrapperClient.cs
+++ BookingsApi/BookingsApi.DAL/Migrations/UserApiWrapperClient.cs
@@ -1,35 +1,54 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using BookingsApi.Common.Configuration;
+using Microsoft.ApplicationInsights;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using UserApi.Client;
 
 namespace BookingsApi.DAL.Migrations
 {
-    public class UserApiWrapperClient
+    public static class UserApiWrapperClient
     {
-        private static IUserApiClient _userApiClient;
-        private static ILogger _logger;
-
-        public UserApiWrapperClient(IUserApiClient userApiClient, ILogger logger)
-        {
-            _userApiClient = userApiClient;
-            _logger = logger;
-        }
-
-
         public static async Task<List<string>> GetJudgesFromAdAsync()
         {
             List<string> judges = new List<string>();
 
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .AddUserSecrets("D76B6EB8-F1A2-4A51-9B8F-21E1B6B81E4F");
+
+            var config = builder.Build();
+     
+            var instrumentationKey = config["ApplicationInsights:InstrumentationKey"];
+
+            var telemetryClient = new TelemetryClient() { InstrumentationKey = instrumentationKey };
+            
+
+            var azureAd = config.GetSection("AzureAd").Get<AzureAdConfiguration>();
+            var services = config.GetSection("Services").Get<ServicesConfiguration>();
+
+            var authContext = new AuthenticationContext($"{azureAd.Authority}{azureAd.TenantId}");
+            var credential = new ClientCredential(azureAd.ClientId, azureAd.ClientSecret);
+
+            var result = await authContext.AcquireTokenAsync(services.UserApiUrl, credential);
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {result.AccessToken}");
+
+            var userApiClient = new UserApiClient(client);
             try
             {
-
-            judges =  (await _userApiClient.GetJudgesAsync()).Select(x => x.Email).ToList();
+                judges =  (await userApiClient.GetJudgesAsync()).Select(x => x.Email).ToList();
             }catch(Exception ex)
             {
-                _logger.LogError($"Error in retrieving the judges from AD, Message: {ex.Message}");
+                telemetryClient.TrackException(ex);
             }
 
             return judges;
