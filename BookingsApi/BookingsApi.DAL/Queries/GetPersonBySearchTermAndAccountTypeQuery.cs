b5b00efc9c36963c2c6002b7cae835297ae1c1f0
muktadiru <muktadiru.ahmed@google.com>
2021-07-26 14:33:36 +0100

    added new query to search by string term and account type

---
+++ BookingsApi/BookingsApi.DAL/Queries/GetPersonBySearchTermAndAccountTypeQuery.cs
@@ -0,0 +1,54 @@
+﻿using BookingsApi.DAL.Queries.Core;
+using BookingsApi.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingsApi.DAL.Queries
+{
+    public class GetPersonBySearchTermAndAccountTypeQuery : IQuery
+    {
+        public string Term { get; }
+        public List<string> AccountType { get; }
+
+        public GetPersonBySearchTermAndAccountTypeQuery(string term, List<string> accountType = null)
+        {
+            Term = term.ToLowerInvariant();
+            AccountType = accountType;
+        }
+    }
+
+    public class GetPersonBySearchTermAndAccountTypeQueryHandler : IQueryHandler<GetPersonBySearchTermAndAccountTypeQuery, List<Person>>
+    {
+        private readonly BookingsDbContext _context;
+
+        public GetPersonBySearchTermAndAccountTypeQueryHandler(BookingsDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Person>> Handle(GetPersonBySearchTermAndAccountTypeQuery query)
+        {
+            List<Person> persons;
+            if (query.AccountType == null || query.AccountType.Count == 0)
+            {
+                persons = await _context.Persons
+                    .Include(x => x.Organisation)
+                    .Where(x => x.ContactEmail.ToLowerInvariant().Contains(query.Term.ToLowerInvariant()) && string.IsNullOrWhiteSpace(x.AccountType))
+                    .ToListAsync();
+            }
+            else
+            {
+                persons = await _context.Persons
+                    .Include(x => x.Organisation)
+                    .Where(x => x.ContactEmail.ToLowerInvariant().Contains(query.Term.ToLowerInvariant()) &&
+                                query.AccountType.Any(specifiedAccountType => specifiedAccountType == x.AccountType))
+                    .ToListAsync();
+            }
+            return persons;
+        }
+    }
+}
---
+++ BookingsApi/BookingsApi.IntegrationTests/Database/Queries/GetPersonBySearchTermAndAccountTypeQueryHandlerTests.cs
@@ -0,0 +1,92 @@
+﻿using BookingsApi.DAL;
+using BookingsApi.DAL.Queries;
+using BookingsApi.Domain;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingsApi.IntegrationTests.Database.Queries
+{
+    [TestFixture]
+    public class GetPersonBySearchTermAndAccountTypeQueryHandlerTests
+    {
+        private GetPersonBySearchTermAndAccountTypeQueryHandler _handler;
+        private BookingsDbContext _context;
+        private Person Person1, Person2, Person3;
+
+        [OneTimeSetUp]
+        public void InitialSetup()
+        {
+            var contextOptions = new DbContextOptionsBuilder<BookingsDbContext>().UseInMemoryDatabase(databaseName: "VhBookings").Options;
+            _context = new BookingsDbContext(contextOptions);
+        }
+
+        [OneTimeTearDown]
+        public void FinalCleanUp()
+        {
+            _context.Database.EnsureDeleted();
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            Person1 = new Person("mr", "luffy", "dragon", "luffy2@strawhat.net") { ContactEmail = "luffy2@strawhat.net" };
+            Person2 = new Person("mr", "zoro", "rononora", "zoro@strawhat.net") { ContactEmail = "zoro@strawhat.net" };
+            Person3 = new Person("mr", "luffy", "dragon", "luffy@strawhat.net") { ContactEmail = "luffy@strawhat.net" };
+            _context.Persons.AddRange(Person1, Person2, Person3);
+            _context.SaveChanges();
+            _handler = new GetPersonBySearchTermAndAccountTypeQueryHandler(_context);
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            _context.Persons.RemoveRange(Person1, Person2, Person3);
+        }
+
+        [Test]
+        public async Task Returns_Persons_Record_By_Search_Term()
+        {
+            var persons = await _handler.Handle(new GetPersonBySearchTermAndAccountTypeQuery("luff"));
+
+            Assert.AreEqual(2, persons.Count);
+            persons.Select(m => m.Id).Should().Contain(Person1.Id);
+            persons.Select(m => m.Id).Should().NotContain(Person2.Id);
+
+        }
+
+        [Test]
+        public async Task Returns_Persons_With_Empty_Account_Types_When_Unspecified()
+        {
+            Person1.AccountType = "SomeAccount";
+            _context.Persons.Update(Person1);
+            _context.SaveChanges();
+            _handler = new GetPersonBySearchTermAndAccountTypeQueryHandler(_context);
+
+            var persons = await _handler.Handle(new GetPersonBySearchTermAndAccountTypeQuery("luff"));
+
+            Assert.AreEqual(1, persons.Count);
+            persons.Select(m => m.Id).Should().NotContain(Person1.Id);
+        }
+
+        [Test]
+        public async Task Returns_Persons_With_Specified_Account_Type()
+        {
+            var specifiedAccountType = "SomeAccount";
+            Person1.AccountType = specifiedAccountType;
+            _context.Persons.Update(Person1);
+            _context.SaveChanges();
+            _handler = new GetPersonBySearchTermAndAccountTypeQueryHandler(_context);
+
+            var persons = await _handler.Handle(new GetPersonBySearchTermAndAccountTypeQuery("luff", new List<string> { specifiedAccountType }));
+
+            Assert.AreEqual(1, persons.Count);
+            persons.Select(m => m.Id).Should().Contain(Person1.Id);
+            persons.Select(m => m.Id).Should().NotContain(Person2.Id);
+            persons.Select(m => m.Id).Should().NotContain(Person3.Id);
+        }
+    }
+}
