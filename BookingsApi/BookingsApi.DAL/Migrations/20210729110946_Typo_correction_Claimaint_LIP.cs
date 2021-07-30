f131fa9d081515426779bed894e24c449e902994
Khizar Khan <khizarkhan.hmcts@gmail.com>
2021-07-30 10:45:33 +0100

    VIH-8003 Correcting Typo of Claimaint LIP to Claimant LIP - Hearing Role in database migration

---
+++ BookingsApi/BookingsApi.DAL/Migrations/20210729110946_Typo_correction_Claimaint_LIP.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookingsApi.DAL.Migrations
+{
+    public partial class Typo_correction_Claimaint_LIP : Migration
+    {
+        protected void UpdateTypoHearingRole(MigrationBuilder builder, string oldValue, string newValue)
+        {
+            builder.Sql($"UPDATE HearingRole set [Name] = { newValue } where [Name] = { oldValue } ");
+        }
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            string oldValue = "'Claimaint LIP'";
+            string newvalue = "'Claimant LIP'";
+
+            UpdateTypoHearingRole(migrationBuilder, oldValue, newvalue);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            string oldValue = "'Claimant LIP'";
+            string newvalue = "'Claimaint LIP'";
+
+            UpdateTypoHearingRole(migrationBuilder, oldValue, newvalue);
+        }
+    }
+}
