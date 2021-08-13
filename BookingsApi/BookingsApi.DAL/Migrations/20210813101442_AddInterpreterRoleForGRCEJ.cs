2e26dfab4a31621d4e180e07c23ad46239304ae4
muktadiru <muktadiru.ahmed@google.com>
2021-08-13 12:15:14 +0100

    added missing role for grc ej case type

---
+++ BookingsApi/BookingsApi.DAL/Migrations/20210813101442_AddInterpreterRoleForGRCEJ.cs
@@ -0,0 +1,29 @@
+﻿using BookingsApi.Domain.RefData;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookingsApi.DAL.Migrations
+{
+    public partial class AddInterpreterRoleForGRCEJ : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: nameof(HearingRole),
+                columns: new[] { "Id", "Name", "UserRoleId", "CaseRoleId", },
+                values: new object[,]
+                {
+                    {756, "Interpreter", 5, 203},
+                    {757, "Interpreter", 5, 204},
+                    {758, "Interpreter", 5, 208},
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            for (int hearingRoleId = 756; hearingRoleId <= 758; hearingRoleId++)
+            {
+                migrationBuilder.DeleteData("HearingRole", "Id", hearingRoleId);
+            }
+        }
+    }
+}
