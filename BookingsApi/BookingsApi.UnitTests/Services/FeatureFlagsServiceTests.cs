b49bbe290d3d2c72dc9956d951dd1af703bdc959
Prasanna Dommalapati <prasanna.dommalapati@gmail.com>
2021-09-23 13:55:07 +0100

    added extra tests

--- BookingsApi/BookingsApi.UnitTests/Services/FeatureFlagsServiceTests.cs
+++ BookingsApi/BookingsApi.UnitTests/Services/FeatureFlagsServiceTests.cs
@@ -17,34 +17,69 @@ namespace BookingsApi.UnitTests.Services
         public void SetUp()
         {
             _mocker = AutoMock.GetLoose();
-            _mocker.Mock<IOptions<FeatureFlagConfiguration>>().Setup(opt => opt.Value).Returns(new FeatureFlagConfiguration()
-            {
-                StaffMemberFeature = true,
-                EJudFeature = false
-            });
-
-            _service = _mocker.Create<FeatureFlagService>();
         }
 
         [Test]
         public void GetFeatureFlag_Should_Return_True_for_StaffMember_Feature()
         {
-            var staffMemberFeature = _service.GetFeatureFlag(nameof(FeatureFlags.StaffMemberFeature));
+            _mocker.Mock<IOptions<FeatureFlagConfiguration>>().Setup(opt => opt.Value).Returns(new FeatureFlagConfiguration()
+            {
+                StaffMemberFeature = true
+            });
+            _service = _mocker.Create<FeatureFlagService>();
 
-            staffMemberFeature.Should().BeTrue();
+            var featureFlag = _service.GetFeatureFlag(nameof(FeatureFlags.StaffMemberFeature));
+
+            featureFlag.Should().BeTrue();
+        }
+        
+        [Test]
+        public void GetFeatureFlag_Should_Return_False_for_StaffMember_Feature()
+        {
+            _mocker.Mock<IOptions<FeatureFlagConfiguration>>().Setup(opt => opt.Value).Returns(new FeatureFlagConfiguration()
+            {
+                StaffMemberFeature = false
+            });
+            _service = _mocker.Create<FeatureFlagService>();
+
+            var featureFlag = _service.GetFeatureFlag(nameof(FeatureFlags.StaffMemberFeature));
+
+            featureFlag.Should().BeFalse();
         }
 
         [Test]
         public void GetFeatureFlag_Should_Return_True_for_EJud_Feature()
         {
-            var staffMemberFeature = _service.GetFeatureFlag(nameof(FeatureFlags.EJudFeature));
+            _mocker.Mock<IOptions<FeatureFlagConfiguration>>().Setup(opt => opt.Value).Returns(new FeatureFlagConfiguration()
+            {
+                EJudFeature = true
+            });
+            _service = _mocker.Create<FeatureFlagService>();
 
-            staffMemberFeature.Should().BeFalse();
+            var featureFlag = _service.GetFeatureFlag(nameof(FeatureFlags.EJudFeature));
+
+            featureFlag.Should().BeTrue();
+        }
+        
+        [Test]
+        public void GetFeatureFlag_Should_Return_False_for_EJud_Feature()
+        {
+            _mocker.Mock<IOptions<FeatureFlagConfiguration>>().Setup(opt => opt.Value).Returns(new FeatureFlagConfiguration()
+            {
+                EJudFeature = false
+            });
+            _service = _mocker.Create<FeatureFlagService>();
+
+            var featureFlag = _service.GetFeatureFlag(nameof(FeatureFlags.EJudFeature));
+
+            featureFlag.Should().BeFalse();
         }
 
         [Test]
         public void GetFeatureFlag_Should_Throw_FeatureFlagNotFoundException()
         {
+            _service = _mocker.Create<FeatureFlagService>();
+
             Assert.Throws<FeatureFlagNotFoundException>(() => _service.GetFeatureFlag("TestFeature"));
         }
     }
