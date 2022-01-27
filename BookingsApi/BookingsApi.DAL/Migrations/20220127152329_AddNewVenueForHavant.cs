a603b783ba8f1860f784f1d65e5d112e9e1670db
Prasanna Dommalapati <prasanna.dommalapati@gmail.com>
2022-01-27 15:44:36 +0000

    VIH-8680 Added new venue for Havant

---
+++ BookingsApi/BookingsApi.DAL/Migrations/20220127152329_AddNewVenueForHavant.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using BookingsApi.Domain;
+
+namespace BookingsApi.DAL.Migrations
+{
+    public partial class AddNewVenueForHavant : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: nameof(HearingVenue),
+                columns: new[] {"Id", "Name"},
+                values: new object[,]
+                {
+                    {22, "Havant Justice Centre"}
+                });
+         }
+        
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData("HearingVenue", "Id", 22);
+        }
+    }
+}
