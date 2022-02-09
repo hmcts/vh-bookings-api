2cfc2da5ee4afb5dce504c058c73d6560d179c30
Anaia <Lugo41@hotmail.com>
2022-02-09 10:39:02 +0000

    VIH-8653 - Added new venues for Mental Health Tribs (#352)

---
+++ BookingsApi/BookingsApi.DAL/Migrations/20220208174128_AddNewVenuesForMentalHealthTribs.cs
@@ -0,0 +1,30 @@
+﻿using BookingsApi.Domain;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookingsApi.DAL.Migrations
+{
+    public partial class AddNewVenuesForMentalHealthTribs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+               table: nameof(HearingVenue),
+               columns: new[] { "Id", "Name" },
+               values: new object[,]
+               {
+                    {24, "Mary Seacole House - Birmingham"},
+                    {25, "Goodmayes Hospital - Essex"},
+                    {26, "Huntercombe Roehampton Hospital"},
+                    {27, "Ardenleigh - Birmingham"}
+               });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            for (int hearingVenueId = 24; hearingVenueId <= 27; hearingVenueId++)
+            {
+                migrationBuilder.DeleteData(nameof(HearingVenue), "Id", hearingVenueId);
+            }
+        }
+    }
+}
