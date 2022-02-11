1fddd3a5fa1ea5dbdcb63c1fea97ce393fd6b462
Will Craig <william.craig2@justice.gov.uk>
2022-02-11 16:42:29 +0000

    Amended Representative and Solicitor Hearing Roles to have correct UserRoleID (6, Representative)

---
+++ BookingsApi/BookingsApi.DAL/Migrations/20220211154029_UpdateHearingRoleUserRoleID.cs
@@ -0,0 +1,41 @@
+﻿using BookingsApi.DAL.Helper;
+using BookingsApi.Domain.RefData;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookingsApi.DAL.Migrations
+{
+    public partial class UpdateHearingRoleUserRoleID : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: nameof(HearingRole),
+                keyColumn: "Id",
+                keyValue: 857,
+                column: "UserRoleId",
+                value: UserRoleForHearingRole.UserRoleId["Representative"]);
+            migrationBuilder.UpdateData(
+                table: nameof(HearingRole),
+                keyColumn: "Id",
+                keyValue: 858,
+                column: "UserRoleId",
+                value: UserRoleForHearingRole.UserRoleId["Representative"]);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: nameof(HearingRole),
+                keyColumn: "Id",
+                keyValue: 857,
+                column: "UserRoleId",
+                value: 5);
+            migrationBuilder.UpdateData(
+                table: nameof(HearingRole),
+                keyColumn: "Id",
+                keyValue: 858,
+                column: "UserRoleId",
+                value: 5);
+        }
+    }
+}
