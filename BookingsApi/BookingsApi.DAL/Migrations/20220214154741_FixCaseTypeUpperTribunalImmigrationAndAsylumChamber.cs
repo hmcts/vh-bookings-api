3c055ec12cc0a7b77b3cae6360678836037ab772
Khizar Khan <khizarkhan.hmcts@gmail.com>
2022-02-14 16:22:39 +0000

    VIH-7903 - Fixed case role group for panel member and judge for Upper Tribunal Immigration & Asylum Chamber

---
+++ BookingsApi/BookingsApi.DAL/Migrations/20220214154741_FixCaseTypeUpperTribunalImmigrationAndAsylumChamber.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookingsApi.DAL.Migrations
+{
+    public partial class FixCaseTypeUpperTribunalImmigrationAndAsylumChamber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("update [dbo].[CaseRole] set [Group] = 0 where [Id] = 285 and CaseTypeId = 45 ");  // Judge case role is 0
+            migrationBuilder.Sql("update [dbo].[CaseRole] set [Group] = 6 where [Id] = 286 and CaseTypeId = 45 ");   // Panel Member case role is 6
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("update [dbo].[CaseRole] set [Group] = 0 where [Id] = 285 and CaseTypeId = 45 ");  // Judge case role is 0
+            migrationBuilder.Sql("update [dbo].[CaseRole] set [Group] = 6 where [Id] = 286 and CaseTypeId = 45 ");   // Panel Member case role is 6
+        }
+    }
+}
