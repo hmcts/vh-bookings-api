0da5424eee927543dac1eab48c4e16956a57c152
krishnapottekkat-hmcts <Krishna.pottekkat@hmcts.net>
2022-02-14 10:23:10 +0000

    VIH-8751 Add New Venue - Hatton Cross Tribunal

---
+++ BookingsApi/BookingsApi.DAL/Migrations/20220214092837_AddNewVenueHattonCrossTribunal.cs
@@ -0,0 +1,24 @@
+﻿using BookingsApi.Domain;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookingsApi.DAL.Migrations
+{
+    public partial class AddNewVenueHattonCrossTribunal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+               table: nameof(HearingVenue),
+               columns: new[] { "Id", "Name" },
+               values: new object[,]
+               {
+                    {29, "Hatton Cross Tribunal Hearing Centre"}
+               });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData("HearingVenue", "Id", 29);
+        }
+    }
+}
