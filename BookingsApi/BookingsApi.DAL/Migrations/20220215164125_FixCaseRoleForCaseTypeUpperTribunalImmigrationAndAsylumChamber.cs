cbd416c623a103f81a737c48647a37ef7544be11
Khizar Khan <khizarkhan.hmcts@gmail.com>
2022-02-15 16:57:06 +0000

    VIH-7903 fixed case type role for secretary of state case role group clashing with staff member.

---
+++ BookingsApi/BookingsApi.DAL/Migrations/20220215164125_FixCaseRoleForCaseTypeUpperTribunalImmigrationAndAsylumChamber.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookingsApi.DAL.Migrations
+{
+    public partial class FixCaseRoleForCaseTypeUpperTribunalImmigrationAndAsylumChamber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("update [dbo].[CaseRole] set [Group] = 13 where [Id] = 287 and CaseTypeId = 45 ");  // Secretary of State case role is 13
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("update [dbo].[CaseRole] set [Group] = 13 where [Id] = 287 and CaseTypeId = 45 ");  // Secretary of State case role is 13
+        }
+    }
+}
