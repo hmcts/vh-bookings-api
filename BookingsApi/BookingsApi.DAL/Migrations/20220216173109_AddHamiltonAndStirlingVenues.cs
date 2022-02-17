ce3f643aeb2f9437260c0ec7a99b3ac02f8ff3c2
Arif Ahmed <arifahmed2009@hotmail.co.uk>
2022-02-17 10:19:34 +0000

    Added 2 Scottish Venues

---
+++ BookingsApi/BookingsApi.DAL/Migrations/20220216173109_AddHamiltonAndStirlingVenues.cs
@@ -0,0 +1,26 @@
+﻿using BookingsApi.Domain;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookingsApi.DAL.Migrations
+{
+    public partial class AddHamiltonAndStirlingVenues : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+            table: nameof(HearingVenue),
+            columns: new[] { "Id", "Name" },
+            values: new object[,]
+            {
+                {30, "Hamilton Brandon Gate"},
+                {31, "Stirling Wallace House"}
+            });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData("HearingVenue", "Id", 30);
+            migrationBuilder.DeleteData("HearingVenue", "Id", 31);
+        }
+    }
+}
