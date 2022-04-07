b625dda5ad76d84bd4975a2679787b1ed513bae4
marcogagliardi <marco.gagliardi@justice.gov.uk>
2022-04-07 16:29:21 +0100

    VIH-8804 - ef fix

--- BookingsApi/BookingsApi.DAL/Migrations/20220407104039_AddSENDCaseType.cs
+++ BookingsApi/BookingsApi.DAL/Migrations/20220407104039_AddSENDCaseType.cs
@@ -32,7 +32,7 @@ namespace BookingsApi.DAL.Migrations
                     { 321, "Local Authority", (int) CaseRoleGroup.LocalAuthority, 51 },
                     { 322, "Observer", (int) CaseRoleGroup.Observer, 51 },
                     { 323, "Panel Member", (int) CaseRoleGroup.PanelMember, 51 },
-                    { 324, "Judge", 0, 51 }
+                    { 324, "Judge",  (int) CaseRoleGroup.Judge, 51 }
                 });
 
             migrationBuilder.InsertData(
@@ -40,21 +40,21 @@ namespace BookingsApi.DAL.Migrations
                 new[] { "Id", "Name", "UserRoleId", "CaseRoleId", "Live"},
                 new object[,]
                 {
-                    { 506, "Appellant", 5, 320,true },
-                    { 507, "Expert", 5, 320,true },
-                    { 508, "Intermediary", 5, 320,true },
-                    { 509, "Interpreter", 5, 320,true },
-                    { 510, "Litigant In Person", 5, 320,true },
-                    { 511, "Litigation Friend", 5, 320,true },
-                    { 512, "MacKenzie Friend", 5, 320,true },
-                    { 513, "Representative", 6, 320,true },
-                    { 514, "Solicitor", 6, 320,true },
-                    { 515, "Witness", 5, 320,true },
-                    { 516, "Representative", 6, 321,true },
-                    { 517, "Witness", 5, 321,true },
-                    { 518, "Observer", 5, 322,true },
-                    { 519, "Panel Member", 7, 323,true },
-                    { 520, "Judge", 4, 324,true }
+                    { 1013, "Appellant", 5, 320,true },
+                    { 1014, "Expert", 5, 320,true },
+                    { 1015, "Intermediary", 5, 320,true },
+                    { 1016, "Interpreter", 5, 320,true },
+                    { 1017, "Litigant In Person", 5, 320,true },
+                    { 1018, "Litigation Friend", 5, 320,true },
+                    { 1019, "MacKenzie Friend", 5, 320,true },
+                    { 1020, "Representative", 6, 320,true },
+                    { 1021, "Solicitor", 6, 320,true },
+                    { 1022, "Witness", 5, 320,true },
+                    { 1023, "Representative", 6, 321,true },
+                    { 1024, "Witness", 5, 321,true },
+                    { 1025, "Observer", 5, 322,true },
+                    { 1026, "Panel Member", 7, 323,true },
+                    { 1027, "Judge", 4, 324,true }
                 });
         }
 
