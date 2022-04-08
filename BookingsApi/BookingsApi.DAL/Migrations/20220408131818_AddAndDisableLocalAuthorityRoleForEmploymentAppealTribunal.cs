b089a3fb62f88de1cf29e26f7e8a3d2d75a3e9f5
marcogagliardi <marco.gagliardi@justice.gov.uk>
2022-04-08 14:44:56 +0100

    VIH-8891 - added and disabled Local Authority

---
+++ BookingsApi/BookingsApi.DAL/Migrations/20220408131818_AddAndDisableLocalAuthorityRoleForEmploymentAppealTribunal.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookingsApi.DAL.Migrations
+{
+    public partial class AddAndDisableLocalAuthorityRoleForEmploymentAppealTribunal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                "HearingRole",
+                new[] { "Id", "Name", "UserRoleId", "CaseRoleId", "Live" },
+                new object[,]
+                {
+                    {936, "Local Authority", 5, 295, false},
+                    {922, "Local Authority", 5, 294, false}
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData("HearingRole", "Id", 922);
+            migrationBuilder.DeleteData("HearingRole", "Id", 936);
+        }
+    }
+}
