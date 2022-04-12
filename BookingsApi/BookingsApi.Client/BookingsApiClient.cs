a013152134fc81703f23c063509736ba6406f0df
Khizar Khan <84509291+khizarkhanu-hmcts@users.noreply.github.com>
2022-04-12 11:02:04 +0100

    VIH-7318 Notify hearing reminder email changes (#410)
    * VIH-7318 Noify hearing reminder email changes
    
    * Test fixes.

--- BookingsApi/BookingsApi.Client/BookingsApiClient.cs
+++ BookingsApi/BookingsApi.Client/BookingsApiClient.cs
@@ -344,6 +344,17 @@ namespace BookingsApi.Client
         /// <exception cref="BookingsApiException">A server side error occurred.</exception>
         System.Threading.Tasks.Task<System.Collections.Generic.ICollection<HearingDetailsResponse>> GetHearingsByGroupIdAsync(System.Guid groupId, System.Threading.CancellationToken cancellationToken);
     
+        /// <summary>Get list of all hearings for notification between next 48 to 72 hrs.</summary>
+        /// <returns>Hearing details</returns>
+        /// <exception cref="BookingsApiException">A server side error occurred.</exception>
+        System.Threading.Tasks.Task<System.Collections.Generic.ICollection<HearingDetailsResponse>> GetHearingsForNotificationAsync();
+    
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <summary>Get list of all hearings for notification between next 48 to 72 hrs.</summary>
+        /// <returns>Hearing details</returns>
+        /// <exception cref="BookingsApiException">A server side error occurred.</exception>
+        System.Threading.Tasks.Task<System.Collections.Generic.ICollection<HearingDetailsResponse>> GetHearingsForNotificationAsync(System.Threading.CancellationToken cancellationToken);
+    
         /// <summary>Create a new hearing with the details of a given hearing on given dates</summary>
         /// <param name="hearingId">Original hearing to clone</param>
         /// <param name="request">List of dates to create a new hearing on</param>
@@ -2926,6 +2937,82 @@ namespace BookingsApi.Client
             var urlBuilder_ = new System.Text.StringBuilder();
             urlBuilder_.Append(BaseUrl != null ? BaseUrl.TrimEnd('/') : "").Append("/hearings/{groupId}/hearings");
             urlBuilder_.Replace("{groupId}", System.Uri.EscapeDataString(ConvertToString(groupId, System.Globalization.CultureInfo.InvariantCulture)));
+    
+            var client_ = _httpClient;
+            var disposeClient_ = false;
+            try
+            {
+                using (var request_ = new System.Net.Http.HttpRequestMessage())
+                {
+                    request_.Method = new System.Net.Http.HttpMethod("GET");
+                    request_.Headers.Accept.Add(System.Net.Http.Headers.MediaTypeWithQualityHeaderValue.Parse("application/json"));
+    
+                    PrepareRequest(client_, request_, urlBuilder_);
+    
+                    var url_ = urlBuilder_.ToString();
+                    request_.RequestUri = new System.Uri(url_, System.UriKind.RelativeOrAbsolute);
+    
+                    PrepareRequest(client_, request_, url_);
+    
+                    var response_ = await client_.SendAsync(request_, System.Net.Http.HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
+                    var disposeResponse_ = true;
+                    try
+                    {
+                        var headers_ = System.Linq.Enumerable.ToDictionary(response_.Headers, h_ => h_.Key, h_ => h_.Value);
+                        if (response_.Content != null && response_.Content.Headers != null)
+                        {
+                            foreach (var item_ in response_.Content.Headers)
+                                headers_[item_.Key] = item_.Value;
+                        }
+    
+                        ProcessResponse(client_, response_);
+    
+                        var status_ = (int)response_.StatusCode;
+                        if (status_ == 200)
+                        {
+                            var objectResponse_ = await ReadObjectResponseAsync<System.Collections.Generic.ICollection<HearingDetailsResponse>>(response_, headers_, cancellationToken).ConfigureAwait(false);
+                            if (objectResponse_.Object == null)
+                            {
+                                throw new BookingsApiException("Response was null which was not expected.", status_, objectResponse_.Text, headers_, null);
+                            }
+                            return objectResponse_.Object;
+                        }
+                        else
+                        {
+                            var responseData_ = response_.Content == null ? null : await response_.Content.ReadAsStringAsync().ConfigureAwait(false);
+                            throw new BookingsApiException("The HTTP status code of the response was not expected (" + status_ + ").", status_, responseData_, headers_, null);
+                        }
+                    }
+                    finally
+                    {
+                        if (disposeResponse_)
+                            response_.Dispose();
+                    }
+                }
+            }
+            finally
+            {
+                if (disposeClient_)
+                    client_.Dispose();
+            }
+        }
+    
+        /// <summary>Get list of all hearings for notification between next 48 to 72 hrs.</summary>
+        /// <returns>Hearing details</returns>
+        /// <exception cref="BookingsApiException">A server side error occurred.</exception>
+        public System.Threading.Tasks.Task<System.Collections.Generic.ICollection<HearingDetailsResponse>> GetHearingsForNotificationAsync()
+        {
+            return GetHearingsForNotificationAsync(System.Threading.CancellationToken.None);
+        }
+    
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <summary>Get list of all hearings for notification between next 48 to 72 hrs.</summary>
+        /// <returns>Hearing details</returns>
+        /// <exception cref="BookingsApiException">A server side error occurred.</exception>
+        public async System.Threading.Tasks.Task<System.Collections.Generic.ICollection<HearingDetailsResponse>> GetHearingsForNotificationAsync(System.Threading.CancellationToken cancellationToken)
+        {
+            var urlBuilder_ = new System.Text.StringBuilder();
+            urlBuilder_.Append(BaseUrl != null ? BaseUrl.TrimEnd('/') : "").Append("/hearings/notifications/gethearings");
     
             var client_ = _httpClient;
             var disposeClient_ = false;
---
+++ BookingsApi/BookingsApi.IntegrationTests/Database/Queries/GetHearingsForNotificationsQueryHandlerTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookingsApi.DAL;
+using BookingsApi.DAL.Queries;
+using BookingsApi.Domain;
+using BookingsApi.Domain.Enumerations;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace BookingsApi.IntegrationTests.Database.Queries
+{
+    public class GetHearingsForNotificationsQueryHandlerTests : DatabaseTestsBase
+    {
+        private GetHearingsForNotificationsQueryHandler _handler;
+        private GetHearingsByGroupIdQueryHandler _groupByHandler;
+
+        [SetUp]
+        public void Setup()
+        {
+            var context = new BookingsDbContext(BookingsDbContextOptions);
+            _handler = new GetHearingsForNotificationsQueryHandler(context);
+            _groupByHandler = new GetHearingsByGroupIdQueryHandler(context);
+        }
+
+        [Test]
+        public async Task Shoud_return_hearings_between_48_to_72_hrs_for_notifications()
+        {
+            var hearing1 = await Hooks.SeedPastVideoHearing(DateTime.Today.AddDays(2), null, false, BookingStatus.Created);
+            var hearing2 = await Hooks.SeedPastVideoHearing(DateTime.Today.AddDays(2), null, false, BookingStatus.Created);
+            var hearing3 = await Hooks.SeedPastVideoHearing(DateTime.Today.AddDays(3), null, false, BookingStatus.Created);
+
+            var query = new GetHearingsForNotificationsQuery();
+
+            var result = await _handler.Handle(query);
+
+            var resulthearing1 = result.FirstOrDefault(x => x.Id == hearing1.Id);
+            var resulthearing2 = result.FirstOrDefault(x => x.Id == hearing2.Id);
+            var resulthearing3 = result.FirstOrDefault(x => x.Id == hearing3.Id);
+
+            resulthearing1.Should().NotBeNull();
+            resulthearing2.Should().NotBeNull();
+            resulthearing3.Should().BeNull();
+
+        }
+
+
+        [Test]
+        public async Task Should_not_return_multiday_hearings_between_48_to_72_hrs_for_notifications()
+        {
+            var hearing1 = await Hooks.SeedPastVideoHearing(DateTime.Today.AddDays(2), null, false, BookingStatus.Created);
+
+            var groupId = hearing1.SourceId;
+
+            var dates = new List<DateTime> { DateTime.Now.AddDays(2), DateTime.Now.AddDays(3) };
+            await Hooks.CloneVideoHearing(hearing1.Id, dates);
+
+            var groupQuery = new GetHearingsByGroupIdQuery(groupId.Value);
+            var groupResults = await _groupByHandler.Handle(groupQuery);
+
+            var query = new GetHearingsForNotificationsQuery();
+            var result = await _handler.Handle(query);
+
+            var returnedHearing1 = groupResults[0];
+            var returnedHearing2 = groupResults[1];
+            var returnedHearing3 = groupResults[2];
+
+            var resulthearing1 = result.FirstOrDefault(x => x.Id == returnedHearing1.Id);
+            var resulthearing2 = result.FirstOrDefault(x => x.Id == returnedHearing2.Id);
+            var resulthearing3 = result.FirstOrDefault(x => x.Id == returnedHearing3.Id);
+
+            resulthearing1.Should().NotBeNull();
+            resulthearing2.Should().BeNull();
+            resulthearing3.Should().BeNull();
+
+        }
+    }
+}
