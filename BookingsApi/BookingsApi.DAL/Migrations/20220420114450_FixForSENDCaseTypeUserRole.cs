dfb1ef350067a979c73370410cc3511998348ab2
marcogagliardi <marco.gagliardi@justice.gov.uk>
2022-04-21 09:49:15 +0100

    VIH-8804 - amended down migration

--- BookingsApi/BookingsApi.DAL/Migrations/20220420114450_FixForSENDCaseTypeUserRole.cs
+++ BookingsApi/BookingsApi.DAL/Migrations/20220420114450_FixForSENDCaseTypeUserRole.cs
@@ -44,28 +44,36 @@ namespace BookingsApi.DAL.Migrations
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            var sqlScript = @"
-                declare @caseTypeId int
-                -- get CaseTypeId
+            var sqlScript = $@"
+                declare @caseTypeId int;
                 select @caseTypeId = Id from CaseType
-                where Name = 'Special Educational Needs and Disability'
+                where Name = 'Special Educational Needs and Disability';
 
-                declare @caseRoleIds table
-                (
-                    Value int
-                )
-                -- get CaseRoleIds
-                insert into @caseRoleIds select Id from CaseRole where CaseTypeId = @caseTypeId
-                -- delete all HearingRoles connected to CaseRoleIds
-                delete from HearingRole where CaseRoleId in (select value from @caseRoleIds)
+                declare @caseRoleId1 int;
+                select @caseRoleId1 = Id from CaseRole
+                where CaseTypeId = @caseTypeId and Name = 'Appellant';
 
-                -- delete all HearingType connected to CaseTypeId
-                delete from HearingType where CaseTypeId = @caseTypeId
+                declare @caseRoleId2 int;
+                select @caseRoleId2 = Id from CaseRole
+                where CaseTypeId = @caseTypeId and Name = 'Local Authority'
 
-                -- delete all CaseRole connected to CaseTypeId
-                delete from CaseRole where CaseTypeId = @caseTypeId
+                declare @hearingRoleId1 int;
+                select @hearingRoleId1 = Id from HearingRole
+                where Name = 'MacKenzie Friend' and CaseRoleId = @caseRoleId1
 
-                DELETE FROM CaseType WHERE Id = @caseTypeId
+                declare @hearingRoleId2 int;
+                select @hearingRoleId2 = Id from HearingRole
+                where Name = 'Witness' and CaseRoleId = @caseRoleId1
+
+                declare @hearingRoleId3 int;
+                select @hearingRoleId3 = Id from HearingRole
+                where Name = 'Witness' and CaseRoleId = @caseRoleId2
+
+                declare @userRoleId int;
+                select @userRoleId = Id from UserRole where Name = 'Representative';
+
+                update HearingRole set UserRoleId = @userRoleId
+                where Id in (@hearingRoleId1, @hearingRoleId2, @hearingRoleId3);
                 ";
             
             migrationBuilder.Sql(sqlScript);
