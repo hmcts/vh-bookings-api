f39ccfdf517e2b11fa08f39e65a9c6e16caba4d8
marcogagliardi <marco.gagliardi@justice.gov.uk>
2022-04-26 10:10:54 +0100

    VIH-8925 - Incorrect User Role For Respondent In Criminal Injuries Compensation (#412)
    * VIH-8925 - Fix for UserRole
    
    * VIH-8925 - sql script fix
    
    * VIH-8925 - fixed down method

---
+++ BookingsApi/BookingsApi.DAL/Migrations/20220421073350_FixUserRoleForRespondentInCriminalInjuriesCompensation.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookingsApi.DAL.Migrations
+{
+    public partial class FixUserRoleForRespondentInCriminalInjuriesCompensation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sqlScript = @"
+                declare @caseTypeId int;
+                -- get CaseTypeId
+                select @caseTypeId = Id from CaseType
+                where Name = 'Criminal Injuries Compensation';
+
+                declare @caseRoleId int;
+                select @caseRoleId = Id from CaseRole
+                where CaseTypeId = @caseTypeId and Name = 'None';
+
+                declare @hearingRoleId int;
+                select @hearingRoleId = Id from HearingRole
+                where Name = 'Respondent' and CaseRoleId = @caseRoleId;
+                
+                declare @UserRoleId1 int;
+                select @UserRoleId1 = Id from UserRole where Name = 'Individual';
+                
+                update HearingRole set UserRoleId = @UserRoleId1  where Id = @hearingRoleId;
+            ";
+            
+            migrationBuilder.Sql(sqlScript);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            var sqlScript = @"
+                declare @caseTypeId int;
+                -- get CaseTypeId
+                select @caseTypeId = Id from CaseType
+                where Name = 'Criminal Injuries Compensation';
+
+                declare @caseRoleId int;
+                select @caseRoleId = Id from CaseRole
+                where CaseTypeId = @caseTypeId and Name = 'None';
+
+                declare @hearingRoleId int;
+                select @hearingRoleId = Id from HearingRole
+                where Name = 'Respondent' and CaseRoleId = @caseRoleId;
+                
+                declare @UserRoleId1 int;
+                select @UserRoleId1 = Id from UserRole where Name = 'Representative';
+                
+                update HearingRole set UserRoleId = @UserRoleId1  where Id = @hearingRoleId;
+                ";
+            
+            migrationBuilder.Sql(sqlScript);
+        }
+    }
+}
