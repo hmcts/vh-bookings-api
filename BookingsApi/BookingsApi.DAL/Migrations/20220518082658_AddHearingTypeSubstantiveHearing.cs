cc69325eace7727c7fba6bcbd7c7d3dc77a4f5cb
marcogagliardi <marco.gagliardi@justice.gov.uk>
2022-05-18 10:31:48 +0100

    VIH-8984 - Added Hearing type Substantive Hearing (#426)

---
+++ BookingsApi/BookingsApi.DAL/Migrations/20220518082658_AddHearingTypeSubstantiveHearing.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using BookingsApi.Contract.Enums;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookingsApi.DAL.Migrations
+{
+    public partial class AddHearingTypeSubstantiveHearing : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+var sqlScript = $@"
+            declare @caseTypeId int
+            -- get CaseTypeId
+            select @caseTypeId = Id from CaseType
+            where Name = 'Upper Tribunal Tax';
+            INSERT INTO HearingType (""Name"", ""CaseTypeId"", ""Live"",""CreatedDate"",""UpdatedDate"") VALUES ('Substantive Hearing',@caseTypeId,1,'{DateTime.UtcNow}','{DateTime.UtcNow}');";
+            
+            migrationBuilder.Sql(sqlScript);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            var sqlScript = @"
+                declare @caseTypeId int
+                -- get CaseTypeId
+                select @caseTypeId = Id from CaseType
+                where Name = 'Upper Tribunal Tax'
+
+                -- delete HearingType 
+                delete from HearingType where CaseTypeId = @caseTypeId and Name = 'Substantive Hearing'
+                ";
+            
+            migrationBuilder.Sql(sqlScript);
+        }
+    }
+}
