19589a9c2ef215f92fb47d77ccddd292260d823a
muktadiru <muktadiru.ahmed@gmail.com>
2022-06-16 11:27:07 +0100

    adding missing changes for ef core migration

--- BookingsApi/BookingsApi.Domain/JudiciaryPersonStaging.cs
+++ BookingsApi/BookingsApi.Domain/JudiciaryPersonStaging.cs
@@ -8,7 +8,7 @@ namespace BookingsApi.Domain
         private readonly DateTime _currentUTC = DateTime.UtcNow;
         
         public JudiciaryPersonStaging(string externalRefId, string personalCode, string title, string knownAs, string surname,
-            string fullname, string postNominals, string email, string hasLeft, string leaver, string leftOn)
+            string fullname, string postNominals, string email, string leaver, string leftOn)
         {
             Id = Guid.NewGuid();
             ExternalRefId = externalRefId;
@@ -21,23 +21,21 @@ namespace BookingsApi.Domain
             Email = email;
             CreatedDate = _currentUTC;
             UpdatedDate = _currentUTC;
-            HasLeft = hasLeft;
             Leaver = leaver;
             LeftOn = leftOn;
         }
-        
+
         public string ExternalRefId { get; set; }
         public string PersonalCode { get; set; }
         public string Title { get; set; }
         public string KnownAs { get; set; }
-        public string Surname { get; set; }
         public string Fullname { get; set; }
+        public string Surname { get; set; }
         public string PostNominals { get; set; }
         public string Email { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public DateTime UpdatedDate { get; set; }
-        public string HasLeft { get; set; }
         public string Leaver { get; set; }
         public string LeftOn { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
     }
 }
