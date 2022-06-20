ae111c12ad1e8affa308262239097d2c77717bbc
muktadiru <muktadiru.ahmed@gmail.com>
2022-06-20 10:19:04 +0100

    added new endpoint

--- BookingsApi/BookingsApi.Client/BookingsApiClient.cs
+++ BookingsApi/BookingsApi.Client/BookingsApiClient.cs
@@ -440,6 +440,13 @@ namespace BookingsApi.Client
         /// <exception cref="BookingsApiException">A server side error occurred.</exception>
         System.Threading.Tasks.Task RemoveAllJudiciaryPersonsStagingAsync(System.Threading.CancellationToken cancellationToken);
     
+        /// <exception cref="BookingsApiException">A server side error occurred.</exception>
+        System.Threading.Tasks.Task BulkJudiciaryPersonsStagingAsync(System.Collections.Generic.IEnumerable<JudiciaryPersonStagingRequest> request);
+    
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <exception cref="BookingsApiException">A server side error occurred.</exception>
+        System.Threading.Tasks.Task BulkJudiciaryPersonsStagingAsync(System.Collections.Generic.IEnumerable<JudiciaryPersonStagingRequest> request, System.Threading.CancellationToken cancellationToken);
+    
         /// <summary>Get a participants by username</summary>
         /// <param name="username">The username of the participant</param>
         /// <returns>Participant</returns>
@@ -3810,6 +3817,78 @@ namespace BookingsApi.Client
                 using (var request_ = new System.Net.Http.HttpRequestMessage())
                 {
                     request_.Method = new System.Net.Http.HttpMethod("GET");
+    
+                    PrepareRequest(client_, request_, urlBuilder_);
+    
+                    var url_ = urlBuilder_.ToString();
+                    request_.RequestUri = new System.Uri(url_, System.UriKind.RelativeOrAbsolute);
+    
+                    PrepareRequest(client_, request_, url_);
+    
+                    var response_ = await client_.SendAsync(request_, System.Net.Http.HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
+                    var disposeResponse_ = true;
+                    try
+                    {
+                        var headers_ = System.Linq.Enumerable.ToDictionary(response_.Headers, h_ => h_.Key, h_ => h_.Value);
+                        if (response_.Content != null && response_.Content.Headers != null)
+                        {
+                            foreach (var item_ in response_.Content.Headers)
+                                headers_[item_.Key] = item_.Value;
+                        }
+    
+                        ProcessResponse(client_, response_);
+    
+                        var status_ = (int)response_.StatusCode;
+                        if (status_ == 200)
+                        {
+                            return;
+                        }
+                        else
+                        {
+                            var responseData_ = response_.Content == null ? null : await response_.Content.ReadAsStringAsync().ConfigureAwait(false);
+                            throw new BookingsApiException("The HTTP status code of the response was not expected (" + status_ + ").", status_, responseData_, headers_, null);
+                        }
+                    }
+                    finally
+                    {
+                        if (disposeResponse_)
+                            response_.Dispose();
+                    }
+                }
+            }
+            finally
+            {
+                if (disposeClient_)
+                    client_.Dispose();
+            }
+        }
+    
+        /// <exception cref="BookingsApiException">A server side error occurred.</exception>
+        public System.Threading.Tasks.Task BulkJudiciaryPersonsStagingAsync(System.Collections.Generic.IEnumerable<JudiciaryPersonStagingRequest> request)
+        {
+            return BulkJudiciaryPersonsStagingAsync(request, System.Threading.CancellationToken.None);
+        }
+    
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <exception cref="BookingsApiException">A server side error occurred.</exception>
+        public async System.Threading.Tasks.Task BulkJudiciaryPersonsStagingAsync(System.Collections.Generic.IEnumerable<JudiciaryPersonStagingRequest> request, System.Threading.CancellationToken cancellationToken)
+        {
+            var urlBuilder_ = new System.Text.StringBuilder();
+            urlBuilder_.Append(BaseUrl != null ? BaseUrl.TrimEnd('/') : "").Append("/BulkJudiciaryPersonsStaging?");
+            if (request != null)
+            {
+                foreach (var item_ in request) { urlBuilder_.Append(System.Uri.EscapeDataString("request") + "=").Append(System.Uri.EscapeDataString(ConvertToString(item_, System.Globalization.CultureInfo.InvariantCulture))).Append("&"); }
+            }
+            urlBuilder_.Length--;
+    
+            var client_ = _httpClient;
+            var disposeClient_ = false;
+            try
+            {
+                using (var request_ = new System.Net.Http.HttpRequestMessage())
+                {
+                    request_.Content = new System.Net.Http.StringContent(string.Empty, System.Text.Encoding.UTF8, "application/json");
+                    request_.Method = new System.Net.Http.HttpMethod("POST");
     
                     PrepareRequest(client_, request_, urlBuilder_);
     
--- BookingsApi/BookingsApi/Controllers/JudiciaryPersonStagingController.cs
+++ BookingsApi/BookingsApi/Controllers/JudiciaryPersonStagingController.cs
@@ -1,8 +1,13 @@
-﻿using System.Net;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using BookingsApi.Contract.Requests;
 using BookingsApi.DAL.Commands;
 using BookingsApi.DAL.Commands.Core;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using NSwag.Annotations;
 
 namespace BookingsApi.Controllers
@@ -10,21 +15,47 @@ namespace BookingsApi.Controllers
     public class JudiciaryPersonStagingController : Controller
     {
         private readonly ICommandHandler _commandHandler;
-        
-        public JudiciaryPersonStagingController(ICommandHandler commandHandler)
+        private readonly ILogger<JudiciaryPersonStagingController> _logger;
+
+        public JudiciaryPersonStagingController(ICommandHandler commandHandler, ILogger<JudiciaryPersonStagingController> logger)
         {
             _commandHandler = commandHandler;
+            _logger = logger;
         }
         
         [HttpGet("RemoveAllJudiciaryPersonsStaging")]
         [OpenApiOperation("RemoveAllJudiciaryPersonsStaging")]
         [ProducesResponseType((int)HttpStatusCode.OK)]
-        public async Task<IActionResult> RemoveAllJudiciaryPersonsStaging()
+        public async Task<IActionResult> RemoveAllJudiciaryPersonsStagingAsync()
         {
             await _commandHandler.Handle(new RemoveAllJudiciaryPersonStagingCommand());
 
             return Ok();
         }
 
+        [HttpPost("BulkJudiciaryPersonsStaging")]
+        [OpenApiOperation("BulkJudiciaryPersonsStaging")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<IActionResult> BulkJudiciaryPersonsStagingAsync(IEnumerable<JudiciaryPersonStagingRequest> request)
+        {
+            var judiciaryPersonStagingRequests = request.ToList();
+            
+            foreach (var item in judiciaryPersonStagingRequests)
+            {
+                try
+                {
+                    await _commandHandler.Handle(new AddJudiciaryPersonStagingCommand(item.Id, item.PersonalCode,
+                        item.Title, item.KnownAs, item.Surname,
+                        item.Fullname, item.PostNominals, item.Email, item.Leaver, item.LeftOn));
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError(e, "BookingsApi - BulkJudiciaryPersonsStagingAsync | error updating user");
+                }
+                
+            }
+            
+            return Ok();
+        }
     }
 }
