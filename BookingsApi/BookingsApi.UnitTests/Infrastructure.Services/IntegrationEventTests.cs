7498d1756372a2e9e128565fc6ca4296a5dd0407
mk <murali.katuri@hmcts.net>
2022-06-26 12:34:37 +0100

    Added tests for updating user name for a person and extracting additional values from other info for Judges

--- BookingsApi/BookingsApi.UnitTests/Infrastructure.Services/IntegrationEventTests.cs
+++ BookingsApi/BookingsApi.UnitTests/Infrastructure.Services/IntegrationEventTests.cs
@@ -153,6 +153,59 @@ namespace BookingsApi.UnitTests.Infrastructure.Services
         }
 
         [Test]
+        public void Should_publish_message_to_queue_when_HearingIsReadyForVideoIntegrationEvent_with_otherinformation_is_raised()
+        {
+            var hearing = new VideoHearingBuilder().Build();
+            hearing.CaseType = new CaseType(1, "test");
+            hearing.AddCase("1234", "test", true);
+            var email = "judge.login@hearings.reform.hmcts.net";
+            var phone = "9594395734534";
+            hearing.OtherInformation = $"JudgeEmail|{email}|JudgePhone|{phone}";
+            var individuals = hearing.GetParticipants().Where(x => x is Individual).ToList();
+
+            var individual1 = individuals.First();
+            individual1.CaseRole = new CaseRole(1, "test");
+
+            var individual2 = individuals.Last();
+            individual2.CaseRole = new CaseRole(2, "test2");
+
+            var representative = hearing.GetParticipants().Single(x => x is Representative);
+            representative.CaseRole = new CaseRole(3, "test3");
+
+            var judge = hearing.GetParticipants().Single(x => x is Judge);
+            judge.CaseRole = new CaseRole(3, "test4");
+            
+            var joh = hearing.GetParticipants().Single(x => x is JudicialOfficeHolder);
+            joh.CaseRole = new CaseRole(4, "test5");
+            var staffMember = hearing.GetParticipants().Single(x => x is StaffMember);
+            staffMember.CaseRole = new CaseRole(5, "test5");
+
+            hearing.AddEndpoints(new List<Endpoint>
+            {
+                new Endpoint("one", Guid.NewGuid().ToString(), "1234", null),
+                new Endpoint("two", Guid.NewGuid().ToString(), "1234", representative)
+            });
+
+            var hearingIsReadyForVideoIntegrationEvent = new HearingIsReadyForVideoIntegrationEvent(hearing);
+            _eventPublisher.PublishAsync(hearingIsReadyForVideoIntegrationEvent);
+
+            _serviceBusQueueClient.Count.Should().Be(1);
+            var @event = _serviceBusQueueClient.ReadMessageFromQueue();
+            @event.IntegrationEvent.Should().BeOfType<HearingIsReadyForVideoIntegrationEvent>();
+            var typedEvent = (HearingIsReadyForVideoIntegrationEvent)@event.IntegrationEvent;
+            typedEvent.Hearing.RecordAudio.Should().Be(hearing.AudioRecordingRequired);
+            typedEvent.Participants.Count.Should().Be(hearing.GetParticipants().Count);
+            typedEvent.Endpoints.Should().NotBeNull();
+            typedEvent.Endpoints.Count.Should().Be(hearing.GetEndpoints().Count);
+            var sentJudge = typedEvent.Participants.Single(x => x.UserRole == "Judge");
+            sentJudge.ContactEmailForNonEJudJudgeUser.Should().Be(email);
+            sentJudge.ContactPhoneForNonEJudJudgeUser.Should().Be(phone);
+            var participant1 = typedEvent.Participants.First(x => x.UserRole != "Judge");
+            participant1.ContactEmailForNonEJudJudgeUser.Should().BeNull();
+            participant1.ContactPhoneForNonEJudJudgeUser.Should().BeNull();
+        }
+
+        [Test]
         public void Should_publish_message_to_queue_when_EndpointAddedIntegrationEvent_is_raised()
         {
             var endpointAddedIntegrationEvent =
