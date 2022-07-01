b6619dde1bc5da1c741488e0aa67379babfc75f9
Will Craig <william.craig2@justice.gov.uk>
2022-07-01 11:42:22 +0100

    Revert "VIH-9099 Removed references to Vh.Core.Configuration (#448)"
    This reverts commit db0fd90897355088693c9c8a56706b2fc2bc642f.

--- BookingsApi/BookingsApi/Program.cs
+++ BookingsApi/BookingsApi/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using VH.Core.Configuration;
 
 namespace BookingsApi
 {
@@ -18,7 +19,15 @@ namespace BookingsApi
 // ReSharper disable once MemberCanBePrivate.Global Needed for client generation on build with nswag
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
+            const string vhInfraCore = "/mnt/secrets/vh-infra-core";
+			const string vhBookingsApi = "/mnt/secrets/vh-bookings-api";
+
             return Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((configBuilder) =>
+                {
+                    configBuilder.AddAksKeyVaultSecretProvider(vhInfraCore);
+					configBuilder.AddAksKeyVaultSecretProvider(vhBookingsApi);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseContentRoot(Directory.GetCurrentDirectory());
