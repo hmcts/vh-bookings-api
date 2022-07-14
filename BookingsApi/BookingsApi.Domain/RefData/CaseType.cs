d00007df27817fc9e8c404a2e2fe219debbe41c0
Will Craig <william.craig2@justice.gov.uk>
2022-07-14 11:06:38 +0100

    Added new case type columns, live and serviceId. Generated migrations. Created new featureToggle for reference data

--- BookingsApi/BookingsApi.Domain/RefData/CaseType.cs
+++ BookingsApi/BookingsApi.Domain/RefData/CaseType.cs
@@ -10,10 +10,15 @@ namespace BookingsApi.Domain.RefData
             Name = name;
             CaseRoles = new List<CaseRole>();
             HearingTypes = new List<HearingType>();
+            Live = true;
         }
         
         public string Name { get; set; }
         public virtual List<CaseRole> CaseRoles { get; set; }
         public virtual List<HearingType> HearingTypes { get; set; }
+        
+        public string ServiceId { get; set; }
+        public bool Live { get; set; }
+        
     }
 }
