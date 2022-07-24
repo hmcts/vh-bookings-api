bb3fe0d604eb2063a69d06a95a7c17140a7b4a24
Will Craig <william.craig2@justice.gov.uk>
2022-07-24 19:09:58 +0100

    migrations

---
+++ BookingsApi/BookingsApi.DAL/Migrations/20220722161644_AddJobNameAndIsSuccessfulToJobHistory.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookingsApi.DAL.Migrations
+{
+    public partial class AddJobNameAndIsSuccessfulToJobHistory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSuccessful",
+                table: "JobHistory",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "JobName",
+                table: "JobHistory",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsSuccessful",
+                table: "JobHistory");
+
+            migrationBuilder.DropColumn(
+                name: "JobName",
+                table: "JobHistory");
+        }
+    }
+}
