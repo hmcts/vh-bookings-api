4529659e5e92b26a2b88e4e2af7485ed6c4295e0
Oliver Scott <oliver.scott@justice.gov.uk>
2022-09-05 15:33:56 +0100

    Add Code and WelshName to HearingType

---
+++ BookingsApi/BookingsApi.DAL/Migrations/20220905140545_AddNewAttributesToHearingType.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookingsApi.DAL.Migrations
+{
+    public partial class AddNewAttributesToHearingType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Code",
+                table: "HearingType",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "WelshName",
+                table: "HearingType",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Code",
+                table: "HearingType");
+
+            migrationBuilder.DropColumn(
+                name: "WelshName",
+                table: "HearingType");
+        }
+    }
+}
