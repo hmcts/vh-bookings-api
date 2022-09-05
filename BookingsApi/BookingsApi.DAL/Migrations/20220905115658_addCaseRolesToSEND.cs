d49f728ae2349493bce03ccffafe11fe3de30b83
Will Craig <william.craig2@justice.gov.uk>
2022-09-05 14:55:45 +0100

    updated ID used

--- BookingsApi/BookingsApi.DAL/Migrations/20220905115658_addCaseRolesToSEND.cs
+++ BookingsApi/BookingsApi.DAL/Migrations/20220905115658_addCaseRolesToSEND.cs
@@ -14,8 +14,8 @@ namespace BookingsApi.DAL.Migrations
                new[] { "Id", "Name", "Group", "CaseTypeId" },
                new object[,]
                {
-                    { 338, "Applicant", (int) CaseRoleGroup.Applicant, 52 },
-                    { 339, "Respondant", (int) CaseRoleGroup.Respondent, 52 },
+                    { 343, "Applicant", (int) CaseRoleGroup.Applicant, 52 },
+                    { 344, "Respondant", (int) CaseRoleGroup.Respondent, 52 },
                });
 
             migrationBuilder.InsertData(
@@ -24,36 +24,36 @@ namespace BookingsApi.DAL.Migrations
                 new object[,]
                 {
                     //Applicants
-                    { 1081, HearingRoles.Appellant, UserRoleForHearingRole.UserRoleId[UserRoles.Individual], 338 },
-                    { 1082, HearingRoles.Expert, UserRoleForHearingRole.UserRoleId[UserRoles.Individual], 338 },
-                    { 1083, HearingRoles.Intermediary, UserRoleForHearingRole.UserRoleId[UserRoles.Individual], 338 },
-                    { 1084, HearingRoles.Interpreter, UserRoleForHearingRole.UserRoleId[UserRoles.Individual], 338 },
-                    { 1085, HearingRoles.LitigantInPerson, UserRoleForHearingRole.UserRoleId[UserRoles.Individual], 338 },
-                    { 1086, HearingRoles.LitigationFriend, UserRoleForHearingRole.UserRoleId[UserRoles.Individual], 338 },
-                    { 1087, HearingRoles.MacKenzieFriend, UserRoleForHearingRole.UserRoleId[UserRoles.Individual], 338 },
-                    { 1088, HearingRoles.Representative, UserRoleForHearingRole.UserRoleId[UserRoles.Representative], 338 },
-                    { 1089, HearingRoles.Solicitor, UserRoleForHearingRole.UserRoleId[UserRoles.Representative], 338 },
-                    { 1090, HearingRoles.Witness, UserRoleForHearingRole.UserRoleId[UserRoles.Individual], 338 },
+                    { 1081, HearingRoles.Appellant, UserRoleForHearingRole.UserRoleId[UserRoles.Individual], 343 },
+                    { 1082, HearingRoles.Expert, UserRoleForHearingRole.UserRoleId[UserRoles.Individual], 343 },
+                    { 1083, HearingRoles.Intermediary, UserRoleForHearingRole.UserRoleId[UserRoles.Individual], 343 },
+                    { 1084, HearingRoles.Interpreter, UserRoleForHearingRole.UserRoleId[UserRoles.Individual], 343 },
+                    { 1085, HearingRoles.LitigantInPerson, UserRoleForHearingRole.UserRoleId[UserRoles.Individual], 343 },
+                    { 1086, HearingRoles.LitigationFriend, UserRoleForHearingRole.UserRoleId[UserRoles.Individual], 343 },
+                    { 1087, HearingRoles.MacKenzieFriend, UserRoleForHearingRole.UserRoleId[UserRoles.Individual], 343 },
+                    { 1088, HearingRoles.Representative, UserRoleForHearingRole.UserRoleId[UserRoles.Representative], 343 },
+                    { 1089, HearingRoles.Solicitor, UserRoleForHearingRole.UserRoleId[UserRoles.Representative], 343 },
+                    { 1090, HearingRoles.Witness, UserRoleForHearingRole.UserRoleId[UserRoles.Individual], 343 },
                     
                     //Respondent
-                    { 1091, HearingRoles.Appellant, UserRoleForHearingRole.UserRoleId[UserRoles.Individual], 339 },
-                    { 1092, HearingRoles.Expert, UserRoleForHearingRole.UserRoleId[UserRoles.Individual], 339 },
-                    { 1093, HearingRoles.Intermediary, UserRoleForHearingRole.UserRoleId[UserRoles.Individual], 339 },
-                    { 1094, HearingRoles.Interpreter, UserRoleForHearingRole.UserRoleId[UserRoles.Individual], 339 },
-                    { 1095, HearingRoles.LitigantInPerson, UserRoleForHearingRole.UserRoleId[UserRoles.Individual], 339 },
-                    { 1096, HearingRoles.LitigationFriend, UserRoleForHearingRole.UserRoleId[UserRoles.Individual], 339 },
-                    { 1097, HearingRoles.MacKenzieFriend, UserRoleForHearingRole.UserRoleId[UserRoles.Individual], 339 },
-                    { 1098, HearingRoles.Representative, UserRoleForHearingRole.UserRoleId[UserRoles.Representative], 339 },
-                    { 1099, HearingRoles.Solicitor, UserRoleForHearingRole.UserRoleId[UserRoles.Representative], 339 },
-                    { 1100, HearingRoles.Witness, UserRoleForHearingRole.UserRoleId[UserRoles.Individual], 339 },
+                    { 1091, HearingRoles.Appellant, UserRoleForHearingRole.UserRoleId[UserRoles.Individual], 344 },
+                    { 1092, HearingRoles.Expert, UserRoleForHearingRole.UserRoleId[UserRoles.Individual], 344 },
+                    { 1093, HearingRoles.Intermediary, UserRoleForHearingRole.UserRoleId[UserRoles.Individual], 344 },
+                    { 1094, HearingRoles.Interpreter, UserRoleForHearingRole.UserRoleId[UserRoles.Individual], 344 },
+                    { 1095, HearingRoles.LitigantInPerson, UserRoleForHearingRole.UserRoleId[UserRoles.Individual], 344 },
+                    { 1096, HearingRoles.LitigationFriend, UserRoleForHearingRole.UserRoleId[UserRoles.Individual], 344 },
+                    { 1097, HearingRoles.MacKenzieFriend, UserRoleForHearingRole.UserRoleId[UserRoles.Individual], 344 },
+                    { 1098, HearingRoles.Representative, UserRoleForHearingRole.UserRoleId[UserRoles.Representative], 344 },
+                    { 1099, HearingRoles.Solicitor, UserRoleForHearingRole.UserRoleId[UserRoles.Representative], 344 },
+                    { 1100, HearingRoles.Witness, UserRoleForHearingRole.UserRoleId[UserRoles.Individual], 344 },
             });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
      
-            migrationBuilder.DeleteData(nameof(HearingRole), "Id", 338);
-            migrationBuilder.DeleteData(nameof(HearingRole), "Id", 339);
+            migrationBuilder.DeleteData(nameof(HearingRole), "Id", 343);
+            migrationBuilder.DeleteData(nameof(HearingRole), "Id", 344);
             
             for (int caseRoleId = 1081; caseRoleId <= 1100; caseRoleId++)
                 migrationBuilder.DeleteData(nameof(CaseRole), "Id", caseRoleId);
