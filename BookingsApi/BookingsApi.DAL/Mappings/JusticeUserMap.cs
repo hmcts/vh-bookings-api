66d2042802494abb147b1fb4c0b29af7c74b815e
Will Craig <william.craig2@justice.gov.uk>
2022-10-25 15:56:02 +0100

    added virtual properties to justice user

--- BookingsApi/BookingsApi.DAL/Mappings/JusticeUserMap.cs
+++ BookingsApi/BookingsApi.DAL/Mappings/JusticeUserMap.cs
@@ -8,8 +8,9 @@ namespace BookingsApi.DAL.Mappings
         public void Configure(EntityTypeBuilder<JusticeUser> builder)
         {
             builder.ToTable("JusticeUser");
-
             builder.HasKey(x => x.Id);
+            builder.HasMany<VhoWorkHours>("VhoWorkHours").WithOne("JusticeUser").HasForeignKey(x => x.JusticeUserId);
+            builder.HasMany<VhoNonAvailability>("VhoNonAvailability").WithOne("JusticeUser").HasForeignKey(x => x.JusticeUserId);
         }
     }
 }
