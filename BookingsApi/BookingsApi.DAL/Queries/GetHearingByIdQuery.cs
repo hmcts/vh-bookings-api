83d5abd96b081d94eb75c6088203c5b45589a263
marcogagliardi <marco.gagliardi@justice.gov.uk>
2022-11-30 10:16:37 +0000

    revert change

--- BookingsApi/BookingsApi.DAL/Queries/GetHearingByIdQuery.cs
+++ BookingsApi/BookingsApi.DAL/Queries/GetHearingByIdQuery.cs
@@ -42,8 +42,7 @@ namespace BookingsApi.DAL.Queries
                 .Include(x => x.HearingType)
                 .Include(x => x.HearingVenue)
                 .Include(x => x.Endpoints).ThenInclude(x => x.DefenceAdvocate).ThenInclude(x => x.Person)
-                .Include(x => x.Allocations)// .ThenInclude(x => x.JusticeUser)
-                .Include("Allocations.JusticeUser")
+                .Include(x => x.Allocations).ThenInclude(x => x.JusticeUser)
                 .AsNoTracking()
                 .SingleOrDefaultAsync(x => x.Id == query.HearingId);
         }
