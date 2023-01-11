9fdfb5ae49255bb6c44f366d0ec861231239ecac
mk <murali.katuri@hmcts.net>
2023-01-11 23:03:38 +0000

    Added comments to the update booking status enum

--- BookingsApi/BookingsApi.Contract/Requests/Enums/UpdateBookingStatus.cs
+++ BookingsApi/BookingsApi.Contract/Requests/Enums/UpdateBookingStatus.cs
@@ -5,9 +5,21 @@
     /// </summary>
     public enum UpdateBookingStatus
     {
+        /// <summary>
+        /// Hearing booked
+        /// </summary>
         Booked = 1,
+        /// <summary>
+        /// Conference created
+        /// </summary>
         Created = 2,
+        /// <summary>
+        /// Cancelled
+        /// </summary>
         Cancelled = 3,
+        /// <summary>
+        /// Failed
+        /// </summary>
         Failed = 4
     }
 }
