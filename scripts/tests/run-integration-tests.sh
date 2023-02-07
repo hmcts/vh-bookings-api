74147941e17283143e770089cab1bcb3b2f0543a
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-02-07 21:32:25 +0000

    add scripts to run unit and integration tests

---
+++ scripts/tests/run-integration-tests.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Script is for docker compose tests where the script is at the root level
+dotnet test BookingsApi/BookingsApi.IntegrationTests/BookingsApi.IntegrationTests.csproj -c Release --no-build --results-directory ../TestResults \
+    "/p:CollectCoverage=true" \
+    "/p:Exclude=\"[*]Testing.Common.*,[*]BookingsApi.Common.*,[*.Common]*\"" \
+    "/p:CoverletOutput=../Coverage/" \
+    "/p:MergeWith=../Coverage/coverage.json" \
+    "/p:CoverletOutputFormat=\"json,cobertura\""
