cc1cdd4be8cfddc0a1edb95d9d939b8c282901c5
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-02-09 12:19:51 +0000

    run only db tests

--- scripts/tests/run-tests.sh
+++ scripts/tests/run-tests.sh
@@ -8,7 +8,7 @@ dotnet test BookingsApi/BookingsApi.UnitTests/BookingsApi.UnitTests.csproj -c Re
     "/p:MergeWith=/Coverage/coverage.json" \
     "/p:CoverletOutputFormat=\"json,cobertura\""
 
-dotnet test BookingsApi/BookingsApi.IntegrationTests/BookingsApi.IntegrationTests.csproj -c Release --no-build --results-directory ./TestResults --logger "trx;LogFileName=BookingsApi-Integration-Tests-TestResults.trx" \
+dotnet test BookingsApi/BookingsApi.IntegrationTests/BookingsApi.IntegrationTests.csproj -c Release --no-build --filter FullyQualifiedName~BookingsApi.IntegrationTests.Database --results-directory ./TestResults --logger "trx;LogFileName=BookingsApi-Integration-Tests-TestResults.trx" \
     "/p:CollectCoverage=true" \
     "/p:Exclude=\"[*]Testing.Common.*,[*]BookingsApi.Common.*,[*.Common]*\"" \
     "/p:CoverletOutput=/Coverage/" \
