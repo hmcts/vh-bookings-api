7a79f01a497d42e30c2d348490c06dc62ec06b4e
abbasiTahir <99877665+abbasiTahir@users.noreply.github.com>
2023-02-09 14:30:04 +0000

    Added the Migration
    added the new migration which updates the data in hearing type table .

---
+++ BookingsApi/BookingsApi.DAL/Migrations/20230209092722_UpdateHearingTypeColumn.cs
@@ -0,0 +1,28 @@
+﻿using BookingsApi.Domain.RefData;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookingsApi.DAL.Migrations
+{
+    public partial class UpdateHearingTypeColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: nameof(HearingType),
+                keyColumn: "Id",
+                keyValue: 284,
+                column: "Name",
+                value: "Interlocutory/Directions");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: nameof(HearingType),
+                keyColumn: "Id",
+                keyValue: 284,
+                column: "Name",
+                value: "Interloculatory/Directions");
+        }
+    }
+}
