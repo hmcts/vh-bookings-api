1491b8464f99ba5a93b2e502e636d86aa6175718
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-02-10 10:22:50 +0000

    update api setup for generating tokens

--- BookingsApi/BookingsApi.AcceptanceTests/Api/ApiTest.cs
+++ BookingsApi/BookingsApi.AcceptanceTests/Api/ApiTest.cs
@@ -1,11 +1,12 @@
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using BookingsApi.AcceptanceTests.Hooks;
 using BookingsApi.Client;
 using BookingsApi.Common.Configuration;
-using BookingsApi.Common.Security;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Options;
 using NUnit.Framework;
+using VHConfigurationManager = AcceptanceTests.Common.Configuration.ConfigurationManager;
 
 namespace BookingsApi.AcceptanceTests.Api;
 
@@ -13,23 +14,43 @@ public abstract class ApiTest
 {
     protected IConfigurationRoot _configRoot;
     protected BookingsApiClient _client;
-    
+    private AzureAdConfiguration _azureConfiguration;
+    private ServicesConfiguration _serviceConfiguration;
+
     [OneTimeSetUp]
-    public void OneTimeSetup()
+    public async Task OneTimeSetup()
     {
         var userSecretsId = "D76B6EB8-F1A2-4A51-9B8F-21E1B6B81E4F";
         _configRoot = new ConfigurationBuilder().AddJsonFile("appsettings.json").AddUserSecrets(userSecretsId)
             .AddEnvironmentVariables().Build();
-        
-        // generate token
-        var azureOptions = Options.Create(_configRoot.GetSection("AzureAd").Get<AzureAdConfiguration>());
-        var serviceConfiguration = _configRoot.GetSection("Services").Get<ServicesConfiguration>();
-        var apiToken = new AzureTokenProvider(azureOptions).GetClientAccessToken(
-            azureOptions.Value.ClientId, azureOptions.Value.ClientSecret, serviceConfiguration.BookingsApiResourceId);
-        
+
+        RegisterSettings();
+        var apiToken = await GenerateApiToken();
         var httpClient = new HttpClient();
         httpClient.DefaultRequestHeaders.Authorization =
             new AuthenticationHeaderValue("bearer", apiToken);
-        _client = BookingsApiClient.GetClient(serviceConfiguration.BookingsApiUrl, httpClient);
+        _client = BookingsApiClient.GetClient(_serviceConfiguration.BookingsApiUrl, httpClient);
+    }
+
+    private void RegisterSettings()
+    {
+        _azureConfiguration = _configRoot.GetSection("AzureAd").Get<AzureAdConfiguration>();
+        VHConfigurationManager.VerifyConfigValuesSet(_azureConfiguration);
+
+        _serviceConfiguration = _configRoot.GetSection("Services").Get<ServicesConfiguration>();
+        VHConfigurationManager.VerifyConfigValuesSet(_serviceConfiguration);
+    }
+
+    private Task<string> GenerateApiToken()
+    {
+        // We should get rid of this and use azure the AzureTokenProvider we have in the Common Project
+        var adConfig = new AzureAdConfig()
+        {
+            Authority = _azureConfiguration.Authority += _azureConfiguration.TenantId,
+            ClientId = _azureConfiguration.ClientId,
+            ClientSecret = _azureConfiguration.ClientSecret,
+            TenantId = _azureConfiguration.TenantId
+        };
+        return VHConfigurationManager.GetBearerToken(adConfig, _serviceConfiguration.BookingsApiResourceId);
     }
 }
