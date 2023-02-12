c4c40f04ac092818de81ba24e4b163ba648cc1af
william.craig <william.craig2@justice.gov.uk>
2023-02-12 13:25:02 +0000

    Revert "Updated api endpoint to also include JusticeUser"
    This reverts commit bd76ec16

--- BookingsApi/BookingsApi.Contract/Responses/VenueWithAllocatedCsoReponse.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace BookingsApi.Contract.Responses;
-
-public class VenueWithAllocatedCsoResponse
-{
-    public string HearingVenueName { get; set; }
-    public JusticeUserResponse Cso { get; set; }
-}
