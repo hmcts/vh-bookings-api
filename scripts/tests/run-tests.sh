2ac28ecab799aa84c4699b61d81edb0e02937668
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-02-15 19:17:33 +0000

    enable integration db tests

--- scripts/tests/run-tests.sh
+++ scripts/tests/run-tests.sh
@@ -5,8 +5,8 @@ echo "ProjectKey ${SONAR_PROJECT_KEY}"
 echo "SONAR TOKEN ${SONAR_TOKEN}"
 echo "Sonar Host ${SONAR_HOST}"
 echo "Sonar Org ${SONAR_ORG}"
-echo "Sonar Org ${SONAR_PROJECT_VERSION}"
-echo "Sonar Org ${SONAR_PROJECT_NAME}"
+echo "Sonar SONAR_PROJECT_VERSION ${SONAR_PROJECT_VERSION}"
+echo "Sonar SONAR_PROJECT_NAME ${SONAR_PROJECT_NAME}"
 
 echo "Sonar Begining"
 dotnet sonarscanner begin /k:"${SONAR_PROJECT_KEY}" /o:"${SONAR_ORG}" /version:"${SONAR_PROJECT_VERSION}" /name:"${SONAR_PROJECT_NAME}" /d:sonar.host.url="${SONAR_HOST}" /d:sonar.login="${SONAR_TOKEN}" /d:sonar.cs.opencover.reportsPaths="${PWD}/Coverage/coverage.opencover.xml"
@@ -23,11 +23,11 @@ dotnet test BookingsApi/BookingsApi.UnitTests/BookingsApi.UnitTests.csproj -c Re
     "/p:MergeWith=/Coverage/coverage.json" \
     "/p:CoverletOutputFormat=\"opencover,json,cobertura,lcov\""
 
-# dotnet test BookingsApi/BookingsApi.IntegrationTests/BookingsApi.IntegrationTests.csproj -c Release --no-build --filter FullyQualifiedName~BookingsApi.IntegrationTests.Database --results-directory ./TestResults --logger "trx;LogFileName=BookingsApi-Integration-Tests-TestResults.trx" \
-#     "/p:CollectCoverage=true" \
-#     "/p:Exclude=\"[*]Testing.Common.*,[*]BookingsApi.Common.*,[*.Common]*\"" \
-#     "/p:CoverletOutput=/Coverage/" \
-#     "/p:MergeWith={PWD}/Coverage/coverage.json" \
-#     "/p:CoverletOutputFormat=\"opencover,json,cobertura,lcov\""
+dotnet test BookingsApi/BookingsApi.IntegrationTests/BookingsApi.IntegrationTests.csproj -c Release --no-build --filter FullyQualifiedName~BookingsApi.IntegrationTests.Database --results-directory ./TestResults --logger "trx;LogFileName=BookingsApi-Integration-Tests-TestResults.trx" \
+    "/p:CollectCoverage=true" \
+    "/p:Exclude=\"[*]Testing.Common.*,[*]BookingsApi.Common.*,[*.Common]*\"" \
+    "/p:CoverletOutput=/Coverage/" \
+    "/p:MergeWith={PWD}/Coverage/coverage.json" \
+    "/p:CoverletOutputFormat=\"opencover,json,cobertura,lcov\""
 echo "Sonar End"
 dotnet sonarscanner end /d:sonar.login="${SONAR_TOKEN}"
