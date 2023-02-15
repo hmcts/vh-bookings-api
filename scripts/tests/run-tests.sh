f67f5f1e34f82e941827a71194d47a9bef985392
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-02-15 14:18:38 +0000

    build and test only in docker

--- scripts/tests/run-tests.sh
+++ scripts/tests/run-tests.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
 echo "Current DIR ${PWD}"
-echo "Restoring dotnet tools"
-dotnet tool restore
+# echo "ProjectKey ${SONAR_PROJECT_KEY}"
+# echo "ProjectToken ${SONAR_TOKEN}"
+# echo "Restoring dotnet tools"
+# dotnet tool install --global dotnet-sonarscanner
+# export PATH="$PATH:/root/.dotnet/tools"
 
-echo "Sonar Begining"
-dotnet sonarscanner begin /k:"${SONAR_PROJECTKEY}" /d:sonar.cs.opencover.reportsPaths="${PWD}/Coverage/coverage.opencover.xml" /o:hmcts
+# echo "Sonar Begining"
+# dotnet sonarscanner begin /k:"${SONAR_PROJECT_KEY}" /d:sonar.cs.opencover.reportsPaths="${PWD}/Coverage/coverage.opencover.xml" /o:hmcts /d:sonar.login="${SONAR_TOKEN}"
+# dotnet sonarscanner begin /k:"${SONAR_PROJECT_KEY}" /d:sonar.login="${SONAR_TOKEN}" /d:sonar.cs.opencover.reportsPaths="${PWD}/Coverage/coverage.opencover.xml"
 
 echo "Building solution"
 dotnet build BookingsApi/BookingsApi.sln -c Release
@@ -19,12 +23,12 @@ dotnet test BookingsApi/BookingsApi.UnitTests/BookingsApi.UnitTests.csproj -c Re
     "/p:MergeWith=/Coverage/coverage.json" \
     "/p:CoverletOutputFormat=\"opencover,json,cobertura,lcov\""
 
-dotnet test BookingsApi/BookingsApi.IntegrationTests/BookingsApi.IntegrationTests.csproj -c Release --no-build --filter FullyQualifiedName~BookingsApi.IntegrationTests.Database --results-directory ./TestResults --logger "trx;LogFileName=BookingsApi-Integration-Tests-TestResults.trx" \
-    "/p:CollectCoverage=true" \
-    "/p:Exclude=\"[*]Testing.Common.*,[*]BookingsApi.Common.*,[*.Common]*\"" \
-    "/p:CoverletOutput=/Coverage/" \
-    "/p:MergeWith={PWD}/Coverage/coverage.json" \
-    "/p:CoverletOutputFormat=\"opencover,json,cobertura,lcov\""
+# dotnet test BookingsApi/BookingsApi.IntegrationTests/BookingsApi.IntegrationTests.csproj -c Release --no-build --filter FullyQualifiedName~BookingsApi.IntegrationTests.Database --results-directory ./TestResults --logger "trx;LogFileName=BookingsApi-Integration-Tests-TestResults.trx" \
+#     "/p:CollectCoverage=true" \
+#     "/p:Exclude=\"[*]Testing.Common.*,[*]BookingsApi.Common.*,[*.Common]*\"" \
+#     "/p:CoverletOutput=/Coverage/" \
+#     "/p:MergeWith={PWD}/Coverage/coverage.json" \
+#     "/p:CoverletOutputFormat=\"opencover,json,cobertura,lcov\""
 
-echo "Sonar End"
-dotnet sonarscanner end
+# echo "Sonar End"
+# dotnet sonarscanner end /d:sonar.login="${SONAR_TOKEN}"
