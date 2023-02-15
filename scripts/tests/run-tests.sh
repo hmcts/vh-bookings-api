49295e43d4600a5b765b3a9bf5967089c9b825a0
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-02-15 12:49:36 +0000

    add organisation

--- scripts/tests/run-tests.sh
+++ scripts/tests/run-tests.sh
@@ -2,7 +2,7 @@
 
 dotnet tool restore
 
-dotnet sonarscanner begin /k:"${SONAR_PROJECTKEY}" /d:sonar.cs.opencover.reportsPaths="${PWD}/Coverage/coverage.opencover.xml"
+dotnet sonarscanner begin /k:"${SONAR_PROJECTKEY}" /d:sonar.cs.opencover.reportsPaths="${PWD}/Coverage/coverage.opencover.xml" /o:hmcts
 
 # Script is for docker compose tests where the script is at the root level
 dotnet test BookingsApi/BookingsApi.UnitTests/BookingsApi.UnitTests.csproj -c Release --no-build --results-directory ./TestResults --logger "trx;LogFileName=BookingsApi-Unit-Tests-TestResults.trx" \
