3b501d191377444baa4d1fd2a773ba5c12d99f75
Phillip Chambers <phillipjchambers@gmail.com>
2023-03-03 12:47:28 +0000

    VIH-9352 add validation tests

--- BookingsApi/BookingsApi/Validations/EditJusticeUserRequestValidation.cs
+++ BookingsApi/BookingsApi/Validations/EditJusticeUserRequestValidation.cs
@@ -6,10 +6,13 @@ namespace BookingsApi.Validations;
 public class EditJusticeUserRequestValidation : AbstractValidator<EditJusticeUserRequest>
 {
     public static readonly string NoUsernameErrorMessage = "Username is required";
+    public static readonly string NoIdErrorMessage = "Id is required";
+    public static readonly string NoRoleErrorMessage = "Role is required";
+    
     public EditJusticeUserRequestValidation()
     {
-        RuleFor(x => x.Id).NotNull();
+        RuleFor(x => x.Id).NotNull().WithMessage(NoIdErrorMessage);
         RuleFor(x => x.Username).NotEmpty().WithMessage(NoUsernameErrorMessage);
-        RuleFor(x => x.Role).IsInEnum();
+        RuleFor(x => x.Role).IsInEnum().WithMessage(NoRoleErrorMessage);
     }
 }
