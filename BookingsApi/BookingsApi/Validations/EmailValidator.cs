742ed2722be85313e65b4d15127f1b522e6b9f5d
twyfordr273 <123372987+twyfordr273@users.noreply.github.com>
2023-03-06 10:43:16 +0000

    VIH-9428 - Participant Email Property otherMails has an invalid value (#601)
    * 1. Updated the IsValidEmail() function to use a Regex pattern instead of the MailAddress class. The MailAddress class missed a few validations for invalid email addresses
    
    * 1. Removed the FormatException from EmailValidator
    
    * 1. Added Timeout option to Regex Match
    
    * 1. Added RegexMatchTimeoutException to Match code
    
    * 1. Updated invalid emails in tests to valid emails based on new regex validation for emails
    
    * 1. Removed RegexMatchTimeoutException try catch block from email validator
    2. Cleaned the EmailValidator tests
    
    * 1. Code smells said to mark the GetInvalidEmail() and GetValidEmail() functions in the EmailValidatorTests as static
    
    * 1. Addressing "does not access instance data and can be marked as static" code smells and set functions to static
    
    * 1. Updated Email Validation Regex to allow single character domains such a @a.com
    2. Revert emails updated due to previously outdated regex that did not allow single character domains in emails
    
    * 1. Forcing at least 1 dot with a valid character before and after in the domain of an email
    
    * 1. Updated regex validation to reduce the size of the regex pattern
    
    * 1. Updated regex to prevent denial of service caused by regex backtracking

--- BookingsApi/BookingsApi/Validations/EmailValidator.cs
+++ BookingsApi/BookingsApi/Validations/EmailValidator.cs
@@ -1,33 +1,29 @@
 ﻿using System;
 using System.Net.Mail;
+using System.Text.RegularExpressions;
 
 namespace BookingsApi.Validations
 {
     /// <summary>Simple validator to check email formats</summary>
     public static class EmailValidator
     {
+        private const string RegexPattern = @"^([!#-'*/-9=?A-Z^-~-]+(\.[!#-'*/-9=?A-Z^-~-]+)*)@([!#-'*/-9=?A-Z^-~-]+(\.[!#-'*/-9=?A-Z^-~-]+)+)$";
+        
         /// <summary>
         /// Test if the given string is specified and a valid email address
         /// </summary>
         /// <remarks>
         /// This was recommended one of the simplest way to manage email validation.
+        /// Use of the MailAddress class did not validate all email formats e.g. 'email.@email.com' was considered valid
+        /// Updated to use the above regex during validation - VIH-9428
         /// </remarks>
         public static bool IsValidEmail(this string email)
         {
             if (string.IsNullOrEmpty(email))
                 return false;
 
-            try
-            {
-#pragma warning disable S1848 // Objects should not be created to be dropped immediately without being used
-                new MailAddress(email);
-#pragma warning restore S1848 // Objects should not be created to be dropped immediately without being used
-                return true;
-            }
-            catch (FormatException)
-            {
-                return false;
-            }
+            var match = Regex.Match(email, RegexPattern);
+            return match.Success;
         }
     }
 }
