6650eee0109b37cc3e96a218e34979d3d18df32e
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-03-07 23:00:35 +0000

    run sds pipeline only

--- scripts/tests/run-tests.sh
+++ scripts/tests/run-tests.sh
@@ -4,7 +4,7 @@ set -x
 rm -d -r ${PWD}/Coverage
 rm -d -r ${PWD}/TestResults
 
-dotnet sonarscanner begin /k:"${SONAR_PROJECT_KEY}" /o:"${SONAR_ORG}" /version:"${SONAR_PROJECT_VERSION}" /name:"${SONAR_PROJECT_NAME}" /d:sonar.host.url="${SONAR_HOST}" /d:sonar.login="${SONAR_TOKEN}" /d:sonar.cs.opencover.reportsPaths="${PWD}/Coverage/coverage.opencover.xml" /d:sonar.coverage.exclusions="**/BookingsApi/Swagger/**/*,**/Program.cs,**/Startup.cs,**/Testing.Common/**/*,**/BookingsApi.Common/**/*,**/BookingsApi.IntegrationTests/**/*,**/BookingsApi.UnitTests/**/*,**/BookingsApi/Extensions/*,**/BookingsApi.DAL/Migrations/**/*" /d:sonar.cpd.exclusions="**/Program.cs,**/Startup.cs,**/Testing.Common/**/*,**/BookingsApi/Swagger/**/*,BookingsApi/BookingsApi.DAL/Migrations/*,BookingsApi/BookingsApi.DAL/TemplateDataForEnvironments.cs"
+dotnet sonarscanner begin /k:"${SONAR_PROJECT_KEY}" /o:"${SONAR_ORG}" /version:"${SONAR_PROJECT_VERSION}" /name:"${SONAR_PROJECT_NAME}" /d:sonar.host.url="${SONAR_HOST}" /d:sonar.login="${SONAR_TOKEN}" /d:sonar.cs.opencover.reportsPaths="${PWD}/Coverage/coverage.opencover.xml" /d:sonar.coverage.exclusions="**/BookingsApi/Swagger/**/*, **/Program.cs, **/Startup.cs, **/Testing.Common/**/*, **/BookingsApi.Common/**/*, **/BookingsApi.IntegrationTests/**/*, **/BookingsApi.UnitTests/**/*, **/BookingsApi/Extensions/*, **/BookingsApi.DAL/Migrations/**/*" /d:sonar.cpd.exclusions="**/Program.cs, **/Startup.cs, **/Testing.Common/**/*, **/BookingsApi/Swagger/**/*, BookingsApi/BookingsApi.DAL/Migrations/*, BookingsApi/BookingsApi.DAL/TemplateDataForEnvironments.cs"
 
 exclusions="[Testing.Common]*,[BookingsApi.Common]BookingsApi.Common.*,[BookingsApi.Domain]*.Ddd*,[BookingsApi.DAL]*.Migrations*,[BookingsApi]*.Swagger"
 
