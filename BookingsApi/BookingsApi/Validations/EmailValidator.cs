3714891fbd4b659b5bcd11958ddd487a2431a5e3
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-03-25 14:58:31 +0000

    remove accidental commit

--- BookingsApi/BookingsApi/Validations/EmailValidator.cs
+++ BookingsApi/BookingsApi/Validations/EmailValidator.cs
@@ -7,7 +7,6 @@ namespace BookingsApi.Validations
     /// <summary>Simple validator to check email formats</summary>
     public static class EmailValidator
     {
-        private const string EmailRegexPattern = 
         private const string RegexPattern = @"^([!#-'*/-9=?A-Z^-~-]+(\.[!#-'*/-9=?A-Z^-~-]+)*)@([!#-'*/-9=?A-Z^-~-]+(\.[!#-'*/-9=?A-Z^-~-]+)+)$";
         
         /// <summary>
