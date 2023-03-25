aae1b2c92022fd76a67c8e9d3bf21386d3007fea
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-03-25 14:20:34 +0000

    remove build from test step

--- scripts/tests/run-tests.sh
+++ scripts/tests/run-tests.sh
@@ -7,7 +7,6 @@ rm -d -r ${PWD}/TestResults
 exclusions="[Testing.Common]*,[BookingsApi.Common]BookingsApi.Common.*,[BookingsApi.Domain]*.Ddd*,[BookingsApi.DAL]*.Migrations*,[BookingsApi]*.Swagger"
 configuration=Release
 
-dotnet build BookingsApi/BookingsApi.sln -c $configuration
 # Script is for docker compose tests where the script is at the root level
 dotnet test BookingsApi/BookingsApi.UnitTests/BookingsApi.UnitTests.csproj -c $configuration --no-build --results-directory ./TestResults --logger "trx;LogFileName=BookingsApi-Unit-Tests-TestResults.trx" \
     "/p:CollectCoverage=true" \
