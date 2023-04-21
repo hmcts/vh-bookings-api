4dd2fca974694558bead3949673659bc81045591
william.craig <william.craig2@justice.gov.uk>
2023-04-21 14:41:06 +0100

    fix mapper to return default when no role available

--- BookingsApi/BookingsApi/Mappings/JusticeUserToResponseMapper.cs
+++ BookingsApi/BookingsApi/Mappings/JusticeUserToResponseMapper.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using BookingsApi.Contract.Responses;
 using BookingsApi.Domain;
 
@@ -10,20 +11,20 @@ namespace BookingsApi.Mappings
             JusticeUser judiciaryPersonStagingRequest)
         {
             return new JusticeUserResponse
-            {
-                FirstName = judiciaryPersonStagingRequest.FirstName,
-                Lastname = judiciaryPersonStagingRequest.Lastname,
-                ContactEmail = judiciaryPersonStagingRequest.ContactEmail,
-                Username = judiciaryPersonStagingRequest.Username,
-                Telephone = judiciaryPersonStagingRequest.Telephone,
-                UserRoleId = judiciaryPersonStagingRequest.JusticeUserRoles.First().UserRole.Id, 
-                UserRoleName = judiciaryPersonStagingRequest.JusticeUserRoles.First().UserRole.Name, 
-                IsVhTeamLeader = judiciaryPersonStagingRequest.IsTeamLeader(),
-                CreatedBy = judiciaryPersonStagingRequest.CreatedBy,
-                Id = judiciaryPersonStagingRequest.Id,
-                FullName = judiciaryPersonStagingRequest.FirstName + " " + judiciaryPersonStagingRequest.Lastname,
-                Deleted = judiciaryPersonStagingRequest.Deleted
-            };
+                {
+                    FirstName = judiciaryPersonStagingRequest.FirstName,
+                    Lastname = judiciaryPersonStagingRequest.Lastname,
+                    ContactEmail = judiciaryPersonStagingRequest.ContactEmail,
+                    Username = judiciaryPersonStagingRequest.Username,
+                    Telephone = judiciaryPersonStagingRequest.Telephone,
+                    UserRoleId = judiciaryPersonStagingRequest.JusticeUserRoles.FirstOrDefault()?.UserRole.Id ?? 0, 
+                    UserRoleName = judiciaryPersonStagingRequest.JusticeUserRoles.FirstOrDefault()?.UserRole.Name, 
+                    IsVhTeamLeader = judiciaryPersonStagingRequest.IsTeamLeader(),
+                    CreatedBy = judiciaryPersonStagingRequest.CreatedBy,
+                    Id = judiciaryPersonStagingRequest.Id,
+                    FullName = judiciaryPersonStagingRequest.FirstName + " " + judiciaryPersonStagingRequest.Lastname,
+                    Deleted = judiciaryPersonStagingRequest.Deleted
+                };
         }
     }
 }
