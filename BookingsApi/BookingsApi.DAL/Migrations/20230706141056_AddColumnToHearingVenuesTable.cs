1a8f636cc78e0b3478e18493e0ebbf9ec7271ec8
marcogagliardi <marco.gagliardi@justice.gov.uk>
2023-07-06 15:58:04 +0100

    added columns to hearingVenue table

---
+++ BookingsApi/BookingsApi.DAL/Migrations/20230706141056_AddColumnToHearingVenuesTable.cs
@@ -0,0 +1,38 @@
+﻿using BookingsApi.Domain;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookingsApi.DAL.Migrations
+{
+    public partial class AddColumnToHearingVenuesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IsScottish",
+                table: nameof(HearingVenue),
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+            
+            migrationBuilder.AddColumn<string>(
+                name: "IsWorkAllocationEnabled",
+                table: nameof(HearingVenue),
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsScottish",
+                table: nameof(HearingVenue));
+            
+            migrationBuilder.DropColumn(
+                name: "IsWorkAllocationEnabled",
+                table: nameof(HearingVenue));
+        }
+    }
+}
