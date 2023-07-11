cfabd06c4ed5f5c2d80c9460d6c5a0bba2e7b4a8
shaed-parkar <41630528+shaed-parkar@users.noreply.github.com>
2023-07-11 17:57:06 +0100

    VIH-9981 rename epmis code to venue code (#669)
    * Add migration to rename EpimsCode to VenueCode
    * Migrate HearingVenue AC tests to non-specflow version
    * Migrate case types ac tests to non-specflow

---
+++ BookingsApi/BookingsApi.DAL/Migrations/20230706083211_RenameEpmisCodeToVenueCode.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookingsApi.DAL.Migrations
+{
+    public partial class RenameEpmisCodeToVenueCode : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "EpimsCode",
+                table: "HearingVenue",
+                newName: "VenueCode");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_HearingVenue_EpimsCode",
+                table: "HearingVenue",
+                newName: "IX_HearingVenue_VenueCode");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "VenueCode",
+                table: "HearingVenue",
+                newName: "EpimsCode");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_HearingVenue_VenueCode",
+                table: "HearingVenue",
+                newName: "IX_HearingVenue_EpimsCode");
+        }
+    }
+}
