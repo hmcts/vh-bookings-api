dd8b9515e23e17ce6d21d6aeaed48edacc20ba36
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-07-18 17:59:54 +0100

    Add env variables for dbcontext factory

--- BookingsApi/BookingsApi.DAL/DesignTimeHearingsContextFactory.cs
+++ BookingsApi/BookingsApi.DAL/DesignTimeHearingsContextFactory.cs
@@ -13,6 +13,7 @@ namespace BookingsApi.DAL
             var config = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json", true)
                 .AddUserSecrets("D76B6EB8-F1A2-4A51-9B8F-21E1B6B81E4F")
+                .AddEnvironmentVariables()
                 .Build();
             var builder = new DbContextOptionsBuilder<BookingsDbContext>();
             builder.UseSqlServer(config.GetConnectionString("VhBookings"));
