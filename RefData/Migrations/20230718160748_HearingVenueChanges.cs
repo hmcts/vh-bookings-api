caa1fd6d5f584d728300e35c30bd6c6415d84721
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-07-18 18:09:18 +0100

    house keeping migration file

--- RefData/Migrations/20230718160748_HearingVenueChanges.cs
+++ RefData/Migrations/20230718160748_HearingVenueChanges.cs
@@ -10,29 +10,25 @@ namespace RefData.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            Console.WriteLine("Running 9777_rename_venues_and_add_venue_codes");
             var sqlFile1 = Path.Combine("data/9777_rename_venues_and_add_venue_codes.sql");
             RunSqlFile(File.ReadAllText(sqlFile1), migrationBuilder);
-            Console.WriteLine("Completed 9777_rename_venues_and_add_venue_codes");
-            // migrationBuilder.Sql(File.ReadAllText(sqlFile1));
-
-            Console.WriteLine("Running 9965_add_private_law_case_type");
+            
             var sqlFile2 = Path.Combine("data/9965_add_private_law_case_type.sql");
-            // migrationBuilder.Sql(File.ReadAllText(sqlFile2));
             RunSqlFile(File.ReadAllText(sqlFile2), migrationBuilder);
-            Console.WriteLine("Complete 9965_add_private_law_case_type");
-
-            Console.WriteLine("Running 9987_add_venue_codes");
+            
             var sqlFile3 = Path.Combine("data/9987_add_venue_codes.sql");
-            // migrationBuilder.Sql(File.ReadAllText(sqlFile3));
             RunSqlFile(File.ReadAllText(sqlFile3), migrationBuilder);
-            Console.WriteLine("Completed 9987_add_venue_codes");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
         }
 
+        /// <summary>
+        /// EF Core does not like GO statements in the migration files, so we need to split the file into batches
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="migrationBuilder"></param>
         private void RunSqlFile(string sql, MigrationBuilder migrationBuilder)
         {
             string[] batches = sql.Split(new [] {"GO;"}, StringSplitOptions.None);
