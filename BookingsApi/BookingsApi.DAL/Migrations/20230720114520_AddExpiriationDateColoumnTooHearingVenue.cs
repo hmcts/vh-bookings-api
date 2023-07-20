5df79465a6f7bf6024107ecee7879e247b55714b
William Craig <mr.craig1991@gmail.com>
2023-07-20 12:46:13 +0100

    add new column migration

---
+++ BookingsApi/BookingsApi.DAL/Migrations/20230720114520_AddExpiriationDateColoumnTooHearingVenue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookingsApi.DAL.Migrations
+{
+    public partial class AddExpiriationDateColoumnTooHearingVenue : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExpirationDate",
+                table: "HearingVenue",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpirationDate",
+                table: "HearingVenue");
+        }
+    }
+}
