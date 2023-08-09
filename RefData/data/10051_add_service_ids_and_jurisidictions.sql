59067421c9a19b058554d45619e6017a706e1b53
Oliver Scott <oliver.scott@justice.gov.uk>
2023-08-09 11:00:00 +0100

    Update service ids and jurisdictions

---
+++ RefData/data/10051_add_service_ids_and_jurisidictions.sql
@@ -0,0 +1,60 @@
+SET XACT_ABORT ON
+GO
+
+CREATE OR ALTER PROC #Jurisdiction_CreateIfNotExist @name varchar(max)
+AS
+BEGIN
+    IF NOT EXISTS(SELECT TOP 1 1 FROM Jurisdiction WHERE Name = @name)
+        BEGIN
+            INSERT INTO Jurisdiction (Code, Name, IsLive, CreatedDate, UpdatedDate) VALUES (@name, @name, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+        END
+END
+GO
+
+CREATE OR ALTER PROC #CaseType_Update @name varchar(max), @serviceId nvarchar(max), @jurisdictionName nvarchar(max)
+AS
+BEGIN
+    DECLARE @jurisdictionId int = (SELECT Id FROM Jurisdiction WHERE Name = @jurisdictionName)
+
+    IF EXISTS(SELECT TOP 1 1 FROM CaseType WHERE Name = @name)
+        BEGIN
+            UPDATE CaseType SET ServiceId = @serviceId, JurisdictionId = @jurisdictionId WHERE Name = @name
+        END
+    ELSE
+        BEGIN
+            Print ('WARNING! Could not find case type with the name: ' + @name);
+        END
+END
+GO
+
+BEGIN TRANSACTION
+
+-- Add jurisdictions
+EXEC #Jurisdiction_CreateIfNotExist 'Health, Education and Social Care Chamber'
+EXEC #Jurisdiction_CreateIfNotExist 'General Regulatory Chamber'
+EXEC #Jurisdiction_CreateIfNotExist 'Immigration and Asylum Chamber'
+EXEC #Jurisdiction_CreateIfNotExist 'Property Chamber'
+EXEC #Jurisdiction_CreateIfNotExist 'War Pensions and Armed Forces Compensation Chamber'
+
+-- Update case types
+EXEC #CaseType_Update 'Adoption', 'ABA4', 'Family'
+EXEC #CaseType_Update 'Asylum Support', 'BBA1', 'Social Entitlement Chamber'
+EXEC #CaseType_Update 'Care Standards', 'BCA1', 'Health, Education and Social Care Chamber'
+EXEC #CaseType_Update 'Criminal Injuries Compensation', 'BBA2', 'Social Entitlement Chamber'
+EXEC #CaseType_Update 'Divorce', 'ABA1', 'Family'
+EXEC #CaseType_Update 'Financial Remedy', 'ABA2', 'Family'
+EXEC #CaseType_Update 'GRC - Charity', 'BAA2', 'General Regulatory Chamber'
+EXEC #CaseType_Update 'GRC - Environment', 'BAA5', 'General Regulatory Chamber'
+EXEC #CaseType_Update 'GRC - Estate Agents', 'BAA6', 'General Regulatory Chamber'
+EXEC #CaseType_Update 'GRC - Gambling', 'BAA8 ', 'General Regulatory Chamber'
+EXEC #CaseType_Update 'GRC - Immigration Services', 'BAA9 ', 'General Regulatory Chamber'
+EXEC #CaseType_Update 'GRC - Information Rights', 'BAB1', 'General Regulatory Chamber'
+EXEC #CaseType_Update 'Immigration and Asylum', 'BFA1', 'Immigration and Asylum Chamber'
+EXEC #CaseType_Update 'Land Registration', 'BGA2', 'Property Chamber'
+EXEC #CaseType_Update 'Mental Health', 'BCA2', 'Health, Education and Social Care Chamber'
+EXEC #CaseType_Update 'Special Educational Needs and Disability', 'BCA4', 'Health, Education and Social Care Chamber'
+EXEC #CaseType_Update 'War Pensions Appeals', 'BEA1', 'War Pensions and Armed Forces Compensation Chamber'
+
+COMMIT
+
+SET XACT_ABORT OFF
