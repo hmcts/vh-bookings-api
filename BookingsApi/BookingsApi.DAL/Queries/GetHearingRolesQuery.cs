4c6224bb5e4985604a0a9f8110981baf048206f0
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-08-31 17:29:53 +0100

    Add query to get all hearing roles

---
+++ BookingsApi/BookingsApi.DAL/Queries/GetHearingRolesQuery.cs
@@ -0,0 +1,26 @@
+namespace BookingsApi.DAL.Queries;
+
+/// <summary>
+/// Get all standard hearing roles (i.e. not linked to a case role)
+/// </summary>
+public class GetHearingRolesQuery : IQuery
+{
+    
+}
+
+public class GetHearingRolesQueryHandler : IQueryHandler<GetHearingRolesQuery, List<HearingRole>>
+{
+    private readonly BookingsDbContext _context;
+
+    public GetHearingRolesQueryHandler(BookingsDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<HearingRole>> Handle(GetHearingRolesQuery query)
+    {
+        return await _context.HearingRoles
+            .Where(hr => hr.CaseRoleId == null)
+            .ToListAsync();
+    }
+}
