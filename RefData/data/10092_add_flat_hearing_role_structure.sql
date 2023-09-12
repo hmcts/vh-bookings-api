248be5f8f6832566092a572120acfd9d995555ef
William Craig <william.craig2@justice.gov.uk>
2023-09-12 11:03:39 +0100

    VIH-10104 change ref data scripts to use identiy insert (#714)
    * Added insertIdentity to private law script
    
    * Added insertIdentity to private jurisdiction script
    
    * Added insertIdentity to private flat hearing role script
    
    * Added insertIdentity to hearing venue insert script
    
    * Typos
    
    * Removed insertIdentity ON from venue script
    
    * Added migration for  missing ServiceID script and Chesterfield script sql inserts

--- RefData/data/10092_add_flat_hearing_role_structure.sql
+++ RefData/data/10092_add_flat_hearing_role_structure.sql
@@ -3,14 +3,15 @@ GO;
 
 
 
-CREATE OR ALTER PROC #HearingRole_CreateIfNotExist @hearingRoleName nvarchar(max), @userRoleId varchar(450)
+CREATE OR ALTER PROC #HearingRole_CreateIfNotExist @id int, @hearingRoleName nvarchar(max), @userRoleId varchar(450)
 As
 BEGIN
     IF NOT EXISTS(SELECT * FROM VhBookings.dbo.HearingRole WHERE Name = @hearingRoleName AND UserRoleId = @userRoleId AND CaseRoleId IS NULL)
         BEGIN
             Print ('Adding new role ' + @hearingRoleName);
-            INSERT INTO VhBookings.dbo.HearingRole (Name, UserRoleId, CaseRoleId, Live, CreatedDate, UpdatedDate) VALUES (@hearingRoleName, @userRoleId, NULL, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
-            declare @id int = (SELECT SCOPE_IDENTITY());
+            SET IDENTITY_INSERT VhBookings.dbo.HearingRole ON
+            INSERT INTO VhBookings.dbo.HearingRole (Id, Name, UserRoleId, CaseRoleId, Live, CreatedDate, UpdatedDate) VALUES (@id, @hearingRoleName, @userRoleId, NULL, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+            SET IDENTITY_INSERT VhBookings.dbo.HearingRole OFF
             Print ('NEW ID IS ' + CAST(@id AS VARCHAR));
         END
 END
@@ -25,27 +26,27 @@ DECLARE @JOH int = (SELECT ID From VhBookings.dbo.UserRole WHERE Name = 'Judicia
 DECLARE @Judge int = (SELECT ID From VhBookings.dbo.UserRole WHERE Name = 'Judge');
 DECLARE @StaffMember int = (SELECT ID From VhBookings.dbo.UserRole WHERE Name = 'Staff Member');
 
-EXEC #HearingRole_CreateIfNotExist @hearingRoleName = 'Applicant', @userRoleId = @Individual;
-EXEC #HearingRole_CreateIfNotExist @hearingRoleName = 'Appellant', @userRoleId = @Individual;
-EXEC #HearingRole_CreateIfNotExist @hearingRoleName = 'Barrister', @userRoleId = @Representative;
-EXEC #HearingRole_CreateIfNotExist @hearingRoleName = 'Claimant', @userRoleId = @Individual;
-EXEC #HearingRole_CreateIfNotExist @hearingRoleName = 'Defendant', @userRoleId = @Individual;
-EXEC #HearingRole_CreateIfNotExist @hearingRoleName = 'Expert', @userRoleId = @Individual;
-EXEC #HearingRole_CreateIfNotExist @hearingRoleName = 'Welfare Representative', @userRoleId = @Individual;
-EXEC #HearingRole_CreateIfNotExist @hearingRoleName = 'Intermediaries', @userRoleId = @Individual;
-EXEC #HearingRole_CreateIfNotExist @hearingRoleName = 'Interpreter', @userRoleId = @Individual;
-EXEC #HearingRole_CreateIfNotExist @hearingRoleName = 'Joint Party', @userRoleId = @Individual;
-EXEC #HearingRole_CreateIfNotExist @hearingRoleName = 'Legal Representative', @userRoleId = @Representative;
-EXEC #HearingRole_CreateIfNotExist @hearingRoleName = 'Litigation Friend', @userRoleId = @Individual;
-EXEC #HearingRole_CreateIfNotExist @hearingRoleName = 'Observer', @userRoleId = @Individual;
-EXEC #HearingRole_CreateIfNotExist @hearingRoleName = 'Other Party', @userRoleId = @Individual;
-EXEC #HearingRole_CreateIfNotExist @hearingRoleName = 'Police', @userRoleId = @Individual;
-EXEC #HearingRole_CreateIfNotExist @hearingRoleName = 'Respondent', @userRoleId = @Individual;
-EXEC #HearingRole_CreateIfNotExist @hearingRoleName = 'Support', @userRoleId = @Individual;
-EXEC #HearingRole_CreateIfNotExist @hearingRoleName = 'Panel Member', @userRoleId = @JOH;
-EXEC #HearingRole_CreateIfNotExist @hearingRoleName = 'Witness', @userRoleId = @Individual;
-EXEC #HearingRole_CreateIfNotExist @hearingRoleName = 'Judge', @userRoleId = @Judge;
-EXEC #HearingRole_CreateIfNotExist @hearingRoleName = 'Staff Member', @userRoleId = @StaffMember;
+EXEC #HearingRole_CreateIfNotExist @id = 1191, @hearingRoleName = 'Applicant', @userRoleId = @Individual;
+EXEC #HearingRole_CreateIfNotExist @id = 1192, @hearingRoleName = 'Appellant', @userRoleId = @Individual;
+EXEC #HearingRole_CreateIfNotExist @id = 1193, @hearingRoleName = 'Barrister', @userRoleId = @Representative;
+EXEC #HearingRole_CreateIfNotExist @id = 1194, @hearingRoleName = 'Claimant', @userRoleId = @Individual;
+EXEC #HearingRole_CreateIfNotExist @id = 1195, @hearingRoleName = 'Defendant', @userRoleId = @Individual;
+EXEC #HearingRole_CreateIfNotExist @id = 1196, @hearingRoleName = 'Expert', @userRoleId = @Individual;
+EXEC #HearingRole_CreateIfNotExist @id = 1197, @hearingRoleName = 'Welfare Representative', @userRoleId = @Individual;
+EXEC #HearingRole_CreateIfNotExist @id = 1198, @hearingRoleName = 'Intermediaries', @userRoleId = @Individual;
+EXEC #HearingRole_CreateIfNotExist @id = 1199, @hearingRoleName = 'Interpreter', @userRoleId = @Individual;
+EXEC #HearingRole_CreateIfNotExist @id = 1200, @hearingRoleName = 'Joint Party', @userRoleId = @Individual;
+EXEC #HearingRole_CreateIfNotExist @id = 1201, @hearingRoleName = 'Legal Representative', @userRoleId = @Representative;
+EXEC #HearingRole_CreateIfNotExist @id = 1202, @hearingRoleName = 'Litigation Friend', @userRoleId = @Individual;
+EXEC #HearingRole_CreateIfNotExist @id = 1203, @hearingRoleName = 'Observer', @userRoleId = @Individual;
+EXEC #HearingRole_CreateIfNotExist @id = 1204, @hearingRoleName = 'Other Party', @userRoleId = @Individual;
+EXEC #HearingRole_CreateIfNotExist @id = 1205, @hearingRoleName = 'Police', @userRoleId = @Individual;
+EXEC #HearingRole_CreateIfNotExist @id = 1206, @hearingRoleName = 'Respondent', @userRoleId = @Individual;
+EXEC #HearingRole_CreateIfNotExist @id = 1207, @hearingRoleName = 'Support', @userRoleId = @Individual;
+EXEC #HearingRole_CreateIfNotExist @id = 1208, @hearingRoleName = 'Panel Member', @userRoleId = @JOH;
+EXEC #HearingRole_CreateIfNotExist @id = 1209, @hearingRoleName = 'Witness', @userRoleId = @Individual;
+EXEC #HearingRole_CreateIfNotExist @id = 1210, @hearingRoleName = 'Judge', @userRoleId = @Judge;
+EXEC #HearingRole_CreateIfNotExist @id = 1211, @hearingRoleName = 'Staff Member', @userRoleId = @StaffMember;
 
 COMMIT
 SET XACT_ABORT OFF
