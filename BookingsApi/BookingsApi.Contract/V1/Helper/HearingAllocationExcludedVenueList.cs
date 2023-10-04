8e265f0f04e2c44b18092e82860bdeda94b269f3
Oliver Scott <oliver.scott@justice.gov.uk>
2023-10-04 09:14:29 +0100

    VIH-10130 Add Teesside venues back in to work allocation script (#728)
    * Update work allocation enabled for teesside venues
    
    * Fix failing test
    
    * Update exclusion list

--- BookingsApi/BookingsApi.Contract/V1/Helper/HearingAllocationExcludedVenueList.cs
+++ BookingsApi/BookingsApi.Contract/V1/Helper/HearingAllocationExcludedVenueList.cs
@@ -10,8 +10,6 @@ public static class HearingAllocationExcludedVenueList
     [Obsolete("Use the IsWorkAllocationEnabled property on the HearingVenue object instead of this list")]
     public static readonly IReadOnlyList<string> ExcludedHearingVenueNames = new List<string> (HearingScottishVenueNames.ScottishHearingVenuesList)
     {
-        "Teesside Combined Court Centre",
-        "Teesside Magistrates Court",
         "Middlesbrough County Court"
     };
 }
