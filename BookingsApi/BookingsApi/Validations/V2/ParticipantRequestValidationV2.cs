437b8679085fc7f3171a4c080a152d731e0e384b
mk <murali.katuri@hmcts.net>
2023-10-06 15:24:10 +0100

    Fixed merge issue

--- BookingsApi/BookingsApi/Validations/V2/ParticipantRequestValidationV2.cs
+++ BookingsApi/BookingsApi/Validations/V2/ParticipantRequestValidationV2.cs
@@ -5,14 +5,9 @@ namespace BookingsApi.Validations.V2
 {
     public class ParticipantRequestValidationV2 : AbstractValidator<ParticipantRequestV2>
     {
-        private const string NameRegex = "^(\\w+(?:\\w|[\\s'._-](?![\\s'._-]))*\\w+)$";
-        public static readonly string FirstNameDoesntMatchRegex = "First name must match regular expression";
-        public static readonly string LastNameDoesntMatchRegex = "Last name must match regular expression";
         public static readonly string NoDisplayNameErrorMessage = "Display name is required";
         public static readonly string InvalidDisplayNameErrorMessage = "Display name will accept upto 255 alphanumeric characters, spaces, and the following special characters: ',._-";
         public static readonly string NoHearingRoleCodeErrorMessage = "Hearing role code is required";
-        public static readonly string NoFirstNameErrorMessage = "First name is required";
-        public static readonly string NoLastNameErrorMessage = "Last name is required";
         public static readonly string NoUsernameErrorMessage = "Username is required";
         public static readonly string NoTelephoneNumberErrorMessage = "Telephone Number is required";
         public static readonly string NoContactEmailErrorMessage = "Contact Email is required";
@@ -25,10 +20,6 @@ namespace BookingsApi.Validations.V2
             RuleFor(x => x.FirstName).Matches(ParticipantValidationV2.NameRegex).WithMessage(ParticipantValidationV2.FirstNameDoesntMatchRegex);
             RuleFor(x => x.LastName).NotEmpty().WithMessage(ParticipantValidationV2.NoLastNameErrorMessage);
             RuleFor(x => x.LastName).Matches(ParticipantValidationV2.NameRegex).WithMessage(ParticipantValidationV2.LastNameDoesntMatchRegex);
-            RuleFor(x => x.FirstName).NotEmpty().WithMessage(NoFirstNameErrorMessage);
-            RuleFor(x => x.FirstName).Matches(NameRegex).WithMessage(FirstNameDoesntMatchRegex);
-            RuleFor(x => x.LastName).NotEmpty().WithMessage(NoLastNameErrorMessage);
-            RuleFor(x => x.LastName).Matches(NameRegex).WithMessage(LastNameDoesntMatchRegex);
             RuleFor(x => x.ContactEmail).NotEmpty().WithMessage(NoContactEmailErrorMessage).Must(x => x.IsValidEmail()).WithMessage(InvalidContactEmailErrorMessage);
 
             var regex = "^([-A-Za-z0-9 ',._]){1,255}$";
