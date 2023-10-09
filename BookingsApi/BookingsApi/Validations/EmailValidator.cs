ca407ba54d77c1a2e038903da0ee2cef345735e6
William Craig <william.craig2@justice.gov.uk>
2023-10-09 09:45:19 +0100

    VIH-10221 email validator (#729)
    * Fix to align email validator with admin web regex
    
    * unit test
    
    * Further refined regex and test samples
    
    * added a couple more test cases
    
    * Changed queue
    
    * Bumping chart version
    
    * removed queue and merged master
    
    * Fix for acceptance test
    
    ---------
    
    Co-authored-by: hmcts-ado-cnp <174565+hmcts-ado-cnp[bot]@users.noreply.github.com>

--- BookingsApi/BookingsApi/Validations/EmailValidator.cs
+++ BookingsApi/BookingsApi/Validations/EmailValidator.cs
@@ -5,7 +5,7 @@ namespace BookingsApi.Validations
     /// <summary>Simple validator to check email formats</summary>
     public static class EmailValidator
     {
-        private const string RegexPattern = @"^([!#-'*/-9=?A-Z^-~-][^&@.]+(\.[!#-'*/-9=?A-Z^-~-][^&@.]+)*)@([!#-'*/-9=?A-Z^-~-][^&]*(\.[!#-'*/-9=?A-Z^-~-][^&]+)+)$";
+        private const string RegexPattern = @"^[!#$%'*/-9=?A-Z^-~-]+(?:\.[!#$%'*/-9=?A-Z^-~-]+)*@[a-zA-Z0-9]+([a-zA-Z0-9-]+)*(?:\.[a-zA-Z0-9-]+)*\.[a-zA-Z]{2,}$";
         
         /// <summary>
         /// Test if the given string is specified and a valid email address
