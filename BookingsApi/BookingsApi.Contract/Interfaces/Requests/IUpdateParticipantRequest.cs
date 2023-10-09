3b9893c3f4d1fa106f387c1ca2df430a0b9a6d89
Oliver Scott <oliver.scott@justice.gov.uk>
2023-10-09 16:19:31 +0100

    Fix after merge

--- BookingsApi/BookingsApi.Contract/Interfaces/Requests/IUpdateParticipantRequest.cs
+++ BookingsApi/BookingsApi.Contract/Interfaces/Requests/IUpdateParticipantRequest.cs
@@ -5,6 +5,5 @@ namespace BookingsApi.Contract.Interfaces.Requests
     public interface IUpdateParticipantRequest : IRepresentativeInfoRequest
     {
         public Guid ParticipantId { get; set; }
-        public string ContactEmail { get; set; }
     }
 }
