5713b0c1d86c64d55acbe5fa0d427143960a0bf9
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-10-13 10:16:40 +0100

    remove username from IParticipantRequest.cs

--- BookingsApi/BookingsApi.Contract/Interfaces/Requests/IParticipantRequest.cs
+++ BookingsApi/BookingsApi.Contract/Interfaces/Requests/IParticipantRequest.cs
@@ -3,7 +3,6 @@ namespace BookingsApi.Contract.Interfaces.Requests
     public interface IParticipantRequest : IRepresentativeInfoRequest
     {
         public string ContactEmail { get; set; }
-        public string Username { get; set; }
         public string DisplayName { get; set; }
     }
 }
