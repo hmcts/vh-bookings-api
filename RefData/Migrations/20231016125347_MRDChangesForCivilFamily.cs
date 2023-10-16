c71d9cba1cf0c2e89e7d5b41ed21178ef166c7b0
William Craig <mr.craig1991@gmail.com>
2023-10-16 15:54:54 +0100

    Created MRD ref data scripts and migration

---
+++ RefData/Migrations/20231016125347_MRDChangesForCivilFamily.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RefData.Migrations
+{
+    public partial class MRDChangesForCivilFamily : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            SqlFileHelper.RunSqlFile("data/10178_mrd_changes_to_civil_case_types.sql", migrationBuilder);
+            SqlFileHelper.RunSqlFile("data/10181_mrd_changes_family_case_types.sql", migrationBuilder);
+            SqlFileHelper.RunSqlFile("data/10192_mrd_changes_to_Tribunal_case_types.sql", migrationBuilder);
+            SqlFileHelper.RunSqlFile("data/10237_mrd_add_service_id_for_generic.sql", migrationBuilder);
+            SqlFileHelper.RunSqlFile("data/10194_expire_case_types_not_in_mrd.sql", migrationBuilder);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
---
+++ RefData/data/10178_mrd_changes_to_civil_case_types.sql
@@ -0,0 +1,33 @@
+USE vhbookings;
+SET XACT_ABORT ON
+GO
+
+
+CREATE PROCEDURE #CREATE_CASE_TYPES_V10178 @CaseTypeId INT, @name varchar(max), @serviceId varchar(max)
+AS
+DECLARE @JurisdictionId INT = (SELECT Id FROM dbo.Jurisdiction WHERE Code = 'Civil')
+BEGIN IF NOT EXISTS(SELECT TOP 1 1 FROM dbo.CaseType WHERE Id = @CaseTypeId)
+BEGIN
+        INSERT INTO CaseType (Id, Name, CreatedDate, UpdatedDate, JurisdictionId, Live, ServiceId)
+        VALUES (@CaseTypeId, @name, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, @JurisdictionId, 1, @serviceId)
+
+END
+END
+GO
+
+BEGIN TRANSACTION
+
+SET IDENTITY_INSERT CaseType ON
+EXEC #CREATE_CASE_TYPES_V10178 56, 'Civil Enforcement', 'AAA1';
+EXEC #CREATE_CASE_TYPES_V10178 57, 'Insolvency', 'AAA2';
+EXEC #CREATE_CASE_TYPES_V10178 58, 'Mortgage and Landlord Possession Claims', 'AAA3';
+EXEC #CREATE_CASE_TYPES_V10178 59, 'Non-money Claims', 'AAA4';
+EXEC #CREATE_CASE_TYPES_V10178 60, 'Return of Goods Claims', 'AAA5';
+EXEC #CREATE_CASE_TYPES_V10178 61, 'Specified Money Claims', 'AAA6';
+EXEC #CREATE_CASE_TYPES_V10178 62, 'Damages', 'AAA7';
+SET IDENTITY_INSERT CaseType OFF
+
+SELECT * FROM CaseType WHERE ID >= 56 AND ID <= 62
+
+    COMMIT
+SET XACT_ABORT OFF
