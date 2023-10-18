d83f28feb2b36cd3610c9326876613e6c0ebb562
marcogagliardi <marco.gagliardi@justice.gov.uk>
2023-10-18 14:39:19 +0100

    added integration test

--- BookingsApi/BookingsApi.IntegrationTests/Api/V1/Hearings/GetHearingsForNotificationTest.cs
+++ BookingsApi/BookingsApi.IntegrationTests/Api/V1/Hearings/GetHearingsForNotificationTest.cs
@@ -1,11 +1,23 @@
+using BookingsApi.Contract.V1.Responses;
+
 namespace BookingsApi.IntegrationTests.Api.V1.Hearings;
 
 public class GetHearingsForNotificationTest: ApiTest
 {
     [Test]
-    public async Task should_return_ok()
+    public async Task should_return_only_first_day_of_multibooking()
     {
         // arrange
+        var startingDate = DateTime.UtcNow.AddDays(2);
+        var hearing1 = await Hooks.SeedVideoHearing(isMultiDayFirstHearing:true, configureOptions: options =>
+        {
+            options.ScheduledDate = startingDate;
+        });
+
+        var sourceId = hearing1.SourceId;
+
+        var dates = new List<DateTime> {startingDate.AddDays(3), startingDate.AddDays(4)};
+        await Hooks.CloneVideoHearing(hearing1.Id, dates);
 
         // act
         using var client = Application.CreateClient();
@@ -14,6 +26,10 @@ public class GetHearingsForNotificationTest: ApiTest
         // assert
         result.IsSuccessStatusCode.Should().BeTrue();
         result.StatusCode.Should().Be(HttpStatusCode.OK);
+        
+        var hearingsForNotification = await ApiClientResponse.GetResponses<List<HearingNotificationResponse>>(result.Content);
+        hearingsForNotification.Count.Should().Be(1);
+        hearingsForNotification[0].Hearing.GroupId.Should().Be(hearing1.Id);
     }
     
 }
