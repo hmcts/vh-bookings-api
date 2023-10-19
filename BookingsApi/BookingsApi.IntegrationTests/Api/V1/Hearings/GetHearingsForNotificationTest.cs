0937f0f67d95ae824a42143ae4ccac75deaa6162
marcogagliardi <marco.gagliardi@justice.gov.uk>
2023-10-19 07:51:38 +0100

    Update GetHearingsForNotificationTest.cs

--- BookingsApi/BookingsApi.IntegrationTests/Api/V1/Hearings/GetHearingsForNotificationTest.cs
+++ BookingsApi/BookingsApi.IntegrationTests/Api/V1/Hearings/GetHearingsForNotificationTest.cs
@@ -10,52 +10,52 @@ public class GetHearingsForNotificationTest: ApiTest
         // arrange
         
         // Multi day hearing before the cutoff
-        var startingDate1 = DateTime.UtcNow.AddHours(6);
-        var hearing1 = await Hooks.SeedVideoHearing(isMultiDayFirstHearing:true, configureOptions: options =>
+        var startingDate1 = DateTime.UtcNow.Date.AddHours(6);
+        var multiDayHearingBeforeCutOff = await Hooks.SeedVideoHearing(isMultiDayFirstHearing:true, configureOptions: options =>
         {
             options.ScheduledDate = startingDate1;
         });
         
         var dates1 = new List<DateTime> {startingDate1.AddDays(1), startingDate1.AddDays(2)};
-        await Hooks.CloneVideoHearing(hearing1.Id, dates1);
+        await Hooks.CloneVideoHearing(multiDayHearingBeforeCutOff.Id, dates1);
         
         // Multi day hearing after the cutoff
-        var startingDate2 = DateTime.UtcNow.AddDays(4);
-        var hearing2 = await Hooks.SeedVideoHearing(isMultiDayFirstHearing:true, configureOptions: options =>
+        var startingDate2 = DateTime.UtcNow.Date.AddDays(4);
+        var multiDayHearingAfterCutOff = await Hooks.SeedVideoHearing(isMultiDayFirstHearing:true, configureOptions: options =>
         {
             options.ScheduledDate = startingDate2;
         });
         
         var dates2 = new List<DateTime> {startingDate2.AddDays(5), startingDate2.AddDays(6)};
-        await Hooks.CloneVideoHearing(hearing2.Id, dates2);
+        await Hooks.CloneVideoHearing(multiDayHearingAfterCutOff.Id, dates2);
         
         // Multi day hearing within the cutoff
-        var startingDate3 = DateTime.UtcNow.AddDays(2);
-        var hearing3 = await Hooks.SeedVideoHearing(isMultiDayFirstHearing:true, configureOptions: options =>
+        var startingDate3 = DateTime.UtcNow.Date.AddDays(2);
+        var multiDayHearingWithinCutOff = await Hooks.SeedVideoHearing(isMultiDayFirstHearing:true, configureOptions: options =>
         {
             options.ScheduledDate = startingDate3;
         });
         
         var dates3 = new List<DateTime> {startingDate3.AddDays(3), startingDate3.AddDays(4)};
-        await Hooks.CloneVideoHearing(hearing3.Id, dates3);
+        await Hooks.CloneVideoHearing(multiDayHearingWithinCutOff.Id, dates3);
         
         // Single day hearing before the cutoff
-        var startingDate4 = DateTime.UtcNow.AddHours(6);
-        await Hooks.SeedVideoHearing(isMultiDayFirstHearing:false, configureOptions: options =>
+        var startingDate4 = DateTime.UtcNow.Date.AddHours(6);
+        var singleDayBeforeCutOff = await Hooks.SeedVideoHearing(isMultiDayFirstHearing:false, configureOptions: options =>
         {
             options.ScheduledDate = startingDate4;
         });
         
         // Single day hearing after the cutoff
-        var startingDate5 = DateTime.UtcNow.AddDays(4);
-        await Hooks.SeedVideoHearing(isMultiDayFirstHearing:true, configureOptions: options =>
+        var startingDate5 = DateTime.UtcNow.Date.AddDays(4);
+        var singleDayAfterCutOff = await Hooks.SeedVideoHearing(isMultiDayFirstHearing:true, configureOptions: options =>
         {
             options.ScheduledDate = startingDate5;
         });
         
         // Single day hearing within the cutoff
-        var startingDate6 = DateTime.UtcNow.AddDays(2).AddHours(3);
-        await Hooks.SeedVideoHearing(isMultiDayFirstHearing:false, configureOptions: options =>
+        var startingDate6 = DateTime.UtcNow.Date.AddDays(2).AddHours(3);
+        var singleDayWithinCutOff = await Hooks.SeedVideoHearing(isMultiDayFirstHearing:false, configureOptions: options =>
         {
             options.ScheduledDate = startingDate6;
         });
@@ -69,11 +69,14 @@ public class GetHearingsForNotificationTest: ApiTest
         result.StatusCode.Should().Be(HttpStatusCode.OK);
         
         var hearingsForNotification = await ApiClientResponse.GetResponses<List<HearingNotificationResponse>>(result.Content);
-        hearingsForNotification.Count.Should().Be(2);
-        hearingsForNotification[1].Hearing.GroupId.Should().Be(hearing3.Id);
-        hearingsForNotification[1].TotalDays.Should().Be(3);
-        hearingsForNotification[0].Hearing.GroupId.Should().NotHaveValue();
-        hearingsForNotification[0].TotalDays.Should().Be(1);
+        hearingsForNotification.Should().Contain(x=> x.Hearing.Id == singleDayWithinCutOff.Id);
+        hearingsForNotification.Find(x=> x.Hearing.Id == singleDayWithinCutOff.Id).TotalDays.Should().Be(1);
+        hearingsForNotification.Should().Contain(x=> x.Hearing.Id == multiDayHearingWithinCutOff.Id);
+        hearingsForNotification.Find(x=> x.Hearing.Id == multiDayHearingWithinCutOff.Id).TotalDays.Should().Be(3);
+        hearingsForNotification.Select(x => x.Hearing.Id).Should().NotContain(new List<Guid>()
+        {
+            singleDayBeforeCutOff.Id, singleDayAfterCutOff.Id, multiDayHearingBeforeCutOff.Id, multiDayHearingAfterCutOff.Id
+        });
     }
     
 }
