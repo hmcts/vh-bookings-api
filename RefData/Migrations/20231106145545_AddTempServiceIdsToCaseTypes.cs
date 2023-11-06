2b21460919b28ec8318f4ce2d0f68a14b4230827
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-11-06 15:03:21 +0000

    add script to insert temp data for missing service ids

---
+++ RefData/Migrations/20231106145545_AddTempServiceIdsToCaseTypes.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RefData.Migrations
+{
+    public partial class AddTempServiceIdsToCaseTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            SqlFileHelper.RunSqlFile("data/VIH-10303_add_temp_service_ids.sql", migrationBuilder);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
