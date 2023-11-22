9174c23109f284f810cbe4fbe188187fc76b6e7d
mk <murali.katuri@hmcts.net>
2023-11-22 10:37:56 +0000

    update test

--- BookingsApi/BookingsApi.IntegrationTests/Api/V2/Hearings/BookNewHearingV2Tests.cs
+++ BookingsApi/BookingsApi.IntegrationTests/Api/V2/Hearings/BookNewHearingV2Tests.cs
@@ -59,7 +59,6 @@ public class BookNewHearingV2Tests : ApiTest
         var serviceBusStub =
             Application.Services.GetService(typeof(IServiceBusQueueClient)) as ServiceBusQueueClientFake;
         var messages = serviceBusStub!.ReadAllMessagesFromQueue(hearingResponse.Id);
-        messages.Any(x => x.IntegrationEvent is ExistingParticipantHearingConfirmationEvent).Should().BeTrue();
         messages.Any(x => x.IntegrationEvent is HearingIsReadyForVideoIntegrationEvent).Should().BeTrue();
     }
 
