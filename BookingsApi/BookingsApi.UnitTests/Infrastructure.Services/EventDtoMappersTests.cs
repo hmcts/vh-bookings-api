143780ec91d7e5f5a7c7ced8ae7b97f2dc9c79ae
marcogagliardi <marco.gagliardi@justice.gov.uk>
2023-12-13 13:41:26 +0000

    Update EventDtoMappersTests.cs

--- BookingsApi/BookingsApi.UnitTests/Infrastructure.Services/EventDtoMappersTests.cs
+++ BookingsApi/BookingsApi.UnitTests/Infrastructure.Services/EventDtoMappersTests.cs
@@ -1,4 +1,8 @@
+using BookingsApi.Common;
+using BookingsApi.Domain;
 using BookingsApi.Domain.Enumerations;
+using BookingsApi.Domain.Participants;
+using BookingsApi.Domain.RefData;
 using BookingsApi.Infrastructure.Services;
 
 namespace BookingsApi.UnitTests.Infrastructure.Services;
@@ -6,30 +10,98 @@ namespace BookingsApi.UnitTests.Infrastructure.Services;
 public class EventDtoMappersTests
 {
     
-    [Test]
-    public void should_map_judiciary_participant_to_dto()
+    [TestCase(JudiciaryParticipantHearingRoleCode.Judge)]
+    [TestCase(JudiciaryParticipantHearingRoleCode.PanelMember)]
+    public void should_map_judiciary_participant_to_dto(JudiciaryParticipantHearingRoleCode hearingRoleCode)
     {
-        var hearing = new VideoHearingBuilder(addJudge:false).WithCase().WithJudiciaryPanelMember().WithJudiciaryJudge().Build();
-        var @case = hearing.GetCases()[0];
-        foreach (var participant in hearing.JudiciaryParticipants)
+        var @case = new Case("case number", "case name")
         {
-            var mappedParticipant =
-                EventDtoMappers.MapToHearingConfirmationDto(hearing.Id, hearing.ScheduledDateTime, participant,
-                    @case);
-            var role = (participant.HearingRoleCode == JudiciaryParticipantHearingRoleCode.Judge)
-                ? "Judge"
-                : "Judicial Office Holder";
-            mappedParticipant.UserRole.Should().Be(role);
-            mappedParticipant.Username.Should().Be(participant.JudiciaryPerson.Email);
-        }
+            IsLeadCase = true
+        };
 
-        foreach (var participant in hearing.Participants)
+        var judiciaryPerson = new JudiciaryPersonBuilder("Personal Code").Build();
+        var hearingId = Guid.NewGuid();
+        var scheduledTime = DateTime.Now.AddDays(2);
+        
+        
+        
+        var judiciaryParticipant = new JudiciaryParticipant("DisplayName", judiciaryPerson, hearingRoleCode);
+        var mappedParticipant = EventDtoMappers.MapToHearingConfirmationDto(hearingId, scheduledTime, judiciaryParticipant, @case);
+        var role = (judiciaryParticipant.HearingRoleCode == JudiciaryParticipantHearingRoleCode.Judge)
+            ? "Judge"
+            : "Judicial Office Holder";
+
+        mappedParticipant.HearingId.Should().Be(hearingId);
+
+        mappedParticipant.ScheduledDateTime.Should().Be(scheduledTime);
+        mappedParticipant.CaseName.Should().Be(@case.Name);
+        mappedParticipant.CaseNumber.Should().Be(@case.Number);
+        mappedParticipant.DisplayName.Should().Be(judiciaryParticipant.DisplayName);
+        mappedParticipant.Representee.Should().Be("");
+        mappedParticipant.Username.Should().Be(judiciaryParticipant.JudiciaryPerson.Email);
+                    
+        mappedParticipant.ContactEmail.Should().Be(judiciaryParticipant.JudiciaryPerson.Email);
+        mappedParticipant.ContactTelephone.Should().Be(judiciaryParticipant.JudiciaryPerson.WorkPhone);
+        mappedParticipant.FirstName.Should().Be(judiciaryParticipant.JudiciaryPerson.KnownAs);
+        mappedParticipant.LastName.Should().Be(judiciaryParticipant.JudiciaryPerson.Surname);
+        mappedParticipant.ParticipnatId.Should().Be(judiciaryParticipant.Id);
+        mappedParticipant.UserRole.Should().Be(role);
+    }
+    
+    [TestCase(CaseRoleGroup.Judge)]
+    [TestCase(CaseRoleGroup.StaffMember)]
+    public void should_map_participant_to_dto(CaseRoleGroup hearingRoleCode)
+    {
+        var @case = new Case("case number", "case name")
         {
-            var mappedParticipant =
-                EventDtoMappers.MapToHearingConfirmationDto(hearing.Id, hearing.ScheduledDateTime, participant,
-                    @case);
-            mappedParticipant.UserRole.Should().Be(participant.HearingRole.UserRole.Name);
-            mappedParticipant.Username.Should().Be(participant.Person.Username);
+            IsLeadCase = true
+        };
+
+        
+        var hearingId = Guid.NewGuid();
+        var scheduledTime = DateTime.Now.AddDays(2);
+
+        var caseRole = new CaseRole(1,"CaseRoleName");
+        var hearingRole = new HearingRole(1, "RoleName");
+        var person = new PersonBuilder(true).Build();
+
+        Participant participant = null;
+        switch (hearingRoleCode)
+        {
+            case CaseRoleGroup.Judge:
+                participant = new Judge(person, hearingRole, caseRole)
+                {
+                    DisplayName = "Display Name",
+                    CreatedBy = "Created By",
+                    HearingRole = new HearingRole(1, "Judge") {UserRole = new UserRole(1, "Judge")}
+                };
+                break;
+            case CaseRoleGroup.StaffMember:
+                participant = new StaffMember(person, hearingRole, caseRole)
+                {
+                    DisplayName = "Display Name",
+                    CreatedBy = "Created By",
+                    HearingRole = new HearingRole(1, "StaffMember") {UserRole = new UserRole(1, "StaffMember")}
+                };
+                break;
         }
+        
+        var mappedParticipant = EventDtoMappers.MapToHearingConfirmationDto(hearingId, scheduledTime, participant, @case);
+
+        mappedParticipant.HearingId.Should().Be(hearingId);
+
+        mappedParticipant.ScheduledDateTime.Should().Be(scheduledTime);
+        mappedParticipant.CaseName.Should().Be(@case.Name);
+        mappedParticipant.CaseNumber.Should().Be(@case.Number);
+        mappedParticipant.DisplayName.Should().Be(participant.DisplayName);
+        mappedParticipant.Representee.Should().Be("");
+        mappedParticipant.Username.Should().Be(participant.Person.Username);
+                    
+        mappedParticipant.ContactEmail.Should().Be(participant.Person.ContactEmail);
+        mappedParticipant.ContactTelephone.Should().Be(participant.Person.TelephoneNumber);
+        mappedParticipant.FirstName.Should().Be(participant.Person.FirstName);
+        mappedParticipant.LastName.Should().Be(participant.Person.LastName);
+        mappedParticipant.ParticipnatId.Should().Be(participant.Id);
+        mappedParticipant.UserRole.Should().Be(participant.HearingRole.UserRole.Name);
     }
 }
