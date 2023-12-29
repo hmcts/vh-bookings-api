a3761698199d0b6989adaffe5dd662759a2b8dde
mk <murali.katuri@hmcts.net>
2023-12-29 20:45:33 +0000

    Changed the logic to include Judge participant as an existing user

---
+++ BookingsApi/BookingsApi.UnitTests/Services/PublisherHelperTests.cs
@@ -0,0 +1,92 @@
+using BookingsApi.Domain.Participants;
+using BookingsApi.Infrastructure.Services.Publishers;
+
+namespace BookingsApi.UnitTests.Services
+{
+    public class PublisherHelperTests
+    {
+        [Test]
+        public void Should_return_new_participants_when_new_persons_added_as_participants()
+        {
+            var hearing = new VideoHearingBuilder().WithCase().Build();
+            
+            PublisherHelper.GetNewParticipantsSinceLastUpdate(hearing).Count().Should().Be(hearing.Participants.Count(x => x is not Judge));
+        }
+
+        [Test]
+        public void Should_return_ONLY_new_participants_when_new_persons_added_as_participants_WITH_one_exisitng_person()
+        {
+            var hearing = new VideoHearingBuilder().WithCase().Build();
+            var firstPerson = hearing.Participants.Where(x => x is not Judge).ToList()[0].Person;
+            firstPerson.GetType().GetProperty("CreatedDate").SetValue(firstPerson,firstPerson.CreatedDate.AddDays(-10), null);
+
+            PublisherHelper.GetNewParticipantsSinceLastUpdate(hearing).Count().Should().Be(hearing.Participants.Count(x => x is not Judge) - 1);
+        }
+
+        [Test]
+        public void Should_return_new_participants_when_new_persons_added_as_participants_WITH_one_exisitng_person_AND_with_username_not_set()
+        {
+            var hearing = new VideoHearingBuilder().WithCase().Build();
+            var participants = hearing.Participants.Where(x => x is not Judge).ToList();
+            participants[0].GetType().GetProperty("CreatedDate").SetValue(participants[0], participants[0].CreatedDate.AddDays(-10), null);
+            participants[1].Person.GetType().GetProperty("Username").SetValue(participants[1].Person, participants[1].Person.ContactEmail, null);
+
+            PublisherHelper.GetNewParticipantsSinceLastUpdate(hearing).Count().Should().Be(hearing.Participants.Count(x => x is not Judge) - 1);
+        }
+
+        [Test]
+        public void Should_return_ONLY_new_participants_when_new_persons_added_as_participants_TO_existing_hearing()
+        {
+            var hearing = new VideoHearingBuilder().WithCase().Build();
+            var dateWhenHearingCreated = hearing.CreatedDate.AddDays(-10);
+            hearing.GetType().GetProperty("CreatedDate").SetValue(hearing, dateWhenHearingCreated, null);
+
+            var participants = hearing.Participants.Where(x => x is not Judge).ToList();
+            participants.Skip(1).ToList().ForEach(x => x.GetType().GetProperty("CreatedDate").SetValue(x, dateWhenHearingCreated, null));
+
+            PublisherHelper.GetNewParticipantsSinceLastUpdate(hearing).Count().Should().Be(1);
+        }
+
+        [Test]
+        public void Should_return_All_existing_participants_including_Judge()
+        {
+            var hearing = new VideoHearingBuilder().WithCase().Build();
+            var newParticipants = 2;
+            var participants = hearing.Participants.Where(x => x is not Judge).ToList();
+            participants.Skip(newParticipants).ToList().ForEach(x => x.GetType().GetProperty("CreatedDate").SetValue(x, x.CreatedDate.AddDays(-10), null));
+
+            PublisherHelper.GetExistingParticipantsSinceLastUpdate(hearing).Count().Should().Be(hearing.Participants.Count() - newParticipants);
+        }
+
+        [Test]
+        public void Should_return_All_existing_participants_including_Judge_AND_Excluding_existing_person_Username_not_set()
+        {
+            var hearing = new VideoHearingBuilder().WithCase().Build();
+            var newParticipants = 2;
+            var participantWithAccountNotSet = 1;
+            var participants = hearing.Participants.Where(x => x is not Judge).ToList();
+            var firstParticipant = participants[0];
+
+            participants.SkipLast(newParticipants).ToList().ForEach(x => x.GetType().GetProperty("CreatedDate").SetValue(x, x.CreatedDate.AddDays(-10), null));
+            firstParticipant.Person.GetType().GetProperty("Username").SetValue(firstParticipant.Person, firstParticipant.Person.ContactEmail, null);
+
+            PublisherHelper.GetExistingParticipantsSinceLastUpdate(hearing).Count().Should().Be(hearing.Participants.Count() - newParticipants - participantWithAccountNotSet);
+        }
+
+        [Test]
+        public void Should_return_ONLY_existing_participants_when_existing_persons_added_as_participants_TO_existing_hearing()
+        {
+            var hearing = new VideoHearingBuilder().WithCase().Build();
+            var dateWhenHearingCreated = hearing.CreatedDate.AddDays(-10);
+            var existingParticipants = 2;
+            var judgeCount = 1;
+            hearing.GetType().GetProperty("CreatedDate").SetValue(hearing, dateWhenHearingCreated, null);
+
+            var participants = hearing.Participants.Where(x => x is not Judge).ToList();
+            participants.Skip(participants.Count - existingParticipants).ToList().ForEach(x => x.GetType().GetProperty("CreatedDate").SetValue(x, dateWhenHearingCreated, null));
+
+            PublisherHelper.GetNewParticipantsSinceLastUpdate(hearing).Count().Should().Be(existingParticipants + judgeCount);
+        }
+
+    }
+}
--- BookingsApi/BookingsApi.UnitTests/Services/SingledayHearingAsynchronousProcessTests.cs
+++ BookingsApi/BookingsApi.UnitTests/Services/SingledayHearingAsynchronousProcessTests.cs
@@ -102,5 +102,29 @@ namespace BookingsApi.UnitTests.Services
             messages.Count(x => x.IntegrationEvent is ExistingParticipantHearingConfirmationEvent).Should().Be(hearingConfirmationForExistingParticipantsMessageCount);
             messages.Count(x => x.IntegrationEvent is HearingNotificationIntegrationEvent).Should().Be(hearing.JudiciaryParticipants.Count);
         }
+
+        [Test]
+        public async Task Should_publish_messages_for_user_setup_before_booking_but_no_Account_created()
+        {
+            var hearing = new VideoHearingBuilder(addJudge: false).WithCase().WithJudiciaryPanelMember().WithJudiciaryJudge().Build();
+            hearing.Participants[0].Person.GetType().GetProperty("CreatedDate").SetValue(hearing.Participants[0].Person,
+                hearing.Participants[0].Person.CreatedDate.AddDays(-10), null);
+
+            ((FeatureTogglesStub)_featureToggles).NewTemplates = false;
+
+            var createConferenceMessageCount = 1;
+            var newParticipantWelcomeMessageCount = hearing.Participants.Count(x => x is not JudicialOfficeHolder && x is not Judge);
+            var totalMessages = 12;
+
+            await _singledayHearingAsynchronousProcess.Start(hearing);
+
+            var messages = _serviceBusQueueClient.ReadAllMessagesFromQueue(hearing.Id);
+            messages.Length.Should().Be(totalMessages);
+
+            messages.Count(x => x.IntegrationEvent is CreateAndNotifyUserIntegrationEvent).Should().Be(newParticipantWelcomeMessageCount);
+            messages.Count(x => x.IntegrationEvent is HearingIsReadyForVideoIntegrationEvent).Should().Be(createConferenceMessageCount);
+            messages.Count(x => x.IntegrationEvent is HearingNotificationIntegrationEvent).Should().Be(hearing.JudiciaryParticipants.Count + hearing.Participants.Count);
+        }
+
     }
 }
