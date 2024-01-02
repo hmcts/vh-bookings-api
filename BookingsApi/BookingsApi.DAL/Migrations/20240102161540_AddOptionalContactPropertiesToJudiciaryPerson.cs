c366e119debdd5613c02286472462b43a2a7eb4f
William Craig <mr.craig1991@gmail.com>
2024-01-02 16:49:44 +0000

    Added ef migration

---
+++ BookingsApi/BookingsApi.DAL/Migrations/20240102161540_AddOptionalContactPropertiesToJudiciaryPerson.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookingsApi.DAL.Migrations
+{
+    public partial class AddOptionalContactPropertiesToJudiciaryPerson : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsGeneric",
+                table: "JudiciaryPerson",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContactEmail",
+                table: "JudiciaryParticipant",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContactTelephone",
+                table: "JudiciaryParticipant",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsGeneric",
+                table: "JudiciaryPerson");
+
+            migrationBuilder.DropColumn(
+                name: "ContactEmail",
+                table: "JudiciaryParticipant");
+
+            migrationBuilder.DropColumn(
+                name: "ContactTelephone",
+                table: "JudiciaryParticipant");
+        }
+    }
+}
