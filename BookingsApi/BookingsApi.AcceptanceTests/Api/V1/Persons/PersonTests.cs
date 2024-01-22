ed9b5543be00d455ac74580266467af435dc9d82
William Craig <mr.craig1991@gmail.com>
2024-01-22 15:50:27 +0000

    Refactored PersonTests.cs to reduced flakey test failures

--- BookingsApi/BookingsApi.AcceptanceTests/Api/V1/Persons/PersonTests.cs
+++ BookingsApi/BookingsApi.AcceptanceTests/Api/V1/Persons/PersonTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BookingsApi.Contract.V1.Requests;
@@ -11,13 +12,14 @@ namespace BookingsApi.AcceptanceTests.Api.V1.Persons;
 
 public class PersonTests : ApiTest
 {
-    private HearingDetailsResponse _hearing;
+    private List<HearingDetailsResponse> _hearings = new();
     
-    [TearDown]
+    [OneTimeTearDown]
     public async Task TearDown()
     {
-        if (_hearing == null) return;
-        await BookingsApiClient.RemoveHearingAsync(_hearing.Id);
+        foreach (var hearing in _hearings)
+            await BookingsApiClient.RemoveHearingAsync(hearing.Id);
+        
         TestContext.WriteLine("Removed hearing");
     }
 
@@ -25,8 +27,9 @@ public class PersonTests : ApiTest
     public async Task GetPersonByContactEmail()
     {
         // arrange
-        await BookHearing();
-        var participant = _hearing.Participants[0];
+        var hearing = await BookHearing();
+        _hearings.Add(hearing);
+        var participant = hearing.Participants[0];
 
         // act
         var result = await BookingsApiClient.GetPersonByContactEmailAsync(participant.ContactEmail);
@@ -40,8 +43,9 @@ public class PersonTests : ApiTest
     public async Task GetPersonByUsernameEmail()
     {
         // arrange
-        await BookHearing();
-        var participant = _hearing.Participants.Find(x=> x.Username is not null);
+        var hearing = await BookHearing();
+        _hearings.Add(hearing);
+        var participant = hearing.Participants.Find(x=> x.Username is not null);
 
         // act
         var result = await BookingsApiClient.GetPersonByUsernameAsync(participant.Username);
@@ -54,24 +58,30 @@ public class PersonTests : ApiTest
     public async Task GetPersonBySearchTerm()
     {
         // arrange
-        await BookHearing();
-        var participant = _hearing.Participants[0];
+        var hearing = await BookHearing();
+        _hearings.Add(hearing);
+        var participant = hearing.Participants.Find(x=> x.UserRoleName == "Individual");
 
         // act
-        var result = (await BookingsApiClient.PostPersonBySearchTermAsync(new SearchTermRequest(participant.ContactEmail[..5])));
+        var result = await BookingsApiClient.PostPersonBySearchTermAsync(new SearchTermRequest(participant.ContactEmail[..3]));
 
         // assert
         result.Should().NotBeNullOrEmpty();
-        result.First(x => x.ContactEmail == participant.ContactEmail)
-            .Should().BeEquivalentTo(participant, options => options.ExcludingMissingMembers().Excluding(p => p.Id));
+        PersonResponse searchedForParticipant = null;
+
+        searchedForParticipant = result.FirstOrDefault(x => x.ContactEmail == participant.ContactEmail);
+        searchedForParticipant.Should().NotBeNull();    
+        searchedForParticipant.Should().BeEquivalentTo(participant, 
+            options => options.ExcludingMissingMembers()
+                                    .Excluding(p => p.Id));
     }
 
-    private async Task BookHearing()
+    private Task<HearingDetailsResponse> BookHearing()
     {
         var hearingSchedule = DateTime.UtcNow.AddMinutes(5);
         var caseName = "Bookings Api AC Automated";
         var request = new SimpleBookNewHearingRequest(caseName, hearingSchedule).Build();
-        _hearing = await BookingsApiClient.BookNewHearingAsync(request);
+        return BookingsApiClient.BookNewHearingAsync(request);
     }
 
 }
