42b23092ba75f1cabf6c47f324c3505623a021e7
William Craig <mr.craig1991@gmail.com>
2024-01-23 12:05:36 +0000

    Added changes for VIH-10411 scenario 2 judiciary participants not being updated when hearing schedule is updated

--- BookingsApi/BookingsApi/Services/BookingService.cs
+++ BookingsApi/BookingsApi/Services/BookingService.cs
@@ -115,18 +115,28 @@ public class BookingService : IBookingService
 
         await _eventPublisher.PublishAsync(new HearingDetailsUpdatedIntegrationEvent(updatedHearing));
 
+        await PublishHearingUpdateNotificationToParticipants(originalHearing, updatedHearing);
+
+        return updatedHearing;
+    }
+
+    private async Task PublishHearingUpdateNotificationToParticipants(VideoHearing originalHearing, VideoHearing updatedHearing)
+    {
         if (updatedHearing.ScheduledDateTime.Ticks != originalHearing.ScheduledDateTime.Ticks)
         {
             var @case = originalHearing.GetCases()[0];
             foreach (var participant in originalHearing.Participants)
             {
                 var participantDto = ParticipantDtoMapper.MapToDto(participant, originalHearing.OtherInformation);
-                
                 await _eventPublisher.PublishAsync(new HearingAmendmentNotificationEvent(EventDtoMappers.MapToHearingConfirmationDto(originalHearing.Id, 
-                        originalHearing.ScheduledDateTime, participantDto, @case),  updatedHearing.ScheduledDateTime));
+                    originalHearing.ScheduledDateTime, participantDto, @case),  updatedHearing.ScheduledDateTime));
+            }
+            foreach (var judiciaryParticipant in originalHearing.JudiciaryParticipants)
+            {
+                var participantDto = ParticipantDtoMapper.MapToDto(judiciaryParticipant);
+                await _eventPublisher.PublishAsync(new HearingAmendmentNotificationEvent(EventDtoMappers.MapToHearingConfirmationDto(originalHearing.Id, 
+                    originalHearing.ScheduledDateTime, participantDto, @case),  updatedHearing.ScheduledDateTime));
             }
         }
-
-        return updatedHearing;
     }
 }
