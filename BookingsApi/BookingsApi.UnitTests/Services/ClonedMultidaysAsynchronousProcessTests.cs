bf976e408ac8830512368a278b2d9c2fd35bd9c8
Oliver Scott <oliver.scott@justice.gov.uk>
2024-02-19 12:59:41 +0000

    VIH-10493 Fix - multiday notifications not sent when new notify templates are toggled off (#821)
    * Fix for notifications not being sent for existing multiday users with notify template feature off
    
    * Add integration test
    
    * Change hardcoded value
    
    * Fix for V2 as well
    
    * Sonar fix
    
    * Fix typo in dto

--- BookingsApi/BookingsApi.UnitTests/Services/ClonedMultidaysAsynchronousProcessTests.cs
+++ BookingsApi/BookingsApi.UnitTests/Services/ClonedMultidaysAsynchronousProcessTests.cs
@@ -1,6 +1,10 @@
+using System.Collections.Generic;
 using BookingsApi.Common.Services;
+using BookingsApi.Domain;
 using BookingsApi.Domain.Participants;
+using BookingsApi.Infrastructure.Services;
 using BookingsApi.Infrastructure.Services.AsynchronousProcesses;
+using BookingsApi.Infrastructure.Services.Dtos;
 using BookingsApi.Infrastructure.Services.IntegrationEvents;
 using BookingsApi.Infrastructure.Services.IntegrationEvents.Events;
 using BookingsApi.Infrastructure.Services.Publishers;
@@ -27,7 +31,7 @@ namespace BookingsApi.UnitTests.Services
         }
         
         [Test]
-        public async Task Should_publish_messages_for_single_day_booking()
+        public async Task Should_publish_messages_with_new_notify_templates_feature_on()
         {
             var hearing = new VideoHearingBuilder().WithCase().Build();
             hearing.Participants[0].Person.GetType().GetProperty("CreatedDate").SetValue(hearing.Participants[0].Person, 
@@ -35,6 +39,8 @@ namespace BookingsApi.UnitTests.Services
             hearing.Participants[1].Person.GetType().GetProperty("CreatedDate").SetValue(hearing.Participants[1].Person,
                 hearing.Participants[1].Person.CreatedDate.AddDays(-10), null);
 
+            ((FeatureTogglesStub)_featureToggles).NewTemplates = true;
+            
             var judgeAsExistingParticipant = 1;
             var createConfereceMessageCount = 0;
             var newParticipantWelcomeMessageCount = 0;
@@ -54,6 +60,65 @@ namespace BookingsApi.UnitTests.Services
             messages.Count(x => x.IntegrationEvent is HearingIsReadyForVideoIntegrationEvent).Should().Be(createConfereceMessageCount);
             messages.Count(x => x.IntegrationEvent is ExistingParticipantMultidayHearingConfirmationEvent).
                 Should().Be(mulitdayHearingConfirmationForExistingParticipantsMessageCount + judgeAsExistingParticipant);
+        }
+
+        [Test]
+        public async Task Should_publish_messages_for_v1_with_new_notify_templates_feature_off()
+        {
+            // Arrange
+            var hearing = new VideoHearingBuilder().WithCase().Build();
+
+            ((FeatureTogglesStub)_featureToggles).NewTemplates = false;
+
+            const int expectedTotalMessageCount = 6;
+            const int totalDays = 2;
+            
+            // Act
+            await _clonedMultidaysAsynchronousProcess.Start(hearing, totalDays);
+            
+            // Assert
+            AssertEventsPublishedForNotifyFeatureOff(hearing, totalDays, expectedTotalMessageCount);
+        }
+        
+        [Test]
+        public async Task Should_publish_messages_with_v2_with_new_notify_templates_feature_off()
+        {
+            // Arrange
+            var hearing = new VideoHearingBuilder(addJudge: false).WithCase().Build();
+            var judiciaryJudgePerson = new JudiciaryPersonBuilder(Guid.NewGuid().ToString()).Build();
+            hearing.AddJudiciaryJudge(judiciaryJudgePerson, "Judiciary Judge");
+            var panelMemberParticipants = hearing.Participants.Where(p => p is JudicialOfficeHolder).ToList();
+            foreach (var participant in panelMemberParticipants)
+            {
+                // Remove these since they are V1 johs and not applicable to V2
+                hearing.Participants.Remove(participant);
+            }
+
+            ((FeatureTogglesStub)_featureToggles).NewTemplates = false;
+
+            const int expectedTotalMessageCount = 5;
+            const int totalDays = 2;
+            
+            // Act
+            await _clonedMultidaysAsynchronousProcess.Start(hearing, totalDays);
+            
+            // Assert
+            AssertEventsPublishedForNotifyFeatureOff(hearing, totalDays, expectedTotalMessageCount);
+        }
+        
+        private void AssertEventsPublishedForNotifyFeatureOff(Hearing hearing, int totalDayCount, int expectedTotalMessageCount)
+        {
+            var messages = _serviceBusQueueClient.ReadAllMessagesFromQueue(hearing.Id);
+
+            messages.Count(x => x.IntegrationEvent is MultiDayHearingIntegrationEvent).Should().Be(expectedTotalMessageCount);
+            var hearingConfirmationDtos = HearingConfirmationForParticipantDtoMapper.MapToDtos(hearing);
+            var multiDayIntegrationEvents = messages
+                .Where(x => x.IntegrationEvent is MultiDayHearingIntegrationEvent)
+                .Select(x => x.IntegrationEvent as MultiDayHearingIntegrationEvent)
+                .ToList();
+            multiDayIntegrationEvents.TrueForAll(x => x.TotalDays == totalDayCount).Should().BeTrue();
+            multiDayIntegrationEvents.Select(x => x.HearingConfirmationForParticipant)
+                .Should().BeEquivalentTo(hearingConfirmationDtos);
         }
     }
 }
