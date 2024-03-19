03d3a6b578c3ce356474cd331f88e8efe3170de3
marcogagliardi <marco.gagliardi@justice.gov.uk>
2024-03-19 08:59:51 +0000

    Update HearingNotificationEventForJudiciaryParticipantPublisher.cs

--- BookingsApi/BookingsApi.Infrastructure.Services/Publishers/HearingNotificationEventForJudiciaryParticipantPublisher.cs
+++ BookingsApi/BookingsApi.Infrastructure.Services/Publishers/HearingNotificationEventForJudiciaryParticipantPublisher.cs
@@ -4,6 +4,7 @@ using BookingsApi.Infrastructure.Services.IntegrationEvents;
 using BookingsApi.Infrastructure.Services.IntegrationEvents.Events;
 using System.Linq;
 using System.Threading.Tasks;
+using BookingsApi.Domain.Participants;
 
 namespace BookingsApi.Infrastructure.Services.Publishers
 {
@@ -19,7 +20,18 @@ namespace BookingsApi.Infrastructure.Services.Publishers
 
         public async Task PublishAsync(VideoHearing videoHearing)
         {
+            var videoHearingUpdateDate = videoHearing.UpdatedDate.TrimSeconds();
+            // we need to send a hearing confirmation for new Panel Member created for V1 and send new templates. A create email for those users
+            // has been sent previously with login details and needs a second email for hearing confirmation
+            var newJudicialOfficers = PublisherHelper.GetNewParticipantsSinceLastUpdate(videoHearing, videoHearingUpdateDate).Where(x => x is JudicialOfficeHolder);
+
             var @case = videoHearing.GetCases()[0];
+            foreach (var participant in newJudicialOfficers)
+            {
+                var participantDto = ParticipantDtoMapper.MapToDto(participant, videoHearing.OtherInformation);
+                await _eventPublisher.PublishAsync(new HearingNotificationIntegrationEvent(EventDtoMappers.MapToHearingConfirmationDto(
+                    videoHearing.Id, videoHearing.ScheduledDateTime, participantDto, @case)));
+            }
             foreach (var participant in videoHearing.JudiciaryParticipants)
             {
                 await _eventPublisher.PublishAsync(new HearingNotificationIntegrationEvent(EventDtoMappers.MapToHearingConfirmationDto(
