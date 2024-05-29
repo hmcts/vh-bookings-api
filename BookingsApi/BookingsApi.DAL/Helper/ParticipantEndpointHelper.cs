d1240aadd56d2304842aea7bec1efb573889734c
William Craig <mr.craig1991@gmail.com>
2024-05-29 16:35:22 +0100

    error handling

--- BookingsApi/BookingsApi.DAL/Helper/ParticipantEndpointHelper.cs
+++ BookingsApi/BookingsApi.DAL/Helper/ParticipantEndpointHelper.cs
@@ -5,10 +5,17 @@ namespace BookingsApi.DAL.Helper;
 
 public static class ParticipantEndpointHelper
 {
-    public static IEnumerable<(Participant, LinkedParticipantType)> GetEndpointParticipants(List<Participant> hearingParticipants, List<NewEndpointParticipantDto> endpointParticipants)
+    public static IEnumerable<(Participant, LinkedParticipantType)> GetEndpointParticipants(
+        List<Participant> hearingParticipants, List<NewEndpointParticipantDto> endpointParticipants)
     {
-        return endpointParticipants.Select(dto 
-            => (hearingParticipants.First(x => x.Person.ContactEmail.Equals(dto.ContactEmail, StringComparison.CurrentCultureIgnoreCase)), dto.Type));
+        if (endpointParticipants == null || !endpointParticipants.Any())
+        {
+            return new List<(Participant, LinkedParticipantType)>();
+        }
+
+        return endpointParticipants.Select(dto
+            => (hearingParticipants.Find(x =>
+                    x.Person.ContactEmail.Equals(dto.ContactEmail, StringComparison.CurrentCultureIgnoreCase)), dto.Type));
     }
-    
+
 }
