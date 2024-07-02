95de52443dd50c1eff741f2387ec4f5df5067249
William Craig <mr.craig1991@gmail.com>
2024-07-02 12:20:18 +0100

    validation

--- BookingsApi/BookingsApi/Validations/V2/ParticipantValidationV2.cs
+++ BookingsApi/BookingsApi/Validations/V2/ParticipantValidationV2.cs
@@ -2,7 +2,7 @@ namespace BookingsApi.Validations.V2
 {
     public static class ParticipantValidationV2
     {
-        public const string NameRegex = "^(?!.*[^\\w\\s'._-]{2})\\w+(?:[\\s'._-]\\w+)*$";
+        public const string NameRegex = "^(?!.*[^\\w\\s'.-]{2})\\w+(?:[\\s'._-]\\w+)*$";
         public static readonly string FirstNameDoesntMatchRegex = "First name must match regular expression";
         public static readonly string LastNameDoesntMatchRegex = "Last name must match regular expression";
         public static readonly string NoFirstNameErrorMessage = "First name is required";
