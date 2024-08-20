3f9f0e390d844629b9e1086b53f61fa9214018e3
marcogagliardi <marco.gagliardi@justice.gov.uk>
2024-08-20 10:06:52 +0100

    VIH-8198 - Allow accented letters in DisplayName field in AdminWeb Booking Form (#874)
    * Update ParticipantRequestV2.cs
    
    * changed regex on display name of models
    
    * Update ParticipantRequestValidationV2.cs
    
    * added regex to requests
    
    * updated regex for endpoint

--- BookingsApi/BookingsApi/Validations/V2/EndpointRequestValidationV2.cs
+++ BookingsApi/BookingsApi/Validations/V2/EndpointRequestValidationV2.cs
@@ -11,7 +11,7 @@ public class EndpointRequestValidationV2 : AbstractValidator<EndpointRequestV2>
     public EndpointRequestValidationV2()
     {
         // regex where only alphanumeric and underscore are allowed and maximum 255 characters
-        var regex = "^([-A-Za-z0-9 ',._]){1,255}$";
+        var regex = @"^[\p{L}\p{N}\s',._-]+${1,255}$";
             
         RuleFor(x => x.DisplayName).NotEmpty().Matches(regex).WithMessage(InvalidDisplayNameErrorMessage);
         RuleFor(x => x.DefenceAdvocateContactEmail).Must(x => x.IsValidEmail()).When(x => x.DefenceAdvocateContactEmail != null)
