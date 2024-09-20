159413142d3525e2f5d5829b7622b252065b2c85
Oliver Scott <oliver.scott@justice.gov.uk>
2024-09-20 12:57:54 +0100

    Add pascal case enum conversion

---
+++ BookingsApi/BookingsApi.Common.Legacy/Helpers/PascalCaseEnumConverterFactory.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using JsonConverter = System.Text.Json.Serialization.JsonConverter;
+
+namespace BookingsApi.Common.Legacy.Helpers;
+
+public class PascalCaseEnumConverterFactory : JsonConverterFactory
+{
+    public override bool CanConvert(Type typeToConvert)
+    {
+        return typeToConvert.IsEnum;
+    }
+
+    public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+    {
+        var converterType = typeof(PascalCaseEnumConverter<>).MakeGenericType(typeToConvert);
+        return (JsonConverter)Activator.CreateInstance(converterType);
+    }
+}
