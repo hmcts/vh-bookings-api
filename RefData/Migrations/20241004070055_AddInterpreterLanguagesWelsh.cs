23ab04118b3a45d3f1f773f0e6f1d6ae6dc7e2b7
marcogagliardi <marco.gagliardi@justice.gov.uk>
2024-10-08 15:35:42 +0100

    added interpreter language welsh (#899)

---
+++ RefData/Migrations/20241004070055_AddInterpreterLanguagesWelsh.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RefData.Migrations
+{
+    [DbContext(typeof(RefDataContext))]
+    [Migration("20241004070055_AddInterpreterLanguagesWelsh")]
+    public partial class AddInterpreterLanguagesWelsh : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            SqlFileHelper.RunSqlFile("data/10837_update_interpreter_languages_welsh.sql", migrationBuilder);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
