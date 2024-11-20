81dc20636b4917cf4c26990c14d92fb65f26b40a
marcogagliardi <marco.gagliardi@justice.gov.uk>
2024-11-20 13:57:57 +0000

    - Add Jurisdiction Upper Tribunal Tax and Chancery Chamber - Add CaseType linked to the new Jurisdiction

---
+++ RefData/Migrations/20241120150655_AddUpperTribunals.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RefData.Migrations
+{
+    [DbContext(typeof(RefDataContext))]
+    [Migration("20241120150655_AddUpperTribunals")]
+    public partial class AddUpperTribunals : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            SqlFileHelper.RunSqlFile("data/11105_mrd_add_case_types_upper_tribunals.sql", migrationBuilder);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
