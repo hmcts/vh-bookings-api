3c37d6a9763e2cf0229dc1bb276916e1c5098234
marcogagliardi <marco.gagliardi@justice.gov.uk>
2024-11-21 15:11:37 +0000

    Updated expiration date

---
+++ RefData/Migrations/20241121145559_AddExpiringDateToCaseTypes.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RefData.Migrations
+{
+    [DbContext(typeof(RefDataContext))]
+    [Migration("20241121145559_AddExpiringDateToCaseTypes")]
+    public partial class AddExpiringDateToCaseTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            SqlFileHelper.RunSqlFile("data/11107_add_case_types_expiration_dates.sql", migrationBuilder);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ RefData/data/11107_add_case_types_expiration_dates.sql
@@ -0,0 +1,39 @@
+SET XACT_ABORT ON
+GO;
+
+BEGIN TRANSACTION;
+
+UPDATE VhBookings.dbo.CaseType SET ExpirationDate = '2024-11-29', UpdatedDate = CURRENT_TIMESTAMP WHERE Name IN 
+(
+    'Business Lease Renewal',
+    'Children Act',
+    'Civil',
+    'Family',
+    'Family Law Act',
+    'GRC - Alternative Business Structures',
+    'GRC - Claims Management Services',
+    'GRC - Consumer Credit',
+    'GRC - EJ',
+    'GRC - Local Government Standards',
+    'GRC - Professional Regulations',
+    'Housing & Planning Act',
+    'Housing Act',
+    'Leasehold Enfranchisement',
+    'Leasehold Management',
+    'Park Homes',
+    'Placement',
+    'Rents',
+    'Right to buy',
+    'Tenant Fees',
+    'Tribunal',
+    'Employment Appeals Tribunal',
+    'Upper Tribunal Administrative Appeals Chamber',
+    'Upper Tribunals Land Chamber'
+)
+
+GO;
+
+SELECT * FROM VhBookings.dbo.HearingVenue WHERE HearingVenue.ExpirationDate IS NOT NULL
+
+COMMIT TRANSACTION;
+SET XACT_ABORT OFF
