3f24a3e50d07cbf461e0820e74befeafe6e6ca32
marcogagliardi <marco.gagliardi@justice.gov.uk>
2024-11-22 14:32:44 +0000

    Update 11107_add_case_types_expiration_dates.sql

--- RefData/data/11107_add_case_types_expiration_dates.sql
+++ RefData/data/11107_add_case_types_expiration_dates.sql
@@ -33,7 +33,5 @@ UPDATE VhBookings.dbo.CaseType SET ExpirationDate = '2024-11-29', UpdatedDate = 
 
 GO;
 
-SELECT * FROM VhBookings.dbo.HearingVenue WHERE HearingVenue.ExpirationDate IS NOT NULL
-
 COMMIT TRANSACTION;
 SET XACT_ABORT OFF
