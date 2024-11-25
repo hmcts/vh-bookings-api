3086f060e541d5ebb2865f6e660d81a0c8dca6ad
marcogagliardi <marco.gagliardi@justice.gov.uk>
2024-11-25 14:54:21 +0000

    added Jurisdictions

---
+++ RefData/Migrations/20241125160655_AddJurisdictions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RefData.Migrations
+{
+    [DbContext(typeof(RefDataContext))]
+    [Migration("20241125160655_AddJurisdictions")]
+    public partial class AddJurisdictions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            SqlFileHelper.RunSqlFile("data/11173_mrd_update_case_types_with_Jurisdiction.sql", migrationBuilder);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
+
+
