2365bc894bf1b390267c9cf4b0a6da38f7154dbd
marcogagliardi <marco.gagliardi@justice.gov.uk>
2024-11-26 14:44:47 +0000

    Update 11111_mrd_case_types_amend_names.sql

--- RefData/data/11111_mrd_case_types_amend_names.sql
+++ RefData/data/11111_mrd_case_types_amend_names.sql
@@ -6,7 +6,7 @@ BEGIN TRANSACTION;
 
 UPDATE CaseType SET Name = 'Energy & Infrastructure', UpdatedDate = CURRENT_TIMESTAMP WHERE ServiceId = 'BAB5';
 UPDATE CaseType SET Name = 'Pensions Regulation', UpdatedDate = CURRENT_TIMESTAMP WHERE ServiceId = 'BAB9';
-UPDATE CaseType SET Name = 'FtT Tax Chamber inc MPs Expenses', UpdatedDate = CURRENT_TIMESTAMP WHERE ServiceId = 'BDA1';
+UPDATE CaseType SET Name = 'FtT Tax Chamber inc MP''s Expenses', UpdatedDate = CURRENT_TIMESTAMP WHERE ServiceId = 'BDA1';
 
 GO;
 
