d85ee55d2592f90fe9c5e870ec0f5c0795f46c7e
marcogagliardi <marco.gagliardi@justice.gov.uk>
2024-11-27 10:57:03 +0000

    VIH-11111 Amend MRD services on VH (#923)
    * Create 11111_mrd_case_types_amend_names.sql
    
    * update case types
    
    * Update 11111_mrd_case_types_amend_names.sql

---
+++ RefData/Migrations/20241120150655_RenameCaseTypes.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RefData.Migrations
+{
+    [DbContext(typeof(RefDataContext))]
+    [Migration("20241120150655_RenameCaseTypes")]
+    public partial class RenameCaseTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            SqlFileHelper.RunSqlFile("data/11111_mrd_case_types_amend_names.sql", migrationBuilder);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
