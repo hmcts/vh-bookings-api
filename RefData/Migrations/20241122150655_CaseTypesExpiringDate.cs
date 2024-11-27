71a154900ef03604de7ce97897930678e1cedaf1
marcogagliardi <marco.gagliardi@justice.gov.uk>
2024-11-27 09:08:15 +0000

    VIH-11108 Expire upper tribunal case types (#921)
    * update expiring date
    
    * Update 11108_mrd_case_types_expiring_date.sql

---
+++ RefData/Migrations/20241122150655_CaseTypesExpiringDate.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RefData.Migrations
+{
+    [DbContext(typeof(RefDataContext))]
+    [Migration("20241122150655_CaseTypesExpiringDate")]
+    public partial class CaseTypesExpiringDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            SqlFileHelper.RunSqlFile("data/11108_mrd_case_types_expiring_date.sql", migrationBuilder);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
