7cc036f5ca43404386716fdc7959bd5455734664
Shaed Parkar <shaed.parkar@slimsolutions.tech>
2025-02-20 16:39:20 +0000

    VIH-11301 provide the justice name in the allocation update to avoid additional api calls

--- BookingsApi/BookingsApi.Infrastructure.Services/JusticeUserDtoMapper.cs
+++ BookingsApi/BookingsApi.Infrastructure.Services/JusticeUserDtoMapper.cs
@@ -11,6 +11,7 @@ namespace BookingsApi.Infrastructure.Services
             return new JusticeUserDto
             {
                 Username = user.Username,
+                AllocatedToName = $"{user.FirstName} {user.Lastname}",
                 UserId = user.Id.ToString(),
                 UserRoles = user.JusticeUserRoles.Select(jur => jur.UserRole.Name).ToArray()
             };
